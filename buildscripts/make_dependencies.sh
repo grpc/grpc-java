268a35cacc2513e0e629752af2d2531fc7c4e7b5
Louis Ryan <lryan@google.com>
2015-03-19 15:02:24 -0700

    Install GCC-4.8 into /tmp so we can build with c++11 on travis without sudo

--- buildscripts/make_dependencies.sh
+++ buildscripts/make_dependencies.sh
@@ -3,17 +3,29 @@
 # Build protoc & netty
 set -ev
 
+# Need GCC 4.8 for C++ support which is not available by default with travis
+# and we don't want to use sudo on travis to install it as we couldn't use dockerized travis so...
+pushd .
+cd /tmp
+mkdir gcc
+cd gcc
+apt-get download -qq gcc-4.8 g++-4.8 cpp-4.8 libgcc-4.8-dev libstdc++-4.8-dev g++-4.8-multilib gcc-4.8-multilib
+find . -name '*.deb' -exec dpkg --extract {} . \;
+export CXX="/tmp/gcc/usr/bin/g++-4.8"
+export CC="/tmp/gcc/usr/bin/gcc-4.8"
+export ACLOCAL="/usr/bin/aclocal"
+popd
+
 # Make protoc
 pushd .
 cd /tmp
-git clone https://github.com/google/protobuf.git
-cd protobuf
-git checkout v3.0.0-alpha-2
+wget -O - https://github.com/google/protobuf/archive/v3.0.0-alpha-2.tar.gz | tar xz
+cd protobuf-3.0.0-alpha-2
 ./autogen.sh
 # install here so we don't need sudo
 ./configure --prefix=/tmp/grpc-deps
-make
-make check
+make -j12
+make check -j12
 make install
 cd java
 mvn install
