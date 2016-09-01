2687267ed5e35cdfaa82a29417bbee21288beb06
Carl Mastrangelo <notcarl@google.com>
2016-08-31 18:04:12 -0700

    bump some more

--- interop-testing/src/main/java/io/grpc/testing/integration/AbstractInteropTest.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/AbstractInteropTest.java
@@ -964,7 +964,7 @@ public abstract class AbstractInteropTest {
 
     X509Certificate x509cert = (X509Certificate) certificates.get(0);
 
-    assertEquals(1, certificates.size());
+    assertEquals(certificates.toString(), 1, certificates.size());
     assertEquals(tlsInfo, x509cert.getSubjectDN().toString());
   }
 
