64c5e9684a9ce41bbe4b263c55b8f649fb60ea73
Kun Zhang <zhangkun@google.com>
2016-11-17 11:09:36 -0800

    ConnectivityStateInfo:
    Fix a bug in equals() and switch to a non-nullable interface.

--- core/src/main/java/io/grpc/ConnectivityStateInfo.java
+++ core/src/main/java/io/grpc/ConnectivityStateInfo.java
@@ -35,16 +35,16 @@ import com.google.common.base.Preconditions;
 
 import io.grpc.Status;
 
-import javax.annotation.Nullable;
-
 /**
- * A tuple of a {@link ConnectivityState} and an error {@link Status} that is present only with
- * {@link io.grpc.ConnectivityState#TRANSIENT_FAILURE}.
+ * A tuple of a {@link ConnectivityState} and its associated {@link Status}.
+ *
+ * <p>If the state is {@code TRANSIENT_FAILURE}, the status is never {@code OK}.  For other states,
+ * the status is always {@code OK}.
  */
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/1771")
 public final class ConnectivityStateInfo {
   private final ConnectivityState state;
-  @Nullable private final Status error;
+  private final Status status;
 
   /**
    * Returns an instance for a state that is not {@code TRANSIENT_FAILURE}.
@@ -54,24 +54,32 @@ public final class ConnectivityStateInfo {
   public static ConnectivityStateInfo forNonError(ConnectivityState state) {
     Preconditions.checkArgument(state != ConnectivityState.TRANSIENT_FAILURE,
         "state is TRANSIENT_ERROR. Use forError() instead");
-    return new ConnectivityStateInfo(state, null);
+    return new ConnectivityStateInfo(state, Status.OK);
   }
 
   /**
    * Returns an instance for {@code TRANSIENT_FAILURE}, associated with an error status.
    */
   public static ConnectivityStateInfo forTransientFailure(Status error) {
-    Preconditions.checkNotNull(error, "error is null");
+    Preconditions.checkArgument(!error.isOk(), "The error status must not be OK");
     return new ConnectivityStateInfo(ConnectivityState.TRANSIENT_FAILURE, error);
   }
 
+  /**
+   * Returns the state.
+   */
   public ConnectivityState getState() {
     return state;
   }
 
-  @Nullable
-  public Status getError() {
-    return error;
+  /**
+   * Returns the status associated with the state.
+   *
+   * <p>If the state is {@code TRANSIENT_FAILURE}, the status is never {@code OK}.  For other
+   * states, the status is always {@code OK}.
+   */
+  public Status getStatus() {
+    return status;
   }
 
   @Override
@@ -80,35 +88,24 @@ public final class ConnectivityStateInfo {
       return false;
     }
     ConnectivityStateInfo o = (ConnectivityStateInfo) other;
-    if (!state.equals(o.state)) {
-      return false;
-    }
-    if (error == null) {
-      return o.state == null;
-    } else {
-      return error.equals(o.state);
-    }
+    return state.equals(o.state) && status.equals(o.status);
   }
 
   @Override
   public int hashCode() {
-    int value = state.hashCode();
-    if (error != null) {
-      value = value ^ error.hashCode();
-    }
-    return value;
+    return state.hashCode() ^ status.hashCode();
   }
 
   @Override
   public String toString() {
-    if (error == null) {
+    if (status.isOk()) {
       return state.toString();
     }
-    return state + "(" + error + ")";
+    return state + "(" + status + ")";
   }
 
-  private ConnectivityStateInfo(ConnectivityState state, Status error) {
+  private ConnectivityStateInfo(ConnectivityState state, Status status) {
     this.state = Preconditions.checkNotNull(state, "state is null");
-    this.error = error;
+    this.status = Preconditions.checkNotNull(status, "status is null");
   }
 }
