b67b858a251a8c7bf6781c1705a04c87e438511e
Kun Zhang <zhangkun@google.com>
2017-01-13 10:55:41 -0800

    Phrasing.

--- core/src/main/java/io/grpc/internal/ManagedChannelImpl.java
+++ core/src/main/java/io/grpc/internal/ManagedChannelImpl.java
@@ -775,7 +775,7 @@ public final class ManagedChannelImpl extends ManagedChannel implements WithLogI
       try {
         balancer.handleResolvedAddresses(servers, config);
       } catch (Throwable e) {
-        log.log(Level.WARNING, "[" + getLogId() + "] Caught exception from LoadBalancer", e);
+        log.log(Level.WARNING, "[" + getLogId() + "] Unexpected exception from LoadBalancer", e);
         // It must be a bug! Push the exception back to LoadBalancer in the hope that it may be
         // propagated to the application.
         balancer.handleNameResolutionError(Status.INTERNAL.withCause(e)
