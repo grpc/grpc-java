6c6a6df88daafa14bb60448de0fb4eb91484d55f
Eric Anderson <ejona@google.com>
2017-04-20 15:44:23 -0700

    Remove unused import

--- grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
+++ grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
@@ -44,7 +44,6 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.inOrder;
