1ed2f8f7b4c6eac65f7f1b6f8d43deaa8e35cdc8
Carl Mastrangelo <notcarl@google.com>
2017-04-25 16:18:07 -0700

    un

--- core/src/main/java/io/grpc/internal/GrpcUtil.java
+++ core/src/main/java/io/grpc/internal/GrpcUtil.java
@@ -61,7 +61,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.Nullable;
 
 /**
