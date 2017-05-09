66ae55c47211946713784233952147229e39ddbc
dapengzhang0 <zdapeng@google.com>
2017-05-09 13:22:03 -0700

    more stub package doc

--- stub/src/main/java/io/grpc/stub/package-info.java
+++ stub/src/main/java/io/grpc/stub/package-info.java
@@ -32,19 +32,44 @@
 /**
  * API for the Stub layer.
  *
- * <p>The gRPC Java API is split into two main parts: The stub layer and the call layer layer. The
- * stub layer is a wrapper around the call layer.
- *
- * <p>The server side stub classes are abstract classes with abstract PRC methods for the server
- * application to implement. The server stubs use {@link io.grpc.stub.ServerCalls} to interact with
- * the call layer.
- *
- * <p>The client side stub classes are implementations of {@link io.grpc.stub.AbstractStub} that
- * provides the RPC methods for the client application to call. The RPC methods in the client stubs
- * use {@link io.grpc.stub.ClientCalls} to interact with the call layer.
+ * <p>The gRPC Java API is split into two main parts: The stub layer and the call layer. Thestub
+ * layer is a wrapper around the call layer.
  *
  * <p>In the most common case of gRPC over Protocol Buffers, stub classes are automatically
- * generated from the service definition .proto files by the Protobuf compiler. See gRPC java
- * Generated Code documentation.
+ * generated from service definition .proto files by the Protobuf compiler. See <a
+ * href="http://www.grpc.io/docs/generatedcode/java.html">gRPC java Generated Code Guide</a>.
+ *
+ * <p>The server side stub classes are abstract classes with abstract RPC methods for the server
+ * application to implement. These classes internally use {@link io.grpc.stub.ServerCalls} to
+ * interact with the call layer. The abstract RPC methods take a {@link io.grpc.stub.StreamObserver}
+ * object {@code responseObserver} as one of its arguments. When users are implementing these
+ * methods in the server application, they may call the {@link io.grpc.stub.StreamObserver#onNext
+ * onNext()}, {@link io.grpc.stub.StreamObserver#onError onError()} and {@link
+ * io.grpc.stub.StreamObserver#onCompleted onCompleted()} methods on the {@code responseObserver}
+ * argument to send out a response message, error and completion notification respectively. If the
+ * RPC is client-streaming or bidirectional-streaming, the abstract RPC method should return a
+ * {@code requestObserver} which is also a {@link io.grpc.stub.StreamObserver} object. User should
+ * typically implement the {@link io.grpc.stub.StreamObserver#onNext onNext()}, {@link
+ * io.grpc.stub.StreamObserver#onError onError()} and {@link io.grpc.stub.StreamObserver#onCompleted
+ * onCompleted()} callbacks of {@code requestObserver} to define how the server application would
+ * react when receiving a message, error and completion notification respectively from the client
+ * side.
+ *
+ * <p>The client side stub classes are implementations of {@link io.grpc.stub.AbstractStub} that
+ * provide the RPC methods for the client application to call. The RPC methods in the client stubs
+ * internally use {@link io.grpc.stub.ClientCalls} to interact with the call layer. For asynchronous
+ * stubs, the RPC methods also take a {@link io.grpc.stub.StreamObserver} object {@code
+ * responseObserver} as one of its arguments, and moreover for client-streaming or
+ * bidirectional-streaming, also return a {@code requestObserver} which is also a {@link
+ * io.grpc.stub.StreamObserver} object. In contrast to the server side, users should implement the
+ * {@link io.grpc.stub.StreamObserver#onNext onNext()}, {@link io.grpc.stub.StreamObserver#onError
+ * onError()} and {@link io.grpc.stub.StreamObserver#onCompleted onCompleted()} callbacks of {@code
+ * responseObserver} to define what the client application would do when receiving a response
+ * message, error and completion notification respectively from the server side, and then pass the
+ * {@code requestObserver} to the RPC method in the client stub. If the RPC method returns a {@code
+ * requestObserver}, user should call the {@link io.grpc.stub.StreamObserver#onNext onNext()},
+ * {@link io.grpc.stub.StreamObserver#onError onError()} and {@link
+ * io.grpc.stub.StreamObserver#onCompleted onCompleted()} methods on the {@code requestObserver} to
+ * send out a request message, error and completion notification respectively.
  */
 package io.grpc.stub;
