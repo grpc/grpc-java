d4a05089decbf036f509b94bb3405dffde221bd9
Eric Gribkoff <ericgribkoff@google.com>
2017-05-12 11:26:17 -0700

    Revert "Notify bytesRead directly from the transport thread"

--- core/src/main/java/io/grpc/internal/MessageDeframer.java
+++ core/src/main/java/io/grpc/internal/MessageDeframer.java
@@ -321,12 +321,9 @@ public class MessageDeframer {
       boolean needToCloseData = true;
       try {
         Preconditions.checkState(!isScheduledToClose(), "close already scheduled");
-        int dataSize = data.readableBytes();
         unprocessed.addBuffer(data);
         needToCloseData = false;
         sinkListener.scheduleDeframerSource(source);
-        // TODO(ericgribkoff) Clean up interfaces (move this to Sink.Listener)
-        source.sourceListener.bytesRead(dataSize);
       } finally {
         if (needToCloseData) {
           data.close();
@@ -481,8 +478,7 @@ public class MessageDeframer {
         return true;
       } finally {
         if (totalBytesRead > 0) {
-          // TODO(ericgribkoff) Performance improves by notifying directly from transport thread.
-          //sourceListener.bytesRead(totalBytesRead);
+          sourceListener.bytesRead(totalBytesRead);
           if (state == State.BODY) {
             statsTraceCtx.inboundWireSize(totalBytesRead);
           }
