a311d47cce30b9795d182b44d115d7f4886fe44f
Eric Gribkoff <ericgribkoff@google.com>
2017-06-19 17:32:17 -0700

    add comment

--- core/src/test/java/io/grpc/internal/OverrideAuthorityNameResolverTest.java
+++ core/src/test/java/io/grpc/internal/OverrideAuthorityNameResolverTest.java
@@ -30,6 +30,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+/** Unit tests for {@link OverrideAuthorityNameResolverFactory}. */
 @RunWith(JUnit4.class)
 public class OverrideAuthorityNameResolverTest {
   @Test
