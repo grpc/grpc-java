1472b281e51c99583fd6f2c993d53e3956925821
Spencer Fang <spencerfang@google.com>
2017-07-21 12:58:02 -0700

    unused var

--- benchmarks/src/jmh/java/io/grpc/context/ReadMissingKeyBenchmark.java
+++ benchmarks/src/jmh/java/io/grpc/context/ReadMissingKeyBenchmark.java
@@ -25,7 +25,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.GroupThreads;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
