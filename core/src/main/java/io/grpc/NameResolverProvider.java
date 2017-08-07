d8b94fd81be9b29ced9ec87b11f1c6be98dfb3be
Eric Anderson <ejona@google.com>
2017-08-07 14:20:15 -0700

    core: Providers should consider Robolectric as Android
    null class loader means to use the bootstrap class loader, which would
    normally make sense. However, Robolectric tests run on OpenJDK and
    provide the Android environment as part of the application, so "Android"
    won't be present in the bootstrap class loader.

--- core/src/main/java/io/grpc/NameResolverProvider.java
+++ core/src/main/java/io/grpc/NameResolverProvider.java
@@ -124,7 +124,8 @@ public abstract class NameResolverProvider extends NameResolver.Factory {
 
   private static boolean isAndroid() {
     try {
-      Class.forName("android.app.Application", /*initialize=*/ false, null);
+      // Specify a class loader instead of null because we may be running under Robolectric
+      Class.forName("android.app.Application", /*initialize=*/ false, NameResolverProvider.class);
       return true;
     } catch (Exception e) {
       // If Application isn't loaded, it might as well not be Android.
