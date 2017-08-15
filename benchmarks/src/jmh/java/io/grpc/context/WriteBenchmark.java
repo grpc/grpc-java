64f007675ccd42c3a018a42e7c2469593c39a127
Spencer Fang <spencerfang@google.com>
2017-08-14 17:03:40 -0700

    Make WriteBenchmark only perform a simple mutation. What it is mutating may vary

--- benchmarks/src/jmh/java/io/grpc/context/WriteBenchmark.java
+++ benchmarks/src/jmh/java/io/grpc/context/WriteBenchmark.java
@@ -1,9 +1,6 @@
 package io.grpc.context;
 
-import com.google.common.collect.Lists;
 import io.grpc.Context;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -17,70 +14,36 @@ import org.openjdk.jmh.infra.Blackhole;
 
 /** Write benchmark. */
 public class WriteBenchmark {
-  private static final int NUM_KEYS = 8;
-
-  @State(Scope.Benchmark)
+  @State(Scope.Thread)
   public static class ContextState {
-    @Param({"true", "false"})
-    public boolean batched;
+    @Param({"0", "10", "25", "100"})
+    public int preexistingKeys;
 
-    List<Context.Key<Object>> keys = new ArrayList<Context.Key<Object>>();
-    List<List<Context.Key<Object>>> batchKeys = new ArrayList<List<Context.Key<Object>>>();
-    Context context = Context.ROOT;
+    Context.Key<Object> key1 = Context.key("key1");
+    Context.Key<Object> key2 = Context.key("key2");
+    Context.Key<Object> key3 = Context.key("key3");
+    Context.Key<Object> key4 = Context.key("key4");
+    Context context;
     Object val = new Object();
 
     @Setup
     public void setup() {
-      for (int i = 0; i < 8; i++) {
-        keys.add(Context.key("Key" + i));
+      context = Context.ROOT;
+      for (int i = 0; i < preexistingKeys; i++) {
+        context = context.withValue(Context.key("preexisting_key" + i), val);
       }
-      batchKeys = Lists.partition(keys, 4);
     }
   }
 
   @Benchmark
   @BenchmarkMode(Mode.SampleTime)
   @OutputTimeUnit(TimeUnit.NANOSECONDS)
-  public void testContextWrite(ContextState state, Blackhole bh) {
-    // always reset Context to avoid unbounded memory usage
-    state.context = Context.ROOT;
-    if (state.batched) {
-      for (Context.Key<Object> key : state.keys) {
-        state.context = state.context.withValue(key, state.val);
-      }
-    } else {
-      for (List<Context.Key<Object>> batch : state.batchKeys) {
-        state.context = batchPut(state.context, batch, state.val);
-      }
-    }
-    bh.consume(state.context);
-  }
-
-
-  /**
-   * Returns a new Context based off of `c` where all keys from `batch` are inserted with `val`.
-   */
-  private static Context batchPut(Context c, List<Context.Key<Object>> batch, Object val) {
-    switch (batch.size()) {
-      case 4:
-        return c.withValues(
-            batch.get(0), val,
-            batch.get(1), val,
-            batch.get(2), val,
-            batch.get(3), val);
-      case 3:
-        return c.withValues(
-            batch.get(0), val,
-            batch.get(1), val,
-            batch.get(2), val);
-      case 2:
-        return c.withValues(
-            batch.get(0), val,
-            batch.get(1), val);
-      case 1:
-        return c.withValue(batch.get(0), val);
-      default:
-        throw new AssertionError("Batch size should be between 1 and 4");
-    }
+  public void doWrite(ContextState state, Blackhole bh) {
+    Context result = state.context.withValues(
+        state.key1, state.val,
+        state.key2, state.val,
+        state.key3, state.val,
+        state.key4, state.val);
+    bh.consume(result);
   }
 }
