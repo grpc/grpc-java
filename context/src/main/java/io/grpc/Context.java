157cace0831491ede7a8b1ce144f4401f1f379fc
Spencer Fang <spencerfang@google.com>
2017-08-22 07:47:07 -0700

    unused import

--- context/src/main/java/io/grpc/Context.java
+++ context/src/main/java/io/grpc/Context.java
@@ -23,7 +23,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.Logger;
