3c7143c11c36078770223c092edd34f1e61d7157
Kun Zhang <zhangkun@google.com>
2017-09-19 14:12:48 -0700

    Document why we can't use Guava Collect. Actually copy the Guava version of the helper.

--- core/src/main/java/io/grpc/internal/GrpcUtil.java
+++ core/src/main/java/io/grpc/internal/GrpcUtil.java
@@ -44,6 +44,7 @@ import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.util.Collection;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -631,8 +632,22 @@ public final class GrpcUtil {
     }
   }
 
-  /** Checks whether the given item exists in the iterable. */
+  /**
+   * Checks whether the given item exists in the iterable.  This is copied from Guava Collect's
+   * {@code Iterables.contains()} because Guava Collect is not Android-friendly thus core can't
+   * depend on it.
+   */
   static <T> boolean iterableContains(Iterable<T> iterable, T item) {
+    if (iterable instanceof Collection) {
+      Collection<?> collection = (Collection<?>) iterable;
+      try {
+        return collection.contains(item);
+      } catch (NullPointerException e) {
+        return false;
+      } catch (ClassCastException e) {
+        return false;
+      }
+    }
     for (T i : iterable) {
       if (Objects.equal(i, item)) {
         return true;
