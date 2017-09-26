ce4b596d6fac6f6d45ab6fc771f254491bd5384f
Spencer Fang <spencerfang@google.com>
2017-09-26 14:22:23 -0700

    core: introduce CancellableContextApplier class

--- core/src/main/java/io/grpc/Contexts.java
+++ core/src/main/java/io/grpc/Contexts.java
@@ -17,7 +17,17 @@
 package io.grpc;
 
 import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeoutException;
+import javax.annotation.Nullable;
 
 /**
  * Utility methods for working with {@link Context}s in GRPC.
@@ -148,5 +158,164 @@ public final class Contexts {
       return Status.CANCELLED.withCause(cancellationCause);
     }
     return status.withCause(cancellationCause);
+  }
+
+  /**
+   * A utility for performing an operation inside of a
+   * {@link io.grpc.Context.CancellableContext CancellableContext}, and then cancelling the context
+   * after it is done.
+   *
+   * <p>Note: Because gRPC must be compatible with Java 1.6, this class does not support the
+   * try-with-resources idiom.
+   */
+  @ExperimentalApi("https://github.com/grpc/grpc-java/issues/3506")
+  public static CancellableContextApplier cancellableContextApplier(
+      Context.CancellableContext cancellableContext) {
+    return new CancellableContextApplier(cancellableContext);
+  }
+
+  public static class CancellableContextApplier {
+    private final Context.CancellableContext cancellableContext;
+
+    private CancellableContextApplier(Context.CancellableContext cancellableContext) {
+      this.cancellableContext = cancellableContext;
+    }
+
+    /**
+     * Runs the specified {@code runnable} in the specified {@code cancellableContext}, and
+     * cancels the context when the function returns or throws an exception.
+     */
+    public void run(Runnable runnable) {
+      try {
+        cancellableContext.run(runnable);
+      } finally {
+        cancellableContext.cancel(null);
+      }
+    }
+
+    /**
+     * Calls the specified {@code callable} in the specified {@code cancellableContext}, and
+     * cancels the context when the function returns or throws an exception.
+     */
+    public <T> T call(Callable<T> callable) throws Exception {
+      try {
+        return cancellableContext.call(callable);
+      } finally {
+        cancellableContext.cancel(null);
+      }
+    }
+
+    /**
+     * Calls the specified async callable in the specified {@code cancellableContext}. The
+     * context is cancelled when the asyncCallable's future completes or if the asyncCallable
+     * throws an exception. If asyncCallable threw an exception, it will be thrown from
+     * {@link ListenableFuture#get()}.
+     *
+     * @return A ListenableFuture whose listeners are notified after the context is cancelled. If
+     *     the returned ListenableFuture is cancelled, then {@code cancellableContext} will be
+     *     cancelled.
+     */
+    public <T> ListenableFuture<T> callAsync(final Callable<ListenableFuture<T>> asyncCallable) {
+      final SettableFuture<T> proxyFuture = SettableFuture.create();
+      try {
+        ListenableFuture<T> originalFuture = cancellableContext.call(asyncCallable);
+
+        // Ensure that the CancellableContext is cancelled before notifying proxyFuture
+        Futures.addCallback(
+            originalFuture,
+            new FutureCallback<T>() {
+              @Override
+              public void onSuccess(@Nullable T t) {
+                try {
+                  cancellableContext.cancel(null);
+                } finally {
+                  proxyFuture.set(t);
+                }
+              }
+
+              @Override
+              public void onFailure(Throwable throwable) {
+                try {
+                  cancellableContext.cancel(throwable);
+                } finally {
+                  // This will cause proxyFuture's onFailure to fire, and we will cancel the
+                  // CancellableContext a second time. This is fine because only the first call
+                  // has any effect.
+                  proxyFuture.setException(throwable);
+                }
+              }
+            },
+            MoreExecutors.directExecutor());
+
+        // If the user cancels proxyFuture, then cancel the CancellableContext
+        addCallback(
+            proxyFuture,
+            new FutureCallback<T>() {
+              @Override
+              public void onSuccess(@Nullable T result) {
+                // We should only enter here when originalFuture's onSuccess callback has fired
+                assert cancellableContext.isCancelled();
+              }
+
+              @Override
+              public void onFailure(Throwable t) {
+                // Fired when originalFuture fails, or if the user cancels proxyFuture
+                cancellableContext.cancel(t);
+              }
+            },
+            MoreExecutors.directExecutor());
+      } catch (Exception e) {
+        cancellableContext.cancel(e);
+        proxyFuture.setException(e);
+      }
+      return proxyFuture;
+    }
+  }
+
+  // com.google.common.util.concurrent.Futures is @Beta, so we can't use it in gRPC.
+  // copy-paste from guava
+  private static <V> void addCallback(final ListenableFuture<V> future,
+      final FutureCallback<? super V> callback, Executor executor) {
+    Preconditions.checkNotNull(callback);
+    Runnable callbackListener = new Runnable() {
+      @Override
+      public void run() {
+        final V value;
+        try {
+          value = getUninterruptibly(future);
+        } catch (ExecutionException e) {
+          callback.onFailure(e.getCause());
+          return;
+        } catch (RuntimeException e) {
+          callback.onFailure(e);
+          return;
+        } catch (Error e) {
+          callback.onFailure(e);
+          return;
+        }
+        callback.onSuccess(value);
+      }
+    };
+    future.addListener(callbackListener, executor);
+  }
+
+  // com.google.common.util.concurrent.Uninterruptables is @Beta, so we can't use it in gRPC.
+  // copy-paste from guava.
+  private static <V> V getUninterruptibly(Future<V> future)
+      throws ExecutionException {
+    boolean interrupted = false;
+    try {
+      while (true) {
+        try {
+          return future.get();
+        } catch (InterruptedException e) {
+          interrupted = true;
+        }
+      }
+    } finally {
+      if (interrupted) {
+        Thread.currentThread().interrupt();
+      }
+    }
   }
 }
--- core/src/test/java/io/grpc/ContextsTest.java
+++ core/src/test/java/io/grpc/ContextsTest.java
@@ -28,13 +28,21 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
 import io.grpc.internal.FakeClock;
 import io.grpc.internal.NoopServerCall;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -46,7 +54,8 @@ import org.junit.runners.JUnit4;
 public class ContextsTest {
   private static Context.Key<Object> contextKey = Context.key("key");
   /** For use in comparing context by reference. */
-  private Context uniqueContext = Context.ROOT.withValue(contextKey, new Object());
+  private Object contextValue = new Object();
+  private Context uniqueContext = Context.ROOT.withValue(contextKey, contextValue);
   @SuppressWarnings("unchecked")
   private ServerCall<Object, Object> call = new NoopServerCall<Object, Object>();
   private Metadata headers = new Metadata();
@@ -261,4 +270,257 @@ public class ContextsTest {
       assertEquals("context must not be null", npe.getMessage());
     }
   }
+
+  @Test
+  public void cancellableCtxApplier_runnable_enters() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    assertSame(Context.ROOT, Context.current());
+    Contexts.cancellableContextApplier(cancellableContext).run(new Runnable() {
+      @Override
+      public void run() {
+        verifyInCancellableContext();
+      }
+    });
+    assertSame(Context.ROOT, Context.current());
+  }
+
+  @Test
+  public void cancellableCtxApplier_callable_enters() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    assertSame(Context.ROOT, Context.current());
+    Contexts.cancellableContextApplier(cancellableContext).call(new Callable<Void>() {
+      @Override
+      public Void call() throws Exception {
+        verifyInCancellableContext();
+        return null;
+      }
+    });
+    assertSame(Context.ROOT, Context.current());
+  }
+
+  @Test
+  public void cancellableCtxApplier_asyncCallable_enters() {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    assertSame(Context.ROOT, Context.current());
+    @SuppressWarnings("unused")
+    ListenableFuture<Integer> ignored = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            verifyInCancellableContext();
+            return Futures.immediateFuture(1);
+          }
+        });
+    assertSame(Context.ROOT, Context.current());
+  }
+
+  @Test
+  public void cancellableCtxApplier_runnable() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    Contexts.cancellableContextApplier(cancellableContext).run(new Runnable() {
+      @Override
+      public void run() {
+      }
+    });
+    assertTrue(cancellableContext.isCancelled());
+  }
+
+  @Test
+  public void cancellableCtxApplier_callable() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    Contexts.cancellableContextApplier(cancellableContext).call(new Callable<Void>() {
+      @Override
+      public Void call() throws Exception {
+        return null;
+      }
+    });
+    assertTrue(cancellableContext.isCancelled());
+  }
+
+  @Test
+  public void cancellableCtxApplier_asyncCallable_already_success() {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    @SuppressWarnings("unused")
+    ListenableFuture<Integer> ignored = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            return Futures.immediateFuture(1);
+          }
+        });
+    assertTrue(cancellableContext.isCancelled());
+  }
+
+  @Test
+  public void cancellableCtxApplier_asyncCallable_already_fail() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    final FakeException fakeException = new FakeException();
+    ListenableFuture<Integer> future = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            return Futures.immediateFailedFuture(fakeException);
+          }
+        });
+    assertTrue(cancellableContext.isCancelled());
+    boolean exceptionCaught = false;
+    try {
+      future.get();
+    } catch (ExecutionException e) {
+      if (e.getCause() == fakeException) {
+        exceptionCaught = true;
+      }
+    }
+    assertTrue(exceptionCaught);
+  }
+
+  @Test
+  public void applier_asyncCallable_success() {
+    final SettableFuture<Integer> settable = SettableFuture.create();
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    ListenableFuture<Integer> ret = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            return settable;
+          }
+        });
+    assertFalse(ret.isDone());
+    assertFalse(cancellableContext.isCancelled());
+    settable.set(1);
+    assertTrue(cancellableContext.isCancelled());
+  }
+
+  @Test
+  public void applier_asyncCallable_fail() {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    final SettableFuture<Integer> settable = SettableFuture.create();
+    @SuppressWarnings("unused")
+    ListenableFuture<Integer> ignored = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            return settable;
+          }
+        });
+    assertFalse(cancellableContext.isCancelled());
+    settable.setException(new FakeException());
+    assertTrue(cancellableContext.isCancelled());
+  }
+
+  @Test
+  public void applier_asyncCallable_userCancel() {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    ListenableFuture<Integer> ret = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            return SettableFuture.create();
+          }
+        });
+    assertFalse(ret.isCancelled());
+    assertFalse(cancellableContext.isCancelled());
+    ret.cancel(true);
+    assertTrue(cancellableContext.isCancelled());
+  }
+
+  @Test
+  public void applier_asyncCallable_alreadyCancelledWhenListenersNotified() {
+    final Context.CancellableContext cancellableContext = makeCancellableContext();
+    ListenableFuture<Integer> ret = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            return SettableFuture.create();
+          }
+        });
+    assertFalse(ret.isCancelled());
+    assertFalse(cancellableContext.isCancelled());
+
+    final AtomicBoolean listenerFired = new AtomicBoolean();
+    ret.addListener(new Runnable() {
+      @Override
+      public void run() {
+        assertTrue(cancellableContext.isCancelled());
+        listenerFired.set(true);
+      }
+    }, MoreExecutors.directExecutor());
+    assertFalse(listenerFired.get());
+    ret.cancel(true);
+    assertTrue(listenerFired.get());
+  }
+
+  @Test(expected = FakeException.class)
+  public void applier_runnable_throws() {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    try {
+      Contexts.cancellableContextApplier(cancellableContext).run(
+          new Runnable() {
+            @Override
+            public void run() {
+              throw new FakeException();
+            }
+          });
+    } finally {
+      assertTrue(cancellableContext.isCancelled());
+    }
+  }
+
+  @Test(expected = FakeException.class)
+  public void applier_callable_throws() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    try {
+      Contexts.cancellableContextApplier(cancellableContext).call(
+          new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+              throw new FakeException();
+            }
+          }
+      );
+    } finally {
+      assertTrue(cancellableContext.isCancelled());
+    }
+  }
+
+  @Test
+  public void applier_asyncCallable_throws() throws Exception {
+    Context.CancellableContext cancellableContext = makeCancellableContext();
+    final FakeException fakeException = new FakeException();
+    ListenableFuture<Integer> future = Contexts.cancellableContextApplier(cancellableContext)
+        .callAsync(new Callable<ListenableFuture<Integer>>() {
+          @Override
+          public ListenableFuture<Integer> call() throws Exception {
+            throw fakeException;
+          }
+        });
+    assertTrue(cancellableContext.isCancelled());
+    boolean exceptionCaught = false;
+    try {
+      future.get();
+      // should not be reached
+      Assert.fail();
+    } catch (ExecutionException expected) {
+      if (expected.getCause() == fakeException) {
+        exceptionCaught = true;
+      }
+    }
+    assertTrue(exceptionCaught);
+  }
+
+  private Context.CancellableContext makeCancellableContext() {
+    return Context.current().withValue(contextKey, contextValue).withCancellation();
+  }
+
+  private void verifyInCancellableContext() {
+    // Context.current() always returns an uncancellable surrogate, so it doesn't make sense to
+    // check that the current context is a CancellableContext. So let's just make sure it looks
+    // equivalent to the context created by makeCancelllableContext.
+    assertSame(contextValue, contextKey.get());
+  }
+
+  /**
+   * A class that lets us have a bit of type safety when testing thrown exceptions.
+   */
+  private static class FakeException extends RuntimeException { }
 }
