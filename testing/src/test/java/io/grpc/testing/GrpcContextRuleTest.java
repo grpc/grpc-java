587409784c0d257f7b65e6d497003db793b92227
Ryan Michela <rmichela@salesforce.com>
2017-10-09 15:25:24 -0700

    GrpcContextRule tests

---
+++ testing/src/test/java/io/grpc/testing/GrpcContextRuleTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright, 1999-2017, SALESFORCE.com
+ * All Rights Reserved
+ * Company Confidential
+ */
+
+package io.grpc.testing;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.grpc.Context;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class GrpcContextRuleTest {
+  @Rule public final GrpcContextRule contextRule = new GrpcContextRule();
+
+  private static Context.Key<String> key = Context.key("key");
+
+  @Test
+  public void badAttach() {
+    Context ctx = Context.current();
+    // Never do this for real
+    ctx.withValue(key, "value").attach();
+
+    ctx = Context.current();
+    System.out.println(key.get());
+  }
+
+  @Test
+  public void verifyEmptyContext() {
+    Context ctx = Context.current();
+    assertThat(key.get(ctx)).isNull();
+  }
+}
