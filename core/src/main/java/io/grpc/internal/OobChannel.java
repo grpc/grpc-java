2fb559bce86f777e1851f1b17615633d291fdc71
dapengzhang0 <zdapeng@google.com>
2017-12-27 15:38:47 -0800

    core: remove circlar dependency on Internal

--- core/src/main/java/io/grpc/internal/OobChannel.java
+++ core/src/main/java/io/grpc/internal/OobChannel.java
@@ -28,6 +28,8 @@ import io.grpc.ConnectivityStateInfo;
 import io.grpc.Context;
 import io.grpc.EquivalentAddressGroup;
 import io.grpc.InternalChannelStats;
+import io.grpc.InternalInstrumented;
+import io.grpc.InternalLogId;
 import io.grpc.LoadBalancer;
 import io.grpc.LoadBalancer.PickResult;
 import io.grpc.LoadBalancer.PickSubchannelArgs;
@@ -51,13 +53,15 @@ import javax.annotation.concurrent.ThreadSafe;
  * to its own RPC needs.
  */
 @ThreadSafe
-final class OobChannel extends ManagedChannel {
+final class OobChannel extends ManagedChannel
+    implements InternalInstrumented<InternalChannelStats> {
   private static final Logger log = Logger.getLogger(OobChannel.class.getName());
 
   private InternalSubchannel subchannel;
   private AbstractSubchannel subchannelImpl;
   private SubchannelPicker subchannelPicker;
 
+  private final InternalLogId logId = InternalLogId.allocate(getClass().getName());
   private final String authority;
   private final DelayedClientTransport delayedTransport;
   private final ObjectPool<? extends Executor> executorPool;
@@ -174,6 +178,11 @@ final class OobChannel extends ManagedChannel {
   @Override
   public String authority() {
     return authority;
+  }
+
+  @Override
+  public InternalLogId getLogId() {
+    return logId;
   }
 
   @Override
