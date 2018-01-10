32ad28382ecb380a6904ba4d5abae9ab22e5adc8
Spencer Fang <spencerfang@google.com>
2018-01-10 15:50:36 -0800

    core: make NameResolverProvider use ServiceProviders
    make test reflection more straight forward NameResolverProvider

--- core/src/test/java/io/grpc/NameResolverProviderTest.java
+++ core/src/test/java/io/grpc/NameResolverProviderTest.java
@@ -22,20 +22,16 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.mock;
 
+import io.grpc.NameResolverProvider.HardcodedClasses;
 import io.grpc.internal.DnsNameResolverProvider;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URI;
-import java.net.URL;
 import java.util.Collections;
-import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.ServiceConfigurationError;
-import java.util.regex.Pattern;
+import java.util.Set;
+import java.util.concurrent.Callable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -43,48 +39,8 @@ import org.junit.runners.JUnit4;
 /** Unit tests for {@link NameResolverProvider}. */
 @RunWith(JUnit4.class)
 public class NameResolverProviderTest {
-  private final String serviceFile = "META-INF/services/io.grpc.NameResolverProvider";
   private final URI uri = URI.create("dns:///localhost");
   private final Attributes attributes = Attributes.EMPTY;
-
-  @Test
-  public void noProvider() {
-    ClassLoader ccl = Thread.currentThread().getContextClassLoader();
-    try {
-      ClassLoader cl = new ReplacingClassLoader(
-          getClass().getClassLoader(), serviceFile,
-          "io/grpc/NameResolverProviderTest-doesNotExist.txt");
-      Thread.currentThread().setContextClassLoader(cl);
-      List<NameResolverProvider> providers = NameResolverProvider.load(cl);
-      assertEquals(Collections.<NameResolverProvider>emptyList(), providers);
-    } finally {
-      Thread.currentThread().setContextClassLoader(ccl);
-    }
-  }
-
-  @Test
-  public void multipleProvider() {
-    ClassLoader cl = new ReplacingClassLoader(
-        getClass().getClassLoader(), serviceFile,
-        "io/grpc/NameResolverProviderTest-multipleProvider.txt");
-    List<NameResolverProvider> providers = NameResolverProvider.load(cl);
-    assertEquals(3, providers.size());
-    assertSame(Available7Provider.class, providers.get(0).getClass());
-    assertSame(Available5Provider.class, providers.get(1).getClass());
-    assertSame(Available0Provider.class, providers.get(2).getClass());
-    assertEquals("schemeAvailable7Provider",
-        NameResolverProvider.asFactory(providers).getDefaultScheme());
-    assertSame(Available7Provider.nameResolver,
-        NameResolverProvider.asFactory(providers).newNameResolver(uri, attributes));
-  }
-
-  @Test
-  public void unavailableProvider() {
-    ClassLoader cl = new ReplacingClassLoader(
-        getClass().getClassLoader(), serviceFile,
-        "io/grpc/NameResolverProviderTest-unavailableProvider.txt");
-    assertEquals(Collections.<NameResolverProvider>emptyList(), NameResolverProvider.load(cl));
-  }
 
   @Test
   public void getDefaultScheme_noProvider() {
@@ -133,117 +89,44 @@ public class NameResolverProviderTest {
   }
 
   @Test
-  public void getCandidatesViaHardCoded_triesToLoadClasses() throws Exception {
+  public void getClassesViaHardcoded_triesToLoadClasses() throws Exception {
     ClassLoader cl = getClass().getClassLoader();
-    final RuntimeException toThrow = new RuntimeException();
-    // Prevent DnsNameResolverProvider from being known
-    cl = new FilteringClassLoader(cl, serviceFile);
-    cl = new ClassLoader(cl) {
-      @Override
-      public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        if (name.startsWith("io.grpc.internal.")) {
-          throw toThrow;
-        } else {
-          return super.loadClass(name, resolve);
-        }
-      }
-    };
-    cl = new StaticTestingClassLoader(cl, Pattern.compile("io\\.grpc\\.[^.]*"));
-    ClassLoader ccl = Thread.currentThread().getContextClassLoader();
-    try {
-      Thread.currentThread().setContextClassLoader(cl);
-      invokeGetCandidatesViaHardCoded(cl);
-      fail("Expected exception");
-    } catch (RuntimeException ex) {
-      assertSame(toThrow, ex);
-    } finally {
-      Thread.currentThread().setContextClassLoader(ccl);
-    }
+    Set<String> classLoaderHistory = new HashSet<String>();
+    Iterator<?> classIter = ServiceProvidersTestUtil
+        .invokeCallable(
+            HardcodedClassesCallable.class.getName(), cl, classLoaderHistory);
+    assertTrue(classIter.hasNext());
+    Class<?> klass = (Class) classIter.next();
+    assertEquals(DnsNameResolverProvider.class.getName(), klass.getName());
+    assertFalse(classIter.hasNext());
+    assertTrue(classLoaderHistory.contains(DnsNameResolverProvider.class.getName()));
   }
 
   @Test
-  public void getCandidatesViaHardCoded_ignoresMissingClasses() throws Exception {
+  public void getClassesViaHardCoded_ignoresMissingClasses() throws Exception {
     ClassLoader cl = getClass().getClassLoader();
-    // Prevent DnsNameResolverProvider from being known
-    cl = new FilteringClassLoader(cl, serviceFile);
+    Set<String> classLoaderHistory = new HashSet<String>();
     cl = new ClassLoader(cl) {
       @Override
       public Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        if (name.startsWith("io.grpc.internal.")) {
+        if (name.equals(DnsNameResolverProvider.class.getName())) {
           throw new ClassNotFoundException();
         } else {
           return super.loadClass(name, resolve);
         }
       }
     };
-    cl = new StaticTestingClassLoader(cl, Pattern.compile("io\\.grpc\\.[^.]*"));
-    ClassLoader ccl = Thread.currentThread().getContextClassLoader();
-    try {
-      Thread.currentThread().setContextClassLoader(cl);
-      Iterable<?> i = invokeGetCandidatesViaHardCoded(cl);
-      assertFalse("Iterator should be empty", i.iterator().hasNext());
-    } finally {
-      Thread.currentThread().setContextClassLoader(ccl);
-    }
+    Iterator<?> classIter = ServiceProvidersTestUtil
+        .invokeCallable(
+            HardcodedClassesCallable.class.getName(), cl, classLoaderHistory);
+    assertFalse("Iterator should be empty", classIter.hasNext());
+    assertTrue(classLoaderHistory.contains(DnsNameResolverProvider.class.getName()));
   }
 
-  @Test
-  public void create_throwsErrorOnMisconfiguration() throws Exception {
-    class PrivateClass {}
-
-    try {
-      NameResolverProvider.create(PrivateClass.class);
-      fail("Expected exception");
-    } catch (ServiceConfigurationError e) {
-      assertTrue("Expected ClassCastException cause: " + e.getCause(),
-          e.getCause() instanceof ClassCastException);
-    }
-  }
-
-  private static Iterable<?> invokeGetCandidatesViaHardCoded(ClassLoader cl) throws Exception {
-    // An error before the invoke likely means there is a bug in the test
-    Class<?> klass = Class.forName(NameResolverProvider.class.getName(), true, cl);
-    Method getCandidatesViaHardCoded = klass.getMethod("getCandidatesViaHardCoded");
-    try {
-      return (Iterable<?>) getCandidatesViaHardCoded.invoke(null);
-    } catch (InvocationTargetException ex) {
-      if (ex.getCause() instanceof Exception) {
-        throw (Exception) ex.getCause();
-      }
-      throw ex;
-    }
-  }
-
-  private static class FilteringClassLoader extends ClassLoader {
-    private final String resource;
-
-    public FilteringClassLoader(ClassLoader parent, String resource) {
-      super(parent);
-      this.resource = resource;
-    }
-
+  public static final class HardcodedClassesCallable implements Callable<Iterator<Class<?>>> {
     @Override
-    public URL getResource(String name) {
-      if (resource.equals(name)) {
-        return null;
-      }
-      return super.getResource(name);
-    }
-
-    @Override
-    public Enumeration<URL> getResources(String name) throws IOException {
-      if (resource.equals(name)) {
-        return new Enumeration<URL>() {
-          @Override public boolean hasMoreElements() {
-            return false;
-          }
-
-          @Override public URL nextElement() {
-            throw new NoSuchElementException();
-          }
-        };
-      }
-      return super.getResources(name);
+    public Iterator<Class<?>> call() throws Exception {
+      return new HardcodedClasses().iterator();
     }
   }
 
@@ -274,42 +157,6 @@ public class NameResolverProviderTest {
     @Override
     public String getDefaultScheme() {
       return "scheme" + getClass().getSimpleName();
-    }
-  }
-
-  public static class Available0Provider extends BaseProvider {
-    public Available0Provider() {
-      super(true, 0);
-    }
-  }
-
-  public static class Available5Provider extends BaseProvider {
-    public Available5Provider() {
-      super(true, 5);
-    }
-  }
-
-  public static class Available7Provider extends BaseProvider {
-    public static final NameResolver nameResolver = mock(NameResolver.class);
-
-    public Available7Provider() {
-      super(true, 7);
-    }
-
-    @Override
-    public NameResolver newNameResolver(URI targetUri, Attributes params) {
-      return nameResolver;
-    }
-  }
-
-  public static class UnavailableProvider extends BaseProvider {
-    public UnavailableProvider() {
-      super(false, 10);
-    }
-
-    @Override
-    protected int priority() {
-      throw new RuntimeException("purposefully broken");
     }
   }
 }
