e61b5821ef5a04b177319ee6f471993ad97e7127
Spencer Fang <spencerfang@google.com>
2018-02-13 17:42:54 -0800

    revert unneeded, expect MVN_ARTIFACTS to be passed via rpc

--- buildscripts/kokoro/macos_artifacts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script assumes `set -e`. Removing it may lead to undefined behavior.
-set -exu -o pipefail
-
-BUILD_MVN_ARTIFACTS='true' bash "$(dirname $0)/unix.sh"
--- buildscripts/make_dependencies.sh
+++ buildscripts/make_dependencies.sh
@@ -4,11 +4,7 @@
 set -evux -o pipefail
 
 DOWNLOAD_DIR=/tmp/source
-# install here so we don't need sudo
 INSTALL_DIR="/tmp/protobuf-$PROTOBUF_VERSION/$(uname -s)-$(uname -p)"
-CONFIG_FLAGS="--prefix=$INSTALL_DIR ${CONFIG_FLAGS:-}"
-echo $CONFIG_FLAGS
-
 mkdir -p $DOWNLOAD_DIR
 
 # Start with a sane default
@@ -22,17 +18,15 @@ fi
 
 # Make protoc
 # Can't check for presence of directory as cache auto-creates it.
-if [[ -f ${INSTALL_DIR}/bin/protoc ]]; then
+if [ -f ${INSTALL_DIR}/bin/protoc ]; then
   echo "Not building protobuf. Already built"
 # TODO(ejona): swap to `brew install --devel protobuf` once it is up-to-date
 else
-  if [[ ! -f "$DOWNLOAD_DIR/protobof-${PROTOBUF_VERSION}" ]]; then
-    wget -O - https://github.com/google/protobuf/archive/v${PROTOBUF_VERSION}.tar.gz | tar xz -C $DOWNLOAD_DIR
-  fi
+  wget -O - https://github.com/google/protobuf/archive/v${PROTOBUF_VERSION}.tar.gz | tar xz -C $DOWNLOAD_DIR
   pushd $DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION}
   ./autogen.sh
-  ./configure $CONFIG_FLAGS
-  make clean # in case we are building again with different flags
+  # install here so we don't need sudo
+  ./configure --prefix="$INSTALL_DIR"
   make -j$NUM_CPU
   make install
   popd
