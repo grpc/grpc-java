c45f4fe788f8d07599c64337b8ea3380d76f6d7f
Eric Anderson <ejona@google.com>
2018-03-07 12:52:35 -0800

    Improve variable name

--- interop-testing/src/main/java/io/grpc/testing/integration/AbstractInteropTest.java
+++ interop-testing/src/main/java/io/grpc/testing/integration/AbstractInteropTest.java
@@ -703,12 +703,12 @@ public abstract class AbstractInteropTest {
     for (int i = 0; i < requests.size(); i++) {
       assertNull(queue.peek());
       requestObserver.onNext(requests.get(i));
-      Object o = queue.poll(operationTimeoutMillis(), TimeUnit.MILLISECONDS);
-      assertNotNull("Timed out waiting for response", o);
-      if (o instanceof Throwable) {
-        throw new AssertionError((Throwable) o);
+      Object actualResponse = queue.poll(operationTimeoutMillis(), TimeUnit.MILLISECONDS);
+      assertNotNull("Timed out waiting for response", actualResponse);
+      if (actualResponse instanceof Throwable) {
+        throw new AssertionError((Throwable) actualResponse);
       }
-      assertEquals(goldenResponses.get(i), o);
+      assertEquals(goldenResponses.get(i), actualResponse);
     }
     requestObserver.onCompleted();
     assertEquals("Completed", queue.poll(operationTimeoutMillis(), TimeUnit.MILLISECONDS));
