3d01da87a56575a4ead76d176df6e3e51cf013d4
Spencer Fang <spencerfang@google.com>
2018-04-23 10:32:15 -0700

    buildscripts: workaround su as 'theuser' issue in grpc-java-releasing
    Workaround for this issue observed on Docker 17.12:
    
    ```
    ~/git/protobuf/protoc-artifacts$ docker build -t protoc-artifacts .
    
    ~/git/grpc-java/buildscripts/grpc-java-releasing$ docker build -t grpc-java-releasing .
    
    spencerfang@spencerfang:~/git/grpc-java/buildscripts$ ./run_in_docker.sh whoami
    su: /bin/bash: Resource temporarily unavailable
    ```
    
    Because the UID and GID are baked into the image, I append -$(whoami)
    to the docker image name.

--- buildscripts/run_in_docker.sh
+++ buildscripts/run_in_docker.sh
@@ -10,31 +10,14 @@ quote() {
   done
 }
 
-if [[ "${1:-}" != "in-docker" ]]; then
-  readonly grpc_java_dir="$(dirname $(readlink -f "$0"))/.."
-  exec docker run -it --rm=true -v "${grpc_java_dir}:/grpc-java" -w /grpc-java \
-    grpc-java-releasing \
-    ./buildscripts/run_in_docker.sh in-docker "$(id -u)" "$(id -g)" "$@"
-fi
+readonly GRPC_JAVA_DIR="$(dirname $(readlink -f "$0"))/.."
+readonly USER=$(whoami)
+cd $GRPC_JAVA_DIR/buildscripts/
+docker build -t grpc-java-releasing-$USER grpc-java-releasing \
+  --build-arg UID=$(id -u) --build-arg GID=$(id -g) \
+  --build-arg USERNAME=$USER
 
-## In Docker
-
-shift
-
-readonly swap_uid="$1"
-readonly swap_gid="$2"
-shift 2
-
-# Java uses NSS to determine the user's home. If that fails it uses '?' in the
-# current directory. So we need to set up the user's home in /etc/passwd.
-# If this wasn't the case, we could have passed -u to docker run and avoided
-# this script inside the container. JAVA_TOOL_OPTIONS is okay, but is noisy.
-groupadd thegroup -g "$swap_gid"
-useradd theuser -u "$swap_uid" -g "$swap_gid" -m
-if [[ "$#" -eq 0 ]]; then
-  exec su theuser
-else
-  # runuser is too old in the container to support the -u flag; if it did, we'd
-  # be able to remove the 'quote' function.
-  exec su theuser -c "$(quote "$@")"
-fi
+exec docker run -it --user $USER --rm=true -v \
+ "${GRPC_JAVA_DIR}:/grpc-java" -w /grpc-java \
+ grpc-java-releasing-$USER \
+ /bin/bash -c "$(quote "$@")"
