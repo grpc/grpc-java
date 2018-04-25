69f1738a8b54a8932ac10ef02d076763f1290720
Spencer Fang <spencerfang@google.com>
2018-04-25 16:32:41 -0700

    do not use -it

--- buildscripts/run_in_docker.sh
+++ buildscripts/run_in_docker.sh
@@ -11,6 +11,7 @@ quote() {
 }
 
 readonly grpc_java_dir="$(dirname $(readlink -f "$0"))/.."
-exec docker run -it --rm=true -v "${grpc_java_dir}:/grpc-java" -w /grpc-java \
+# We do not use -it because the input device in kokoro is not a TTY
+exec docker run --rm=true -v "${grpc_java_dir}:/grpc-java" -w /grpc-java \
   grpc-java-releasing \
   bash -c "$(quote "$@"); chown -R $(id -u):$(id -g) /grpc-java"
