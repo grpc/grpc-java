2c96ab2e2228ec216ee3e6c24130090acc471feb
Spencer Fang <spencerfang@google.com>
2018-04-26 10:17:14 -0700

    upload to sonatype in script

--- buildscripts/kokoro/sonatype_upload.sh
+++ buildscripts/kokoro/sonatype_upload.sh
@@ -11,3 +11,10 @@ readonly GRPC_JAVA_DIR=$(cd $(dirname $0)/../.. && pwd)
 
 echo "all the artifacts should be here..."
 find $KOKORO_GFILE_DIR
+
+mkdir ~/.config/
+gsutil cp gs://grpc-testing-secrets/sonatype_credentials/sonatype-upload ~/.config/sonatype-upload
+
+STAGING_REPO=a93898609ef848
+find $KOKORO_GFILE_DIR -name 'mvn-artifacts' -exec \
+  $GRPC_JAVA_DIR/buildcripts/sonatype_upload.sh $STAGING_REPO {} \;
