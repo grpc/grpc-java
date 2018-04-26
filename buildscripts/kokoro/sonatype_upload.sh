41fe1b895476725341a9c2215c4ff1d305118ce8
Spencer Fang <spencerfang@google.com>
2018-04-26 12:31:24 -0700

    sign all files

--- buildscripts/kokoro/sonatype_upload.sh
+++ buildscripts/kokoro/sonatype_upload.sh
@@ -15,6 +15,15 @@ find $KOKORO_GFILE_DIR
 mkdir -p ~/.config/
 gsutil cp gs://grpc-testing-secrets/sonatype_credentials/sonatype-upload ~/.config/sonatype-upload
 
+mkdir -p ~/java_signing/
+gsutil cp -r gs://grpc-testing-secrets/java_signing/ ~/
+gpg --batch  --import ~/java_signing/grpc-java-team-sonatype.asc
+
+set +x
+find ~/java_signing -type f -exec \
+  gpg --batch --passphrase-file ~/java_signing/passphrase --detach-sign -o {}.asc {} \;
+set -x
+
 STAGING_REPO=a93898609ef848
 find $KOKORO_GFILE_DIR -name 'mvn-artifacts' -type d -exec \
   $GRPC_JAVA_DIR/buildcripts/sonatype-upload-util.sh \
