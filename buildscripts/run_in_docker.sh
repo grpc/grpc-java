ae747b7fe3b6ff8c01138e588a4fb39bd59bd02e
Spencer Fang <spencerfang@google.com>
2018-04-27 21:27:59 -0700

    run_in_docker.sh: use chmod to fix permisisons, do not set up user
    This script is still causing problems on my workstation. I run into
    thread creation limits when running as the new user. Let's just run as
    root and fix permissions upon exit.

--- buildscripts/run_in_docker.sh
+++ buildscripts/run_in_docker.sh
@@ -10,31 +10,10 @@ quote() {
   done
 }
 
-if [[ "${1:-}" != "in-docker" ]]; then
-  readonly grpc_java_dir="$(dirname $(readlink -f "$0"))/.."
-  exec docker run -it --rm=true -v "${grpc_java_dir}:/grpc-java" -w /grpc-java \
-    grpc-java-releasing \
-    ./buildscripts/run_in_docker.sh in-docker "$(id -u)" "$(id -g)" "$@"
-fi
-
-## In Docker
-
-shift
-
-readonly swap_uid="$1"
-readonly swap_gid="$2"
-shift 2
-
-# Java uses NSS to determine the user's home. If that fails it uses '?' in the
-# current directory. So we need to set up the user's home in /etc/passwd.
-# If this wasn't the case, we could have passed -u to docker run and avoided
-# this script inside the container. JAVA_TOOL_OPTIONS is okay, but is noisy.
-groupadd thegroup -g "$swap_gid"
-useradd theuser -u "$swap_uid" -g "$swap_gid" -m
-if [[ "$#" -eq 0 ]]; then
-  exec su theuser
-else
-  # runuser is too old in the container to support the -u flag; if it did, we'd
-  # be able to remove the 'quote' function.
-  exec su theuser -c "$(quote "$@")"
-fi
+readonly grpc_java_dir="$(dirname "$(readlink -f "$0")")/.."
+# We do not use -it because the input device in kokoro is not a TTY.
+# Use a trap function to fix file permissions upon exit, without affecting
+# the original exit code.
+exec docker run --rm=true -v "${grpc_java_dir}":/grpc-java -w /grpc-java \
+  grpc-java-releasing \
+  bash -c "function fixFiles() { chown -R "$(id -u)":"$(id -g)" /grpc-java; }; trap fixFiles EXIT; $(quote "$@")"
