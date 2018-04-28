bcba18c7bb5bbc6c76056100885a920274c6062c
Spencer Fang <spencerfang@google.com>
2018-04-27 21:30:27 -0700

    linux_artifact builds 32 and 64 artifacts using unix.sh
    - modify unix.sh to allow skipping tests the second time around
    - modify unix.sh to specify generating all artifacts or only protoc
      plugin

--- buildscripts/kokoro/linux_artifacts.sh
+++ buildscripts/kokoro/linux_artifacts.sh
@@ -5,6 +5,19 @@ if [[ -f /VERSION ]]; then
   cat /VERSION
 fi
 
-readonly GRPC_JAVA_DIR=$(cd $(dirname $0)/../.. && pwd)
+readonly GRPC_JAVA_DIR="$(cd "$(dirname "$0")"/../.. && pwd)"
 
-# A place holder at the moment
+rm -rf /tmp/source_head/protobuf
+mkdir -p /tmp/source_head/protobuf
+git clone https://github.com/google/protobuf.git /tmp/source_head/protobuf
+
+cd /tmp/source_head/protobuf
+git fetch origin
+git checkout origin/master
+docker build -t protoc-artifacts protoc-artifacts
+
+cd "$GRPC_JAVA_DIR"/buildscripts/
+
+docker build -t grpc-java-releasing grpc-java-releasing
+
+"$GRPC_JAVA_DIR"/buildscripts/run_in_docker.sh /grpc-java/buildscripts/build_artifacts_in_docker.sh
