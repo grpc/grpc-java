2774e24677480849c6ce0f25aea31f9e08722fe5
Spencer Fang <spencerfang@google.com>
2018-05-03 09:26:45 -0700

    pin protobuf to 92898e9e, remove unneeded cd

--- buildscripts/kokoro/linux_artifacts.sh
+++ buildscripts/kokoro/linux_artifacts.sh
@@ -7,18 +7,13 @@ fi
 
 readonly GRPC_JAVA_DIR="$(cd "$(dirname "$0")"/../.. && pwd)"
 
-rm -rf /tmp/source_head/protobuf
-mkdir -p /tmp/source_head/protobuf
-# Clone from latest until protobuf makes a release with this fix:
+rm -rf /tmp/protobuf/
+mkdir -p /tmp/protobuf/
+# Download an unreleased SHA because we need this fix:
 # https://github.com/google/protobuf/pull/4447
-git clone https://github.com/google/protobuf.git /tmp/source_head/protobuf
+wget -O - https://github.com/google/protobuf/archive/92898e9e.tar.gz | tar xz -C /tmp/protobuf/
 
-cd /tmp/source_head/protobuf
-git fetch origin
-git checkout origin/master
-docker build -t protoc-artifacts protoc-artifacts
-
-cd "$GRPC_JAVA_DIR"/buildscripts/
+docker build -t protoc-artifacts /tmp/protobuf/protobuf-92898e9e9cb2f1c006fcc5099c9c96eafce63dc8/protoc-artifacts
 
 docker build -t grpc-java-releasing "$GRPC_JAVA_DIR"/buildscripts/grpc-java-releasing
 
