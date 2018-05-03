59af77bc1d4fd70807a25910d4cded087201fa16
Spencer Fang <spencerfang@google.com>
2018-05-03 10:27:29 -0700

    remove trap quotes, add -it if tty, make cache buster more concise

--- buildscripts/run_in_docker.sh
+++ buildscripts/run_in_docker.sh
@@ -11,9 +11,14 @@ quote() {
 }
 
 readonly grpc_java_dir="$(dirname "$(readlink -f "$0")")/.."
-# We do not use -it because the input device in kokoro is not a TTY.
+if [[ -t 0 ]]; then
+  DOCKER_ARGS="-it"
+else
+  # The input device on kokoro is not a TTY, so -it does not work.
+  DOCKER_ARGS=
+fi
 # Use a trap function to fix file permissions upon exit, without affecting
 # the original exit code.
-exec docker run --rm=true -v "${grpc_java_dir}":/grpc-java -w /grpc-java \
+exec docker run "$DOCKER_ARGS" --rm=true -v "${grpc_java_dir}":/grpc-java -w /grpc-java \
   grpc-java-releasing \
-  bash -c "function fixFiles() { chown -R "$(id -u)":"$(id -g)" /grpc-java; }; trap fixFiles EXIT; $(quote "$@")"
+  bash -c "function fixFiles() { chown -R $(id -u):$(id -g) /grpc-java; }; trap fixFiles EXIT; $(quote "$@")"
