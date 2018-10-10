8bba75605e6dfcdb11c39674090b2c49632a57e1
Kun Zhang <zhangkun@google.com>
2018-10-09 18:40:45 -0700

    services: implement Health.Watch

--- services/src/main/java/io/grpc/services/HealthServiceImpl.java
+++ services/src/main/java/io/grpc/services/HealthServiceImpl.java
@@ -16,6 +16,12 @@
 
 package io.grpc.services;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.MoreExecutors;
+import io.grpc.Context.CancellationListener;
+import io.grpc.Context;
 import io.grpc.Status;
 import io.grpc.StatusException;
 import io.grpc.health.v1.HealthCheckRequest;
@@ -23,23 +29,27 @@ import io.grpc.health.v1.HealthCheckResponse;
 import io.grpc.health.v1.HealthCheckResponse.ServingStatus;
 import io.grpc.health.v1.HealthGrpc;
 import io.grpc.stub.StreamObserver;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
-
+import javax.annotation.concurrent.GuardedBy;
 
 final class HealthServiceImpl extends HealthGrpc.HealthImplBase {
 
   /* Due to the latency of rpc calls, synchronization of the map does not help with consistency.
-   * However, need use ConcurrentHashMap to prevent the possible race condition of concurrently
-   * putting two keys with a colliding hashCode into the map.*/
-  private final Map<String, ServingStatus> statusMap
-      = new ConcurrentHashMap<String, ServingStatus>();
+   * However, need use ConcurrentHashMap to allow concurrent reading by check() */
+  private final ConcurrentHashMap<String, ServingStatus> statusMap =
+      new ConcurrentHashMap<String, ServingStatus>();
+
+  private final Object watchLock = new Object();
+
+  @GuardedBy("watchLock")
+  private final Multimap<String, StreamObserver<HealthCheckResponse>> watchers =
+      HashMultimap.create();
 
   @Override
   public void check(HealthCheckRequest request,
       StreamObserver<HealthCheckResponse> responseObserver) {
-    ServingStatus status = getStatus(request.getService());
+    ServingStatus status = statusMap.get(request.getService());
     if (status == null) {
       responseObserver.onError(new StatusException(
           Status.NOT_FOUND.withDescription("unknown service " + request.getService())));
@@ -50,16 +60,63 @@ final class HealthServiceImpl extends HealthGrpc.HealthImplBase {
     }
   }
 
-  void setStatus(String service, ServingStatus status) {
-    statusMap.put(service, status);
+  @Override
+  public void watch(HealthCheckRequest request,
+      final StreamObserver<HealthCheckResponse> responseObserver) {
+    final String service = request.getService();
+    synchronized (watchLock) {
+      ServingStatus status = statusMap.get(service);
+      responseObserver.onNext(getResponseForWatch(status));
+      watchers.put(service, responseObserver);
+    }
+    Context.current().addListener(
+        new CancellationListener() {
+          @Override
+          // Called when the client has closed the stream
+          public void cancelled(Context context) {
+            synchronized (watchLock) {
+              watchers.remove(service, responseObserver);
+            }
+          }
+        },
+        MoreExecutors.directExecutor());
   }
 
-  @Nullable
-  ServingStatus getStatus(String service) {
-    return statusMap.get(service);
+  void setStatus(String service, ServingStatus status) {
+    synchronized (watchLock) {
+      ServingStatus prevStatus = statusMap.put(service, status);
+      if (prevStatus != status) {
+        notifyWatchers(service, status);
+      }
+    }
   }
 
   void clearStatus(String service) {
-    statusMap.remove(service);
+    synchronized (watchLock) {
+      ServingStatus prevStatus = statusMap.remove(service);
+      if (prevStatus != null) {
+        notifyWatchers(service, null);
+      }
+    }
+  }
+
+  @VisibleForTesting
+  int numWatchersForTest(String service) {
+    synchronized (watchLock) {
+      return watchers.get(service).size();
+    }
+  }
+
+  @GuardedBy("watchLock")
+  private void notifyWatchers(String service, @Nullable ServingStatus status) {
+    HealthCheckResponse response = getResponseForWatch(status);
+    for (StreamObserver<HealthCheckResponse> responseObserver : watchers.get(service)) {
+      responseObserver.onNext(response);
+    }
+  }
+
+  private static HealthCheckResponse getResponseForWatch(@Nullable ServingStatus recordedStatus) {
+    return HealthCheckResponse.newBuilder().setStatus(
+        recordedStatus == null ? ServingStatus.SERVICE_UNKNOWN : recordedStatus).build();
   }
 }
