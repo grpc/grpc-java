5a965f4349ea680672eb0ed8d8d97fd3ad105e73
David Wu <ami.david.w@gmail.com>
2019-02-27 17:24:30 -0500

    Provide two examples: PerMethod and AllMethods

--- examples/src/main/java/io/grpc/examples/experimental/CompressionServerInterceptor.java
+++ examples/src/main/java/io/grpc/examples/experimental/CompressionServerInterceptor.java
@@ -11,9 +11,7 @@ public class CompressionServerInterceptor implements ServerInterceptor {
     @Override
     public <ReqT, RespT> Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers,
         ServerCallHandler<ReqT, RespT> next) {
-
         call.setCompression("gzip");
-
         return next.startCall(call, headers);
     }
 
