f65f45fefe59d8c74cdb10a8125021ec026b136c
Carl Mastrangelo <notcarl@google.com>
2019-03-07 10:32:11 -0800

    grpclb: fix lint

--- grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
+++ grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
@@ -1682,7 +1682,6 @@ public class GrpclbLoadBalancerTest {
     deliverResolvedAddresses(grpclbResolutionList, grpclbResolutionAttrs);
 
     assertEquals(1, fakeOobChannels.size());
-    ManagedChannel oobChannel = fakeOobChannels.poll();
     verify(mockLbService).balanceLoad(lbResponseObserverCaptor.capture());
     StreamObserver<LoadBalanceResponse> lbResponseObserver = lbResponseObserverCaptor.getValue();
     assertEquals(1, lbRequestObservers.size());
@@ -1809,7 +1808,6 @@ public class GrpclbLoadBalancerTest {
 
     // Attempted to connect to balancer
     assertEquals(1, fakeOobChannels.size());
-    ManagedChannel oobChannel = fakeOobChannels.poll();
     verify(mockLbService).balanceLoad(lbResponseObserverCaptor.capture());
     StreamObserver<LoadBalanceResponse> lbResponseObserver = lbResponseObserverCaptor.getValue();
     assertEquals(1, lbRequestObservers.size());
@@ -1930,7 +1928,6 @@ public class GrpclbLoadBalancerTest {
 
     // A new LB stream is created
     assertEquals(1, fakeOobChannels.size());
-    oobChannel = fakeOobChannels.poll();
     verify(mockLbService, times(2)).balanceLoad(lbResponseObserverCaptor.capture());
     lbResponseObserver = lbResponseObserverCaptor.getValue();
     assertEquals(1, lbRequestObservers.size());
