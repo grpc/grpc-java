de25b9f743f350d17639275ce79df1ee71d2bcbf
Eric Anderson <ejona@google.com>
2019-03-13 12:15:11 -0700

    Removed old comment; added missing --tmpdir

--- buildscripts/build_docker.sh
+++ buildscripts/build_docker.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-readonly proto_dir="$(mktemp -d protobuf.XXXXXX)"
-# Download an unreleased SHA to include TLS 1.2 support:
-# https://github.com/google/protobuf/pull/4879
+readonly proto_dir="$(mktemp -d --tmpdir protobuf.XXXXXX)"
 wget -O - https://github.com/google/protobuf/archive/v3.7.0.tar.gz | tar xz -C "$proto_dir"
 
 docker build -t protoc-artifacts "$proto_dir"/protobuf-3.7.0/protoc-artifacts
