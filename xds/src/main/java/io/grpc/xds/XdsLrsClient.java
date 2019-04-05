ad0a8c96cc7dd9cc3aad9b87a5f90d9db91b1ac5
Chengyuan Zhang <chengyuanzhang@google.com>
2019-04-05 10:48:02 -0700

    Implement LRS client with backoff. No load data is invovled yet, only for load reporting interval updates. Unit test with load report interval update and streamClosed retry.

---
+++ xds/src/main/java/io/grpc/xds/XdsLrsClient.java
@@ -0,0 +1,303 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Stopwatch;
+import com.google.protobuf.Duration;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import io.envoyproxy.envoy.api.v2.core.Node;
+import io.envoyproxy.envoy.api.v2.endpoint.ClusterStats;
+import io.envoyproxy.envoy.service.load_stats.v2.LoadReportingServiceGrpc;
+import io.envoyproxy.envoy.service.load_stats.v2.LoadStatsRequest;
+import io.envoyproxy.envoy.service.load_stats.v2.LoadStatsResponse;
+import io.grpc.ChannelLogger;
+import io.grpc.ChannelLogger.ChannelLogLevel;
+import io.grpc.LoadBalancer.Helper;
+import io.grpc.ManagedChannel;
+import io.grpc.Status;
+import io.grpc.SynchronizationContext;
+import io.grpc.SynchronizationContext.ScheduledHandle;
+import io.grpc.internal.BackoffPolicy;
+import io.grpc.stub.StreamObserver;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+
+/**
+ * Client of XDS load reporting service. It should be run in some serialized context.
+ */
+@NotThreadSafe
+class XdsLrsClient {
+
+  @VisibleForTesting
+  static final String TRAFFICDIRECTOR_GRPC_HOSTNAME
+      = "com.googleapis.trafficdirector.grpc_hostname";
+  private final String serviceName;
+  private final ManagedChannel channel;
+  private final Helper helper;
+  private final SynchronizationContext syncContext;
+  private final ScheduledExecutorService timerService;
+  private final Stopwatch stopwatch;
+  private final ChannelLogger logger;
+  private final BackoffPolicy.Provider backoffPolicyProvider;
+
+  @Nullable
+  private BackoffPolicy lrsRpcRetryPolicy;
+  @Nullable
+  private ScheduledHandle lrsRpcRetryTimer;
+
+  @Nullable
+  private LrsStream lrsStream;
+  private List<String> serviceList;  // list of services of interest to be reported
+
+  XdsLrsClient(ManagedChannel channel, Helper helper, Stopwatch stopwatch,
+      BackoffPolicy.Provider backoffPolicyProvider) {
+    this.channel = checkNotNull(channel, "channel");
+    this.helper = checkNotNull(helper, "helper");
+    this.serviceName = checkNotNull(helper.getAuthority(), "serviceName");
+    this.syncContext = checkNotNull(helper.getSynchronizationContext(), "syncContext");
+    this.stopwatch = checkNotNull(stopwatch, "stopwatch");
+    this.logger = checkNotNull(helper.getChannelLogger(), "logger");
+    this.timerService = checkNotNull(helper.getScheduledExecutorService(), "timeService");
+    this.backoffPolicyProvider = checkNotNull(backoffPolicyProvider, "backoffPolicyProvider");
+  }
+
+  static long durationToNum(Duration duration, TimeUnit unit) {
+    return unit.convert(duration.getSeconds() * 1000_000_000L + duration.getNanos(),
+        TimeUnit.NANOSECONDS);
+  }
+
+  void startLrsRpc() {
+    checkState(lrsStream == null, "previous lbStream has not been cleared yet");
+    LoadReportingServiceGrpc.LoadReportingServiceStub stub
+        = LoadReportingServiceGrpc.newStub(channel);
+    lrsStream = new LrsStream(stub);
+    lrsStream.start();
+    stopwatch.reset().start();
+
+    LoadStatsRequest initRequest =
+        LoadStatsRequest.newBuilder()
+            .setNode(Node.newBuilder()
+                .setMetadata(Struct.newBuilder()
+                    .putFields(
+                        TRAFFICDIRECTOR_GRPC_HOSTNAME,
+                        Value.newBuilder().setStringValue(serviceName).build())))
+            .build();
+    try {
+      lrsStream.prevLoadReportNano = stopwatch.elapsed(TimeUnit.NANOSECONDS);
+      lrsStream.lrsRequestWriter.onNext(initRequest);
+    } catch (Exception e) {
+      lrsStream.close(e);
+    }
+  }
+
+  void shutdownLrsRpc() {
+    if (lrsStream != null) {
+      lrsStream.close(null);
+    }
+  }
+
+  @VisibleForTesting
+  static class LoadReportingTask implements Runnable {
+    private final LrsStream stream;
+
+    LoadReportingTask(LrsStream stream) {
+      this.stream = stream;
+    }
+
+    @Override
+    public void run() {
+      stream.loadReportTimer = null;
+      stream.sendLoadReport();
+    }
+  }
+
+  @VisibleForTesting
+  class LrsRpcRetryTask implements Runnable {
+
+    @Override
+    public void run() {
+      startLrsRpc();
+    }
+  }
+
+  @VisibleForTesting
+  static Duration numToDuration(long duration, TimeUnit unit) {
+    long time = unit.toNanos(duration);
+    long sec = time / 1_000_000_000L;
+    int nanos = (int) (time % 1_000_000_000L);
+    return Duration.newBuilder().setSeconds(sec).setNanos(nanos).build();
+  }
+
+  private class LrsStream implements StreamObserver<LoadStatsResponse> {
+
+    final LoadReportingServiceGrpc.LoadReportingServiceStub stub;
+    StreamObserver<LoadStatsRequest> lrsRequestWriter;
+    boolean initialResponseReceived;
+    boolean closed;
+    long prevLoadReportNano;
+    long loadReportIntervalNano = -1;
+    ScheduledHandle loadReportTimer;
+
+    LrsStream(LoadReportingServiceGrpc.LoadReportingServiceStub stub) {
+      this.stub = checkNotNull(stub, "stub");
+    }
+
+    void start() {
+      lrsRequestWriter = stub.withWaitForReady().streamLoadStats(this);
+    }
+
+    @Override
+    public void onNext(final LoadStatsResponse response) {
+      syncContext.execute(new Runnable() {
+        @Override
+        public void run() {
+          handleResponse(response);
+        }
+      });
+    }
+
+    @Override
+    public void onError(final Throwable t) {
+      syncContext.execute(new Runnable() {
+        @Override
+        public void run() {
+          handleStreamClosed(Status.fromThrowable(t)
+              .augmentDescription("Stream to XDS management server had an error"));
+        }
+      });
+    }
+
+    @Override
+    public void onCompleted() {
+      syncContext.execute(new Runnable() {
+        @Override
+        public void run() {
+          handleStreamClosed(
+              Status.UNAVAILABLE.withDescription("Stream to XDS management server was closed"));
+        }
+      });
+    }
+
+    private void sendLoadReport() {
+      long interval = stopwatch.elapsed(TimeUnit.NANOSECONDS) - prevLoadReportNano;
+      prevLoadReportNano = stopwatch.elapsed(TimeUnit.NANOSECONDS);
+      // TODO: get load report and send to management server
+      lrsRequestWriter.onNext(LoadStatsRequest.newBuilder()
+          .setNode(Node.newBuilder()
+              .setMetadata(Struct.newBuilder()
+                  .putFields(
+                      TRAFFICDIRECTOR_GRPC_HOSTNAME,
+                      Value.newBuilder().setStringValue(serviceName).build())))
+          .addClusterStats(ClusterStats.newBuilder().setLoadReportInterval(
+              numToDuration(interval, TimeUnit.NANOSECONDS)))
+          .build());
+      scheduleNextLoadReport();
+    }
+
+    private void scheduleNextLoadReport() {
+      if (loadReportIntervalNano > 0) {
+        // Cancel pending load report and reschedule with updated load reporting interval.
+        if (loadReportTimer != null && loadReportTimer.isPending()) {
+          loadReportTimer.cancel();
+        }
+        loadReportTimer = syncContext.schedule(
+            new LoadReportingTask(this), loadReportIntervalNano, TimeUnit.NANOSECONDS,
+            timerService);
+      }
+    }
+
+    private void handleResponse(LoadStatsResponse response) {
+      if (closed) {
+        return;
+      }
+      logger.log(ChannelLogLevel.DEBUG, "Got an LRS response: {0}", response);
+
+      if (!initialResponseReceived) {
+        initialResponseReceived = true;
+      }
+      loadReportIntervalNano
+          = durationToNum(response.getLoadReportingInterval(), TimeUnit.NANOSECONDS);
+      serviceList = Collections.unmodifiableList(response.getClustersList());
+      scheduleNextLoadReport();
+    }
+
+    private void handleStreamClosed(Status status) {
+      checkArgument(!status.isOk(), "unexpected OK status");
+      if (closed) {
+        return;
+      }
+      closed = true;
+      cleanUp();
+
+      long delayNanos = 0;
+      if (initialResponseReceived || lrsRpcRetryPolicy == null) {
+        // Reset the backoff sequence if balancer has sent the initial response, or backoff sequence
+        // has never been initialized.
+        lrsRpcRetryPolicy = backoffPolicyProvider.get();
+      }
+      // Backoff only when balancer wasn't working previously.
+      if (!initialResponseReceived) {
+        // The back-off policy determines the interval between consecutive RPC upstarts, thus the
+        // actual delay may be smaller than the value from the back-off policy, or even negative,
+        // depending how much time was spent in the previous RPC.
+        delayNanos =
+            lrsRpcRetryPolicy.nextBackoffNanos() - stopwatch.elapsed(TimeUnit.NANOSECONDS);
+      }
+      if (delayNanos <= 0) {
+        startLrsRpc();
+      } else {
+        lrsRpcRetryTimer =
+            syncContext.schedule(new LrsRpcRetryTask(), delayNanos, TimeUnit.NANOSECONDS,
+                timerService);
+      }
+    }
+
+    private void close(@Nullable Exception error) {
+      if (closed) {
+        return;
+      }
+      closed = true;
+      cleanUp();
+      if (error == null) {
+        lrsRequestWriter.onCompleted();
+      } else {
+        lrsRequestWriter.onError(error);
+      }
+    }
+
+    private void cleanUp() {
+      if (loadReportTimer != null) {
+        loadReportTimer.cancel();
+        loadReportTimer = null;
+      }
+      if (lrsStream == this) {
+        lrsStream = null;
+      }
+    }
+
+  }
+}
---
+++ xds/src/test/java/io/grpc/xds/XdsLrsClientTest.java
@@ -0,0 +1,357 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds;
+
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.AdditionalAnswers.delegatesTo;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.google.protobuf.Duration;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import io.envoyproxy.envoy.api.v2.core.Node;
+import io.envoyproxy.envoy.api.v2.endpoint.ClusterStats;
+import io.envoyproxy.envoy.service.load_stats.v2.LoadReportingServiceGrpc;
+import io.envoyproxy.envoy.service.load_stats.v2.LoadStatsRequest;
+import io.envoyproxy.envoy.service.load_stats.v2.LoadStatsResponse;
+import io.grpc.ChannelLogger;
+import io.grpc.LoadBalancer.Helper;
+import io.grpc.ManagedChannel;
+import io.grpc.Status;
+import io.grpc.SynchronizationContext;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.internal.BackoffPolicy;
+import io.grpc.internal.FakeClock;
+import io.grpc.stub.StreamObserver;
+import io.grpc.testing.GrpcCleanupRule;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.TimeUnit;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+public class XdsLrsClientTest {
+
+  private static final String SERVICE_AUTHORITY = "api.google.com";
+  private static final FakeClock.TaskFilter LOAD_REPORTING_TASK_FILTER =
+      new FakeClock.TaskFilter() {
+        @Override
+        public boolean shouldAccept(Runnable command) {
+          return command.toString().contains(XdsLrsClient.LoadReportingTask.class.getSimpleName());
+        }
+      };
+  private static final FakeClock.TaskFilter LRS_RPC_RETRY_TASK_FILTER =
+      new FakeClock.TaskFilter() {
+        @Override
+        public boolean shouldAccept(Runnable command) {
+          return command.toString().contains(XdsLrsClient.LrsRpcRetryTask.class.getSimpleName());
+        }
+      };
+  @Rule
+  public final GrpcCleanupRule cleanupRule = new GrpcCleanupRule();
+  private final SynchronizationContext syncContext = new SynchronizationContext(
+      new Thread.UncaughtExceptionHandler() {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+          throw new AssertionError(e);
+        }
+      });
+  private final ArrayList<String> logs = new ArrayList<>();
+  private final ChannelLogger channelLogger = new ChannelLogger() {
+    @Override
+    public void log(ChannelLogLevel level, String msg) {
+      logs.add(level + ": " + msg);
+    }
+
+    @Override
+    public void log(ChannelLogLevel level, String template, Object... args) {
+      log(level, MessageFormat.format(template, args));
+    }
+  };
+  private LoadReportingServiceGrpc.LoadReportingServiceImplBase mockLoadReportingService;
+  private FakeClock fakeClock = new FakeClock();
+  private final LinkedList<StreamObserver<LoadStatsRequest>> lrsRequestObservers =
+      new LinkedList<>();
+  @Captor
+  private ArgumentCaptor<StreamObserver<LoadStatsResponse>> lrsResponseObserverCaptor;
+
+  @Mock
+  private Helper helper;
+  @Mock
+  private BackoffPolicy.Provider backoffPolicyProvider;
+  private static final LoadStatsRequest EXPECTED_INITIAL_REQ = LoadStatsRequest.newBuilder()
+      .setNode(Node.newBuilder()
+          .setMetadata(Struct.newBuilder()
+              .putFields(
+                  XdsLrsClient.TRAFFICDIRECTOR_GRPC_HOSTNAME,
+                  Value.newBuilder().setStringValue(SERVICE_AUTHORITY).build())))
+      .build();
+  @Mock
+  private BackoffPolicy backoffPolicy1;
+  private XdsLrsClient lrsClient;
+  @Mock
+  private BackoffPolicy backoffPolicy2;
+
+  private static ClusterStats buildClusterStats(long loadReportIntervalNanos) {
+    return ClusterStats.newBuilder().setLoadReportInterval(
+        XdsLrsClient.numToDuration(loadReportIntervalNanos, TimeUnit.NANOSECONDS)).build();
+  }
+
+  private static LoadStatsResponse buildLrsResponse(long loadReportIntervalNanos) {
+    return LoadStatsResponse.newBuilder().setLoadReportingInterval(
+        XdsLrsClient.numToDuration(loadReportIntervalNanos, TimeUnit.NANOSECONDS)).build();
+  }
+
+  @Test
+  public void testUtilDurationConversion() {
+    Duration duration = Duration.newBuilder().setSeconds(123).setNanos(456).build();
+    long nano = TimeUnit.SECONDS.toNanos(123) + 456;
+    assertEquals(nano, XdsLrsClient.durationToNum(duration, TimeUnit.NANOSECONDS));
+    assertEquals(XdsLrsClient.numToDuration(nano, TimeUnit.NANOSECONDS), duration);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+    mockLoadReportingService = mock(LoadReportingServiceGrpc.LoadReportingServiceImplBase.class,
+        delegatesTo(
+            new LoadReportingServiceGrpc.LoadReportingServiceImplBase() {
+              @Override
+              public StreamObserver<LoadStatsRequest> streamLoadStats(
+                  final StreamObserver<LoadStatsResponse> responseObserver) {
+                StreamObserver<LoadStatsRequest> requestObserver =
+                    mock(StreamObserver.class);
+                Answer<Void> closeRpc = new Answer<Void>() {
+                  @Override
+                  public Void answer(InvocationOnMock invocation) {
+                    responseObserver.onCompleted();
+                    return null;
+                  }
+                };
+                doAnswer(closeRpc).when(requestObserver).onCompleted();
+                lrsRequestObservers.add(requestObserver);
+                return requestObserver;
+              }
+            }
+        ));
+    cleanupRule.register(InProcessServerBuilder.forName("fakeLoadReportingServer").directExecutor()
+        .addService(mockLoadReportingService).build().start());
+    ManagedChannel channel = cleanupRule.register(
+        InProcessChannelBuilder.forName("fakeLoadReportingServer").directExecutor().build());
+    when(helper.getSynchronizationContext()).thenReturn(syncContext);
+    when(helper.getScheduledExecutorService()).thenReturn(fakeClock.getScheduledExecutorService());
+    when(helper.getChannelLogger()).thenReturn(channelLogger);
+    when(helper.getAuthority()).thenReturn(SERVICE_AUTHORITY);
+    when(backoffPolicyProvider.get()).thenReturn(backoffPolicy1, backoffPolicy2);
+    when(backoffPolicy1.nextBackoffNanos()).thenReturn(10L, 100L);
+    when(backoffPolicy2.nextBackoffNanos()).thenReturn(10L, 100L);
+    lrsClient = new XdsLrsClient(channel, helper, fakeClock.getStopwatchSupplier().get(),
+        backoffPolicyProvider);
+  }
+
+  private void assertNextReport(InOrder inOrder, StreamObserver<LoadStatsRequest> requestObserver,
+      ClusterStats expectedStats) {
+    long loadReportIntervalNanos = XdsLrsClient
+        .durationToNum(expectedStats.getLoadReportInterval(), TimeUnit.NANOSECONDS);
+    assertEquals(0, fakeClock.forwardTime(loadReportIntervalNanos - 1, TimeUnit.NANOSECONDS));
+    inOrder.verifyNoMoreInteractions();
+    assertEquals(1, fakeClock.forwardTime(1, TimeUnit.NANOSECONDS));
+    // A second load report is scheduled upon the first is sent.
+    assertEquals(1, fakeClock.numPendingTasks(LOAD_REPORTING_TASK_FILTER));
+    inOrder.verify(requestObserver).onNext(
+        eq(LoadStatsRequest.newBuilder().setNode(Node.newBuilder()
+            .setMetadata(Struct.newBuilder()
+                .putFields(
+                    XdsLrsClient.TRAFFICDIRECTOR_GRPC_HOSTNAME,
+                    Value.newBuilder().setStringValue(SERVICE_AUTHORITY).build())))
+            .addClusterStats(ClusterStats.newBuilder(expectedStats))
+            .build()));
+  }
+
+  @Test
+  public void loadReportInitialRequest() {
+    lrsClient.startLrsRpc();
+    verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    assertThat(lrsRequestObservers).hasSize(1);
+    StreamObserver<LoadStatsRequest> requestObserver = lrsRequestObservers.poll();
+    verify(requestObserver).onNext(EXPECTED_INITIAL_REQ);
+    // No more request should be sent until receiving initial response. No load reporting
+    // should be scheduled.
+    assertThat(fakeClock.getPendingTasks(LOAD_REPORTING_TASK_FILTER)).isEmpty();
+    verifyNoMoreInteractions(requestObserver);
+    lrsClient.shutdownLrsRpc();
+  }
+
+  @Test
+  public void loadReportActualIntervalAsSpecified() {
+    lrsClient.startLrsRpc();
+    verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    StreamObserver<LoadStatsResponse> responseObserver = lrsResponseObserverCaptor.getValue();
+    assertThat(lrsRequestObservers).hasSize(1);
+    StreamObserver<LoadStatsRequest> requestObserver = lrsRequestObservers.poll();
+    InOrder inOrder = inOrder(requestObserver);
+    inOrder.verify(requestObserver).onNext(EXPECTED_INITIAL_REQ);
+
+    responseObserver.onNext(
+        LoadStatsResponse.newBuilder()
+            .addClusters(SERVICE_AUTHORITY)
+            .setLoadReportingInterval(Duration.newBuilder().setNanos(1362))
+            .build());
+    assertNextReport(inOrder, requestObserver, buildClusterStats(1362));
+    lrsClient.shutdownLrsRpc();
+  }
+
+  @Test
+  public void loadReportIntervalUpdate() {
+    lrsClient.startLrsRpc();
+    verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    StreamObserver<LoadStatsResponse> responseObserver = lrsResponseObserverCaptor.getValue();
+    assertThat(lrsRequestObservers).hasSize(1);
+    StreamObserver<LoadStatsRequest> requestObserver = lrsRequestObservers.poll();
+    InOrder inOrder = inOrder(requestObserver);
+    inOrder.verify(requestObserver).onNext(EXPECTED_INITIAL_REQ);
+
+    responseObserver.onNext(
+        LoadStatsResponse.newBuilder()
+            .addClusters(SERVICE_AUTHORITY)
+            .setLoadReportingInterval(Duration.newBuilder().setNanos(1362))
+            .build());
+    assertNextReport(inOrder, requestObserver, buildClusterStats(1362));
+
+    responseObserver.onNext(
+        LoadStatsResponse.newBuilder()
+            .addClusters(SERVICE_AUTHORITY)
+            .setLoadReportingInterval(Duration.newBuilder().setNanos(2189))
+            .build());
+    // Updated load reporting interval becomes effective immediately.
+    assertNextReport(inOrder, requestObserver, buildClusterStats(2189));
+    lrsClient.shutdownLrsRpc();
+  }
+
+  @Test
+  public void lrsStreamClosedAndRetried() {
+    lrsClient.startLrsRpc();
+    InOrder inOrder = inOrder(mockLoadReportingService, backoffPolicyProvider, backoffPolicy1,
+        backoffPolicy2);
+    inOrder.verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    StreamObserver<LoadStatsResponse> responseObserver = lrsResponseObserverCaptor.getValue();
+    assertEquals(1, lrsRequestObservers.size());
+    StreamObserver<LoadStatsRequest> requestObserver = lrsRequestObservers.poll();
+
+    // First balancer RPC
+    verify(requestObserver).onNext(EXPECTED_INITIAL_REQ);
+    assertEquals(0, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Balancer closes it immediately (erroneously)
+    responseObserver.onCompleted();
+
+    // Will start backoff sequence 1 (10ns)
+    inOrder.verify(backoffPolicyProvider).get();
+    inOrder.verify(backoffPolicy1).nextBackoffNanos();
+    assertEquals(1, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Fast-forward to a moment before the retry
+    fakeClock.forwardNanos(9);
+    verifyNoMoreInteractions(mockLoadReportingService);
+    // Then time for retry
+    fakeClock.forwardNanos(1);
+    inOrder.verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    responseObserver = lrsResponseObserverCaptor.getValue();
+    assertEquals(1, lrsRequestObservers.size());
+    requestObserver = lrsRequestObservers.poll();
+    verify(requestObserver).onNext(eq(EXPECTED_INITIAL_REQ));
+    assertEquals(0, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Balancer closes it with an error.
+    responseObserver.onError(Status.UNAVAILABLE.asException());
+    // Will continue the backoff sequence 1 (100ns)
+    verifyNoMoreInteractions(backoffPolicyProvider);
+    inOrder.verify(backoffPolicy1).nextBackoffNanos();
+    assertEquals(1, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Fast-forward to a moment before the retry
+    fakeClock.forwardNanos(100 - 1);
+    verifyNoMoreInteractions(mockLoadReportingService);
+    // Then time for retry
+    fakeClock.forwardNanos(1);
+    inOrder.verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    responseObserver = lrsResponseObserverCaptor.getValue();
+    assertEquals(1, lrsRequestObservers.size());
+    requestObserver = lrsRequestObservers.poll();
+    verify(requestObserver).onNext(eq(EXPECTED_INITIAL_REQ));
+    assertEquals(0, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Balancer sends initial response.
+    responseObserver.onNext(buildLrsResponse(0));
+
+    // Then breaks the RPC
+    responseObserver.onError(Status.UNAVAILABLE.asException());
+
+    // Will reset the retry sequence and retry immediately, because balancer has responded.
+    inOrder.verify(backoffPolicyProvider).get();
+    inOrder.verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    responseObserver = lrsResponseObserverCaptor.getValue();
+    assertEquals(1, lrsRequestObservers.size());
+    requestObserver = lrsRequestObservers.poll();
+    verify(requestObserver).onNext(eq(EXPECTED_INITIAL_REQ));
+
+    // Fail the retry after spending 4ns
+    fakeClock.forwardNanos(4);
+    responseObserver.onError(Status.UNAVAILABLE.asException());
+
+    // Will be on the first retry (10ns) of backoff sequence 2.
+    inOrder.verify(backoffPolicy2).nextBackoffNanos();
+    assertEquals(1, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Fast-forward to a moment before the retry, the time spent in the last try is deducted.
+    fakeClock.forwardNanos(10 - 4 - 1);
+    verifyNoMoreInteractions(mockLoadReportingService);
+    // Then time for retry
+    fakeClock.forwardNanos(1);
+    inOrder.verify(mockLoadReportingService).streamLoadStats(lrsResponseObserverCaptor.capture());
+    assertEquals(1, lrsRequestObservers.size());
+    requestObserver = lrsRequestObservers.poll();
+    verify(requestObserver).onNext(eq(EXPECTED_INITIAL_REQ));
+    assertEquals(0, fakeClock.numPendingTasks(LRS_RPC_RETRY_TASK_FILTER));
+
+    // Wrapping up
+    verify(backoffPolicyProvider, times(2)).get();
+    verify(backoffPolicy1, times(2)).nextBackoffNanos();
+    verify(backoffPolicy2, times(1)).nextBackoffNanos();
+    lrsClient.shutdownLrsRpc();
+  }
+}
