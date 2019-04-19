287d69ccfe1d69363d5a076aaa124ac49c5694c8
Carl Mastrangelo <notcarl@google.com>
2019-04-19 11:26:39 -0700

    clean up grpc

--- WORKSPACE
+++
@@ -1,5 +0,0 @@
-workspace(name = "io_grpc_grpc_java")
-
-load("//:repositories.bzl", "grpc_java_repositories")
-
-grpc_java_repositories()
--- buildscripts/build_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu -o pipefail
-
-readonly proto_dir="$(mktemp -d --tmpdir protobuf.XXXXXX)"
-wget -O - https://github.com/google/protobuf/archive/v3.7.1.tar.gz | tar xz -C "$proto_dir"
-
-docker build -t protoc-artifacts "$proto_dir"/protobuf-3.7.1/protoc-artifacts
-rm -r "$proto_dir"
