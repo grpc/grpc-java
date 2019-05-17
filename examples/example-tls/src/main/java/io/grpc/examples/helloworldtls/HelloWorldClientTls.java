62cd93698db349c2b1f1a62377ad0e4211f57ce8
Chengyuan Zhang <chengyuanzhang@google.com>
2019-05-17 15:11:06 -0700

    removed redundent code copied from older examples

--- examples/example-tls/src/main/java/io/grpc/examples/helloworldtls/HelloWorldClientTls.java
+++ examples/example-tls/src/main/java/io/grpc/examples/helloworldtls/HelloWorldClientTls.java
@@ -123,12 +123,7 @@ public class HelloWorldClientTls {
         }
 
         try {
-            /* Access a service running on the local machine on port 50051 */
-            String user = "world";
-            if (args.length > 0) {
-                user = args[0]; /* Use the arg as the name to greet if provided */
-            }
-            client.greet(user);
+            client.greet(args[0]);
         } finally {
             client.shutdown();
         }
