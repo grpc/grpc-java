1ee9cce8179442efc25d4cbc10ac3d86967732ca
Carl Mastrangelo <notcarl@google.com>
2019-06-05 15:23:49 -0700

    examples: add example of how to set log scoped data with log4j

---
+++ examples/src/main/java/io/grpc/examples/logcontext/HeaderServerInterceptor.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2015 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.examples.logcontext;
+
+import io.grpc.ForwardingServerCallListener.SimpleForwardingServerCallListener;
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import java.util.UUID;
+import org.apache.logging.log4j.ThreadContext;
+
+/**
+ * A interceptor to handle server header.
+ */
+public class HeaderServerInterceptor implements ServerInterceptor {
+  
+  private static final String REQUEST_ID_NAME = "requestId";
+
+  static final Metadata.Key<String> CLIENT_NAME_KEY =
+      Metadata.Key.of("clientName", Metadata.ASCII_STRING_MARSHALLER);
+
+  @Override
+  public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+      ServerCall<ReqT, RespT> call,
+      final Metadata requestHeaders,
+      ServerCallHandler<ReqT, RespT> next) {
+
+    final String requestId = UUID.randomUUID().toString();
+    final String clientName = String.valueOf(requestHeaders.get(CLIENT_NAME_KEY));
+
+    return new SimpleForwardingServerCallListener<ReqT>(next.startCall(call, requestHeaders)) {
+      @Override
+      public void onCancel() {
+        ThreadContext.put(REQUEST_ID_NAME, requestId);
+        ThreadContext.put(CLIENT_NAME_KEY.name(), clientName);
+        try {
+          super.onCancel();
+        } finally {
+          ThreadContext.remove(REQUEST_ID_NAME);
+          ThreadContext.remove(CLIENT_NAME_KEY.name());
+        }
+      }
+
+      @Override
+      public void onComplete() {
+        ThreadContext.put(REQUEST_ID_NAME, requestId);
+        ThreadContext.put(CLIENT_NAME_KEY.name(), clientName);
+        try {
+          super.onComplete();
+        } finally {
+          ThreadContext.remove(REQUEST_ID_NAME);
+          ThreadContext.remove(CLIENT_NAME_KEY.name());
+        }
+      }
+
+      @Override
+      public void onMessage(ReqT message) {
+        ThreadContext.put(REQUEST_ID_NAME, requestId);
+        ThreadContext.put(CLIENT_NAME_KEY.name(), clientName);
+        try {
+          super.onMessage(message);
+        } finally {
+          ThreadContext.remove(REQUEST_ID_NAME);
+          ThreadContext.remove(CLIENT_NAME_KEY.name());
+        }
+      }
+
+      @Override
+      public void onReady() {
+        ThreadContext.put(REQUEST_ID_NAME, requestId);
+        ThreadContext.put(CLIENT_NAME_KEY.name(), clientName);
+        try {
+          super.onReady();
+        } finally {
+          ThreadContext.remove(REQUEST_ID_NAME);
+          ThreadContext.remove(CLIENT_NAME_KEY.name());
+        }
+      }
+
+      @Override
+      public void onHalfClose() {
+        ThreadContext.put(REQUEST_ID_NAME, requestId);
+        ThreadContext.put(CLIENT_NAME_KEY.name(), clientName);
+        try {
+          super.onHalfClose();
+        } finally {
+          ThreadContext.remove(REQUEST_ID_NAME);
+          ThreadContext.remove(CLIENT_NAME_KEY.name());
+        }
+      }
+    };
+  }
+}
