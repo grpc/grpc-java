5f8d7daabd8ed8acd0ef8716a62044b70c84e95e
Jihun Cho <jihuncho@google.com>
2019-07-10 15:25:26 -0700

    minor changes

--- buildscripts/request_jcenter.sh
+++ buildscripts/request_jcenter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-
 [[ -z "$MAJOR" ]] && { echo "env variable MAJOR must be provided" ; exit 1; }
 [[ -z "$MINOR" ]] && { echo "env variable MINOR must be provided" ; exit 1; }
 [[ -z "$PATCH" ]] && { echo "env variable PATCH must be provided" ; exit 1; }
@@ -7,7 +6,7 @@
 VERSION="$MAJOR.$MINOR.$PATCH"
 
 # NOTE: prefix grpc is omitted
-ARTIFACTS=(api core error context stub auth okhttp protobuf protobuf-lite netty netty-shaded grpclb testing testing-proto interop-testing all alts benchmarks services)
+ARTIFACTS=(api core context stub auth okhttp protobuf protobuf-lite netty netty-shaded grpclb testing testing-proto interop-testing all alts benchmarks services)
 POMS=(bom)
 
 echo "Ping jcenter to cache grpc v${VERSION}"
@@ -16,14 +15,14 @@ trap "rm -rf $TEMP_DIR" EXIT
 
 for ARTIFACT in "${ARTIFACTS[@]}"
 do
-  echo "downloading grpc-$ARTIFACT"
+  echo -e "\ndownloading grpc-$ARTIFACT"
   wget -q --show-progress "https://jcenter.bintray.com/io/grpc/grpc-${ARTIFACT}/${VERSION}/grpc-${ARTIFACT}-${VERSION}.jar" -P $TEMP_DIR
 done
 
 for POM in "${POMS[@]}"
 do
-  echo "downloading grpc-$POM"
-  wget -q "https://jcenter.bintray.com/io/grpc/grpc-${POM}/${VERSION}/grpc-${POM}-${VERSION}.pom" -P $TEMP_DIR
+  echo -e "\ndownloading grpc-$POM"
+  wget -q --show-progress"https://jcenter.bintray.com/io/grpc/grpc-${POM}/${VERSION}/grpc-${POM}-${VERSION}.pom" -P $TEMP_DIR
 done
 
 echo -e "\ndownloaded files..."
