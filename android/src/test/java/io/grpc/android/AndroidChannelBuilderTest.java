1e475be64d5458c301be73e11b88333eb747ce66
Chengyuan Zhang <chengyuanzhang@google.com>
2019-08-28 09:51:44 -0700

    android: deleted unused code leftover in AndroidChannelBuilder test. (#6098)

--- android/src/test/java/io/grpc/android/AndroidChannelBuilderTest.java
+++ android/src/test/java/io/grpc/android/AndroidChannelBuilderTest.java
@@ -32,13 +32,7 @@ import io.grpc.ClientCall;
 import io.grpc.ManagedChannel;
 import io.grpc.MethodDescriptor;
 import io.grpc.okhttp.OkHttpChannelBuilder;
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
@@ -386,96 +380,6 @@ public final class AndroidChannelBuilderTest {
     @Override
     public void enterIdle() {
       enterIdleCount++;
-    }
-  }
-
-  private static class ScheduledExecutorImpl implements ScheduledExecutorService {
-    @Override
-    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public ScheduledFuture<?> schedule(Runnable cmd, long delay, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public ScheduledFuture<?> scheduleAtFixedRate(
-        Runnable command, long initialDelay, long period, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public ScheduledFuture<?> scheduleWithFixedDelay(
-        Runnable command, long initialDelay, long delay, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean awaitTermination(long timeout, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> List<Future<T>> invokeAll(
-        Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean isShutdown() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean isTerminated() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void shutdown() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public List<Runnable> shutdownNow() {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> Future<T> submit(Callable<T> task) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Future<?> submit(Runnable task) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> Future<T> submit(Runnable task, T result) {
-      throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void execute(Runnable command) {
-      throw new UnsupportedOperationException();
     }
   }
 }
