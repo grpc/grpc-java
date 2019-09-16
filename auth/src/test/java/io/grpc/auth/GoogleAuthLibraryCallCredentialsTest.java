df4da6bef3e94ea31dbe3574ad590eb411508a2b
Jihun Cho <jihuncho@google.com>
2019-09-16 15:48:56 -0700

    auth: removed unused import (#6155)

--- auth/src/test/java/io/grpc/auth/GoogleAuthLibraryCallCredentialsTest.java
+++ auth/src/test/java/io/grpc/auth/GoogleAuthLibraryCallCredentialsTest.java
@@ -54,7 +54,6 @@ import java.net.URI;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
