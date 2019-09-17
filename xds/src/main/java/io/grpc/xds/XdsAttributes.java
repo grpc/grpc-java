fd4f189d2b5562398e439878a9260ba99e095115
sanjaypujare <sanjaypujare@users.noreply.github.com>
2019-09-17 11:01:45 -0700

    Create a new class XdsAttributes in xds to house XDS related attributes and define ATTR_SDS_CONFIG there (#6154)
    Need to define a new attribute so we can plumb SdsSecretConfig from the xDS LB to SSL code so the value can be passed on to the SDS module.

---
+++ xds/src/main/java/io/grpc/xds/XdsAttributes.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds;
+
+import io.envoyproxy.envoy.api.v2.auth.SdsSecretConfig;
+import io.grpc.Attributes;
+import io.grpc.Grpc;
+
+/**
+ * Special attributes that are only useful to gRPC in the XDS context
+ */
+final class XdsAttributes {
+  /**
+   * Attribute key for SdsSecretConfig of a subchannel.
+   *
+   */
+  @Grpc.TransportAttr
+  public static final Attributes.Key<SdsSecretConfig> ATTR_SDS_CONFIG =
+          Attributes.Key.create("io.grpc.xds.XdsAttributes.sdsSecretConfig");
+
+  private XdsAttributes() {}
+}
