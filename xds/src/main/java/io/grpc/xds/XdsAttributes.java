1743fb0178d04cb2bd8d2b8dbfe4817063c03a27
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2019-10-04 08:49:49 -0700

    Adding more attributes to XdsAttributes and making XdsAttributes public to prepare for using *TlsContext in the new SecretManager

--- xds/src/main/java/io/grpc/xds/XdsAttributes.java
+++ xds/src/main/java/io/grpc/xds/XdsAttributes.java
@@ -17,15 +17,18 @@
 package io.grpc.xds;
 
 import io.envoyproxy.envoy.api.v2.auth.CertificateValidationContext;
+import io.envoyproxy.envoy.api.v2.auth.CommonTlsContext;
+import io.envoyproxy.envoy.api.v2.auth.DownstreamTlsContext;
 import io.envoyproxy.envoy.api.v2.auth.SdsSecretConfig;
 import io.envoyproxy.envoy.api.v2.auth.TlsCertificate;
+import io.envoyproxy.envoy.api.v2.auth.UpstreamTlsContext;
 import io.grpc.Attributes;
 import io.grpc.Grpc;
 
 /**
  * Special attributes that are only useful to gRPC in the XDS context.
  */
-final class XdsAttributes {
+public final class XdsAttributes {
   /**
    * Attribute key for SdsSecretConfig of a subchannel.
    */
@@ -46,6 +49,27 @@ final class XdsAttributes {
   @Grpc.TransportAttr
   public static final Attributes.Key<CertificateValidationContext> ATTR_CERT_VALIDATION_CONTEXT =
           Attributes.Key.create("io.grpc.xds.XdsAttributes.certificateValidationContext");
+
+  /**
+   * Attribute key for CommonTlsContext.
+   */
+  @Grpc.TransportAttr
+  public static final Attributes.Key<CommonTlsContext> ATTR_COMMON_TLS_CONTEXT =
+      Attributes.Key.create("io.grpc.xds.XdsAttributes.commonTlsContext");
+
+  /**
+   * Attribute key for UpstreamTlsContext (used by client) for subchannel.
+   */
+  @Grpc.TransportAttr
+  public static final Attributes.Key<UpstreamTlsContext> ATTR_UPSTREAM_TLS_CONTEXT =
+      Attributes.Key.create("io.grpc.xds.XdsAttributes.upstreamTlsContext");
+
+  /**
+   * Attribute key for DownstreamTlsContext (used by server).
+   */
+  @Grpc.TransportAttr
+  public static final Attributes.Key<DownstreamTlsContext> ATTR_DOWNSTREAM_TLS_CONTEXT =
+      Attributes.Key.create("io.grpc.xds.XdsAttributes.downstreamTlsContext");
 
   private XdsAttributes() {}
 }
