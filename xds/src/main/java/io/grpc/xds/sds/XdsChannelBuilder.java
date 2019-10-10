0c4fc64bf2133d7cde8383ca924a8f518e44d8a8
sanjaypujare <sanjaypujare@users.noreply.github.com>
2019-10-10 16:38:27 -0700

    xds: first installment of SdsProtocolNegotiators with Client and Server builders (#6261)
    * xds: SdsProtocolNegotiators with Client and Server builders

---
+++ xds/src/main/java/io/grpc/xds/sds/XdsChannelBuilder.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds.sds;
+
+import io.grpc.ExperimentalApi;
+import io.grpc.ForwardingChannelBuilder;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.xds.sds.internal.SdsProtocolNegotiators;
+import java.net.SocketAddress;
+import javax.annotation.CheckReturnValue;
+
+/**
+ * A version of {@link ManagedChannelBuilder} to create xDS managed channels that will use SDS to
+ * set up SSL with peers. Note, this is not ready to use yet.
+ */
+@ExperimentalApi("https://github.com/grpc/grpc-java/issues/6268")
+public final class XdsChannelBuilder extends ForwardingChannelBuilder<XdsChannelBuilder> {
+
+  private final NettyChannelBuilder delegate;
+
+  private XdsChannelBuilder(NettyChannelBuilder delegate) {
+    this.delegate = delegate;
+    SdsProtocolNegotiators.setProtocolNegotiatorFactory(delegate);
+  }
+
+  /**
+   * Creates a new builder with the given server address. See {@link
+   * NettyChannelBuilder#forAddress(SocketAddress)} for more info.
+   */
+  @CheckReturnValue
+  public static XdsChannelBuilder forAddress(SocketAddress serverAddress) {
+    return new XdsChannelBuilder(NettyChannelBuilder.forAddress(serverAddress));
+  }
+
+  /**
+   * Creates a new builder with the given host and port. See {@link
+   * NettyChannelBuilder#forAddress(String, int)} for more info.
+   */
+  @CheckReturnValue
+  public static XdsChannelBuilder forAddress(String host, int port) {
+    return new XdsChannelBuilder(NettyChannelBuilder.forAddress(host, port));
+  }
+
+  /**
+   * Creates a new builder with the given target string. See {@link
+   * NettyChannelBuilder#forTarget(String)} for more info.
+   */
+  @CheckReturnValue
+  public static XdsChannelBuilder forTarget(String target) {
+    return new XdsChannelBuilder(NettyChannelBuilder.forTarget(target));
+  }
+
+  @Override
+  protected ManagedChannelBuilder<?> delegate() {
+    return delegate;
+  }
+
+  @Override
+  public ManagedChannel build() {
+    return delegate.build();
+  }
+}
---
+++ xds/src/main/java/io/grpc/xds/sds/XdsServerBuilder.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds.sds;
+
+import io.grpc.BindableService;
+import io.grpc.CompressorRegistry;
+import io.grpc.DecompressorRegistry;
+import io.grpc.ExperimentalApi;
+import io.grpc.HandlerRegistry;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerInterceptor;
+import io.grpc.ServerServiceDefinition;
+import io.grpc.ServerStreamTracer;
+import io.grpc.ServerTransportFilter;
+import io.grpc.netty.NettyServerBuilder;
+import io.grpc.xds.sds.internal.SdsProtocolNegotiators;
+import java.io.File;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nullable;
+
+/**
+ * A version of {@link ServerBuilder} to create xDS managed servers that will use SDS to set up SSL
+ * with peers. Note, this is not ready to use yet.
+ */
+@ExperimentalApi("https://github.com/grpc/grpc-java/issues/6268")
+public final class XdsServerBuilder extends ServerBuilder<XdsServerBuilder> {
+
+  private final NettyServerBuilder delegate;
+
+  private XdsServerBuilder(NettyServerBuilder nettyDelegate) {
+    this.delegate = nettyDelegate;
+  }
+
+  @Override
+  public XdsServerBuilder handshakeTimeout(long timeout, TimeUnit unit) {
+    delegate.handshakeTimeout(timeout, unit);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder directExecutor() {
+    delegate.directExecutor();
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder addStreamTracerFactory(ServerStreamTracer.Factory factory) {
+    delegate.addStreamTracerFactory(factory);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder addTransportFilter(ServerTransportFilter filter) {
+    delegate.addTransportFilter(filter);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder executor(Executor executor) {
+    delegate.executor(executor);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder addService(ServerServiceDefinition service) {
+    delegate.addService(service);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder addService(BindableService bindableService) {
+    delegate.addService(bindableService);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder fallbackHandlerRegistry(@Nullable HandlerRegistry fallbackRegistry) {
+    delegate.fallbackHandlerRegistry(fallbackRegistry);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder useTransportSecurity(File certChain, File privateKey) {
+    throw new UnsupportedOperationException("Cannot set security parameters on XdsServerBuilder");
+  }
+
+  @Override
+  public XdsServerBuilder decompressorRegistry(@Nullable DecompressorRegistry registry) {
+    delegate.decompressorRegistry(registry);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder compressorRegistry(@Nullable CompressorRegistry registry) {
+    delegate.compressorRegistry(registry);
+    return this;
+  }
+
+  @Override
+  public XdsServerBuilder intercept(ServerInterceptor interceptor) {
+    delegate.intercept(interceptor);
+    return this;
+  }
+
+  /** Creates a gRPC server builder for the given port. */
+  public static XdsServerBuilder forPort(int port) {
+    NettyServerBuilder nettyDelegate = NettyServerBuilder.forAddress(new InetSocketAddress(port));
+    return new XdsServerBuilder(nettyDelegate);
+  }
+
+  @Override
+  public Server build() {
+    // note: doing it in build() will overwrite any previously set ProtocolNegotiator
+    delegate.protocolNegotiator(SdsProtocolNegotiators.serverProtocolNegotiator());
+    return delegate.build();
+  }
+}
