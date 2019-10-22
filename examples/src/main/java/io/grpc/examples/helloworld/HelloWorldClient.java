d1411b205c93034e722bbb46c6e0e323e2ecf701
Steve Rao <zihaorao@126.com>
2019-10-23 06:45:42 +0800

    examples: adjust comment (#6314)

--- examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -70,12 +70,13 @@ public class HelloWorldClient {
    * greeting.
    */
   public static void main(String[] args) throws Exception {
+    // Access a service running on the local machine on port 50051
     HelloWorldClient client = new HelloWorldClient("localhost", 50051);
     try {
-      /* Access a service running on the local machine on port 50051 */
       String user = "world";
+      // Use the arg as the name to greet if provided
       if (args.length > 0) {
-        user = args[0]; /* Use the arg as the name to greet if provided */
+        user = args[0];
       }
       client.greet(user);
     } finally {
