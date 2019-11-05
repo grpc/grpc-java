218a05639fb0aaaf5e90257d8f7c79353604f64d
Chengyuan Zhang <chengyuanzhang@google.com>
2019-11-05 14:21:45 -0800

    Add test to cover defaulting edsServiceName to clusterName if not set in ClusterUpdate.

---
+++ xds/src/test/java/io/grpc/xds/XdsClientTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.grpc.xds.XdsClient.ClusterUpdate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit tests for {@link XdsClient}.
+ */
+@RunWith(JUnit4.class)
+public class XdsClientTest {
+
+  @Test
+  public void buildClusterUpdate_defaultToClusterNameWhenEdsServiceNameNotSet() {
+    ClusterUpdate clusterUpdate1 =
+        ClusterUpdate.newBuilder()
+            .setClusterName("foo.googleapis.com")
+            .setEdsServiceName("bar.googleapis.com")
+            .setLbPolicy("round_robin")
+            .build();
+    assertThat(clusterUpdate1.getEdsServiceName()).isEqualTo("bar.googleapis.com");
+
+    ClusterUpdate clusterUpdate2 =
+        ClusterUpdate.newBuilder()
+            .setClusterName("foo.googleapis.com")
+            .setLbPolicy("round_robin")
+            .build();
+    assertThat(clusterUpdate2.getEdsServiceName()).isEqualTo("foo.googleapis.com");
+  }
+}
