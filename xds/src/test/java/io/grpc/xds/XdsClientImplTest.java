3a98dcb6d44575cc73f883eebd9d525691da8c75
Chengyuan Zhang <chengyuanzhang@google.com>
2019-11-11 13:24:19 -0800

    Add tests for EDS protocol and endpoint watchers.

--- xds/src/test/java/io/grpc/xds/XdsClientImplTest.java
+++ xds/src/test/java/io/grpc/xds/XdsClientImplTest.java
@@ -1926,17 +1926,12 @@ public class XdsClientImplTest {
   }
 
   /**
-<<<<<<< HEAD
    * Matcher for DiscoveryRequest without the comparison of error_details field, which is used for
    * management server debugging purposes.
    *
    * <p>In general, if you are sure error_details field should not be set in a DiscoveryRequest,
    * compare with message equality. Otherwise, this matcher is handy for comparing other fields
    * only.
-=======
-   * Matcher for DiscoveryRequest used to verify NACK requests. Eliminates the comparison of
-   * error_details for DiscoveryRequests if they are expected to be an NACK request.
->>>>>>> Add tests for EDS protocol and endpoint watchers.
    */
   private static class DiscoveryRequestMatcher implements ArgumentMatcher<DiscoveryRequest> {
     private final String versionInfo;
