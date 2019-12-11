a368103c54e45a5427699ee99d18e0017f80a333
Eric Anderson <ejona@google.com>
2019-12-10 16:19:27 -0800

    Update close() documentation

--- netty/src/main/java/io/grpc/netty/ProtocolNegotiator.java
+++ netty/src/main/java/io/grpc/netty/ProtocolNegotiator.java
@@ -38,9 +38,10 @@ interface ProtocolNegotiator {
   ChannelHandler newHandler(GrpcHttp2ConnectionHandler grpcHandler);
 
   /**
-   * Releases resources held by this negotiator. Called when the Channel transitions to terminated.
-   * Is currently only supported on client-side; server-side protocol negotiators will not see this
-   * method called.
+   * Releases resources held by this negotiator. Called when the Channel transitions to terminated
+   * or when InternalServer is shutdown (depending on client or server). That means handlers
+   * returned by {@link #newHandler()} can outlive their parent negotiator on server-side, but not
+   * on client-side.
    */
   void close();
 }
