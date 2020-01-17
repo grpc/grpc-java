36087ae9af4c408e491354f7cf70c134e95df9f0
Eric Anderson <ejona@google.com>
2020-01-17 10:35:41 -0800

    Use logger instead of printStackTrace

--- examples/example-hostname/src/main/java/io/grpc/examples/hostname/HostnameGreeter.java
+++ examples/example-hostname/src/main/java/io/grpc/examples/hostname/HostnameGreeter.java
@@ -23,9 +23,13 @@ import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /** Greeter implementation which replies identifying itself with its hostname. */
 public final class HostnameGreeter extends GreeterGrpc.GreeterImplBase {
+  private static final Logger logger = Logger.getLogger(HostnameGreeter.class.getName());
+
   private final String serverName;
 
   public HostnameGreeter(String serverName) {
@@ -48,7 +52,7 @@ public final class HostnameGreeter extends GreeterGrpc.GreeterImplBase {
     try {
       return InetAddress.getLocalHost().getHostName();
     } catch (IOException ex) {
-      ex.printStackTrace();
+      logger.log(Level.INFO, "Failed to determine hostname. Will generate one", ex);
     }
     // Strange. Well, let's make an identifier for ourselves.
     return "generated-" + new Random().nextInt();
