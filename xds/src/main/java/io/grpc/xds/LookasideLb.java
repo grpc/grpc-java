4b54ce2210f51bebe2f654d71b9386c111ec07bc
dapengzhang0 <zdapeng@google.com>
2020-01-22 18:45:08 -0800

    xds: EndpointWatcher onError only trigger TRANSIENT_FAILURE if no endpoint update received ealier

--- xds/src/main/java/io/grpc/xds/LookasideLb.java
+++ xds/src/main/java/io/grpc/xds/LookasideLb.java
@@ -437,7 +437,13 @@ final class LookasideLb extends LoadBalancer {
     @Override
     public void onError(Status error) {
       channelLogger.log(ChannelLogLevel.ERROR, "EDS load balancer received an error: {0}",  error);
-      lookasideLbHelper.updateBalancingState(TRANSIENT_FAILURE, new ErrorPicker(error));
+
+      // If we get an error before getting any valid result, we should put the channel in
+      // TRANSIENT_FAILURE; if they get an error after getting a valid result, we keep using the
+      // previous channel state.
+      if (!firstEndpointUpdateReceived) {
+        lookasideLbHelper.updateBalancingState(TRANSIENT_FAILURE, new ErrorPicker(error));
+      }
       endpointUpdateCallback.onError();
     }
   }
