307f7d0e096304bbfed533d05dec0bbcd6dbe92e
Jihun Cho <jihuncho@google.com>
2020-01-31 11:32:26 -0800

    grpclb,xds: fix lint warnings (#6666)

--- grpclb/src/main/java/io/grpc/grpclb/GrpclbLoadBalancer.java
+++ grpclb/src/main/java/io/grpc/grpclb/GrpclbLoadBalancer.java
@@ -208,7 +208,6 @@ class GrpclbLoadBalancer extends LoadBalancer {
    * Gets a list from an object for the given key.  Copy of
    * {@link io.grpc.internal.ServiceConfigUtil#getList}.
    */
-  @SuppressWarnings("unchecked")
   @Nullable
   private static List<?> getList(Map<String, ?> obj, String key) {
     assert key != null;
