3225b5bd73633cfe108412800a202a1f360fc91d
Eric Gribkoff <ericgribkoff@google.com>
2020-01-30 20:33:22 -0800

    buildscripts,interop-testing: Add xDS tests on GCP

---
+++ buildscripts/kokoro/xds.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -exu -o pipefail
+if [[ -f /VERSION ]]; then
+  cat /VERSION
+fi
+
+sudo apt-get install -y python3-pip
+sudo python3 -m pip install grpcio grpcio-tools google-api-python-client google-auth-httplib2
+
+cd github
+
+pushd grpc-java/interop-testing
+../gradlew installDist -x test -PskipCodegen=true -PskipAndroid=true
+popd
+
+git clone https://github.com/ericgribkoff/grpc.git
+
+pushd grpc # TODO: remove when checked in to master
+git checkout run_xds_tests # TODO: remove when checked in to master
+popd # TODO: remove when checked in to master
+
+grpc/tools/run_tests/helper_scripts/prep_xds.sh
+python3 grpc/tools/run_tests/run_xds_tests.py \
+    --bootstrap_file=grpc/tools/run_tests/xds/bootstrap.json \
+    --test_case=all \
+    --project_id=grpc-testing \
+    --gcp_suffix=$(date '+%s') \
+    --verbose \
+    --client_cmd='grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-client --server=xds-experimental:///{service_host}:{service_port} --stats_port={stats_port} --qps={qps}'
