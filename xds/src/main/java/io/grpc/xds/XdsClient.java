557d47361bf9e7528d035075d6ba6db3a13ea925
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2020-02-12 17:00:41 -0800

    xds: add server side support for SDS

--- xds/src/main/java/io/grpc/xds/XdsClient.java
+++ xds/src/main/java/io/grpc/xds/XdsClient.java
@@ -32,6 +32,7 @@ import io.grpc.internal.ObjectPool;
 import io.grpc.xds.Bootstrapper.ChannelCreds;
 import io.grpc.xds.Bootstrapper.ServerInfo;
 import io.grpc.xds.EnvoyProtoData.DropOverload;
+import io.grpc.xds.EnvoyProtoData.Listener;
 import io.grpc.xds.EnvoyProtoData.Locality;
 import io.grpc.xds.EnvoyProtoData.LocalityLbEndpoints;
 import java.util.ArrayList;
@@ -59,13 +60,19 @@ abstract class XdsClient {
    */
   static final class ConfigUpdate {
     private final String clusterName;
+    private final Listener listener;
 
-    private ConfigUpdate(String clusterName) {
+    private ConfigUpdate(String clusterName, Listener listener) {
       this.clusterName = clusterName;
+      this.listener = listener;
     }
 
     String getClusterName() {
       return clusterName;
+    }
+
+    public Listener getListener() {
+      return listener;
     }
 
     static Builder newBuilder() {
@@ -74,6 +81,7 @@ abstract class XdsClient {
 
     static final class Builder {
       private String clusterName;
+      private Listener listener;
 
       // Use ConfigUpdate.newBuilder().
       private Builder() {
@@ -84,9 +92,14 @@ abstract class XdsClient {
         return this;
       }
 
+      Builder setListener(Listener listener) {
+        this.listener = listener;
+        return this;
+      }
+
       ConfigUpdate build() {
         Preconditions.checkState(clusterName != null, "clusterName is not set");
-        return new ConfigUpdate(clusterName);
+        return new ConfigUpdate(clusterName, listener);
       }
     }
   }
