93174cad177bb96a2f7f1abc1e6c333c7675feea
Jihun Cho <jihuncho@google.com>
2020-02-28 16:19:31 -0800

    fix test

--- core/src/test/java/io/grpc/internal/ManagedChannelServiceConfigTest.java
+++ core/src/test/java/io/grpc/internal/ManagedChannelServiceConfigTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.grpc.internal;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -22,7 +38,9 @@ public class ManagedChannelServiceConfigTest {
         ManagedChannelServiceConfig.fromServiceConfig(rawServiceConfig, true, 3, 4, null);
     Map<String, ?> healthCheckingConfig = mcsc.getHealthCheckingConfig();
     assertThat(healthCheckingConfig).isNotNull();
-    assertThat(healthCheckingConfig).containsExactly("serviceName", "COVID-19");
+    assertThat(healthCheckingConfig)
+        .containsExactly(
+            "serviceName", "COVID-19", "description", "I can't visit korea, because of you");
   }
 
   @Test
