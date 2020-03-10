2014d062a92b3f82d21cf78f4eee67553806b612
DRayX <7531689+DRayX@users.noreply.github.com>
2020-03-09 17:10:17 -0700

    Update ManualFlowControlClient example
    Update the manual flow control client example to use a pattern that is applicable in all call arities (unary or streaming request).  If the call is unary request, the StreamObserver isn't returned, so the initial request can't be started from the return value, but the initial request can't be made before the call is started, so we expose a custom "request" method on the ClinetResponseObserver implementation.

--- examples/src/main/java/io/grpc/examples/manualflowcontrol/ManualFlowControlClient.java
+++ examples/src/main/java/io/grpc/examples/manualflowcontrol/ManualFlowControlClient.java
@@ -44,76 +44,79 @@ public class ManualFlowControlClient {
 
     // When using manual flow-control and back-pressure on the client, the ClientResponseObserver handles both
     // request and response streams.
-    ClientResponseObserver<HelloRequest, HelloReply> clientResponseObserver =
-        new ClientResponseObserver<HelloRequest, HelloReply>() {
+    class FlowControlResponseObserver implements ClientResponseObserver<HelloRequest, HelloReply> {
 
-          ClientCallStreamObserver<HelloRequest> requestStream;
+      ClientCallStreamObserver<HelloRequest> requestStream;
+
+      @Override
+      public void beforeStart(final ClientCallStreamObserver<HelloRequest> requestStream) {
+        this.requestStream = requestStream;
+        // Set up manual flow control for the response stream. It feels backwards to configure the response
+        // stream's flow control using the request stream's observer, but this is the way it is.
+        requestStream.disableAutoRequest();
+
+        // Set up a back-pressure-aware producer for the request stream. The onReadyHandler will be invoked
+        // when the consuming side has enough buffer space to receive more messages.
+        //
+        // Messages are serialized into a transport-specific transmit buffer. Depending on the size of this buffer,
+        // MANY messages may be buffered, however, they haven't yet been sent to the server. The server must call
+        // request() to pull a buffered message from the client.
+        //
+        // Note: the onReadyHandler's invocation is serialized on the same thread pool as the incoming
+        // StreamObserver's onNext(), onError(), and onComplete() handlers. Blocking the onReadyHandler will prevent
+        // additional messages from being processed by the incoming StreamObserver. The onReadyHandler must return
+        // in a timely manner or else message processing throughput will suffer.
+        requestStream.setOnReadyHandler(new Runnable() {
+          // An iterator is used so we can pause and resume iteration of the request data.
+          Iterator<String> iterator = names().iterator();
 
           @Override
-          public void beforeStart(final ClientCallStreamObserver<HelloRequest> requestStream) {
-            this.requestStream = requestStream;
-            // Set up manual flow control for the response stream. It feels backwards to configure the response
-            // stream's flow control using the request stream's observer, but this is the way it is.
-            requestStream.disableAutoRequest();
-
-            // Set up a back-pressure-aware producer for the request stream. The onReadyHandler will be invoked
-            // when the consuming side has enough buffer space to receive more messages.
-            //
-            // Messages are serialized into a transport-specific transmit buffer. Depending on the size of this buffer,
-            // MANY messages may be buffered, however, they haven't yet been sent to the server. The server must call
-            // request() to pull a buffered message from the client.
-            //
-            // Note: the onReadyHandler's invocation is serialized on the same thread pool as the incoming
-            // StreamObserver's onNext(), onError(), and onComplete() handlers. Blocking the onReadyHandler will prevent
-            // additional messages from being processed by the incoming StreamObserver. The onReadyHandler must return
-            // in a timely manner or else message processing throughput will suffer.
-            requestStream.setOnReadyHandler(new Runnable() {
-              // An iterator is used so we can pause and resume iteration of the request data.
-              Iterator<String> iterator = names().iterator();
-
-              @Override
-              public void run() {
-                // Start generating values from where we left off on a non-gRPC thread.
-                while (requestStream.isReady()) {
-                  if (iterator.hasNext()) {
-                      // Send more messages if there are more messages to send.
-                      String name = iterator.next();
-                      logger.info("--> " + name);
-                      HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-                      requestStream.onNext(request);
-                  } else {
-                      // Signal completion if there is nothing left to send.
-                      requestStream.onCompleted();
-                  }
-                }
+          public void run() {
+            // Start generating values from where we left off on a non-gRPC thread.
+            while (requestStream.isReady()) {
+              if (iterator.hasNext()) {
+                // Send more messages if there are more messages to send.
+                String name = iterator.next();
+                logger.info("--> " + name);
+                HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+                requestStream.onNext(request);
+              } else {
+                // Signal completion if there is nothing left to send.
+                requestStream.onCompleted();
               }
-            });
+            }
           }
+        });
+      }
 
-          @Override
-          public void onNext(HelloReply value) {
-            logger.info("<-- " + value.getMessage());
-            // Signal the sender to send one message.
-            requestStream.request(1);
-          }
+      @Override
+      public void onNext(HelloReply value) {
+        logger.info("<-- " + value.getMessage());
+        // Signal the sender to send one message.
+        requestStream.request(1);
+      }
 
-          @Override
-          public void onError(Throwable t) {
-            t.printStackTrace();
-            done.countDown();
-          }
+      @Override
+      public void onError(Throwable t) {
+        t.printStackTrace();
+        done.countDown();
+      }
 
-          @Override
-          public void onCompleted() {
-            logger.info("All Done");
-            done.countDown();
-          }
-        };
+      @Override
+      public void onCompleted() {
+        logger.info("All Done");
+        done.countDown();
+      }
+
+      void request(int numMessages) {
+        requestStream.request(numMessages);
+      }
+    }
+    FlowControlResponseObserver responseObserver = new FlowControlResponseObserver();
 
     // Note: clientResponseObserver is handling both request and response stream processing.
-    ClientCallStreamObserver<HelloRequest> requestObserver =
-        (ClientCallStreamObserver<HelloRequest>) stub.sayHelloStreaming(clientResponseObserver);
-    requestObserver.request(1);
+    stub.sayHelloStreaming(responseObserver);
+    responseObserver.request(1);
 
     done.await();
 
