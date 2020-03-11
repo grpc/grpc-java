a2b07795776472eb93b8f8c32d9d5bddfaa9ad9c
Eric Gribkoff <ericgribkoff@google.com>
2020-03-10 17:22:22 -0700

    [Do not review] kokoro test

--- buildscripts/kokoro/xds.sh
+++ buildscripts/kokoro/xds.sh
@@ -7,19 +7,31 @@ fi
 
 cd github
 
-pushd grpc-java/interop-testing
-../gradlew installDist -x test -PskipCodegen=true -PskipAndroid=true
+git clone https://github.com/grpc/grpc-go.git
+
+export GOPATH=~/gopath
+REPO_ROOT=$GOPATH/src/google.golang.org/grpc
+
+mkdir -p ${GOPATH}/src/google.golang.org
+mv grpc-go/ ${REPO_ROOT}
+pushd ${REPO_ROOT}
+go get -v ./...
+go build ...grpc
+pushd interop/xds/client
+go build
+popd
 popd
 
 git clone https://github.com/grpc/grpc.git
 
 grpc/tools/run_tests/helper_scripts/prep_xds.sh
-python3 grpc/tools/run_tests/run_xds_tests.py \
+GRPC_GO_LOG_VERBOSITY_LEVEL=99 GRPC_GO_LOG_SEVERITY_LEVEL=info \
+  python3 grpc/tools/run_tests/run_xds_tests.py \
     --test_case=all \
     --project_id=grpc-testing \
     --gcp_suffix=$(date '+%s') \
     --verbose \
-    --client_cmd="grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-client \
+    --client_cmd="${REPO_ROOT}/interop/xds/client/client \
       --server=xds-experimental:///{server_uri} \
       --stats_port={stats_port} \
       --qps={qps}"
