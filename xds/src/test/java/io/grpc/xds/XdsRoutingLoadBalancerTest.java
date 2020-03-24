f98bad73eeea6096e81915659e1b74b9f7ad19ee
Kun Zhang <zhangkun@google.com>
2020-03-24 10:43:45 -0700

    xds: annotate XdsRoutingLoadBalancerTest with RunWith
    This is required inside google for all tests.

--- xds/src/test/java/io/grpc/xds/XdsRoutingLoadBalancerTest.java
+++ xds/src/test/java/io/grpc/xds/XdsRoutingLoadBalancerTest.java
@@ -61,11 +61,14 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 /** Tests for {@link XdsRoutingLoadBalancer}. */
+@RunWith(JUnit4.class)
 public class XdsRoutingLoadBalancerTest {
 
   private final List<LoadBalancer> fooBalancers = new ArrayList<>();
