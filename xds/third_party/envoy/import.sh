e2083fb1a639e0f16d23b33863cc75cdd903f46b
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2020-03-27 17:06:15 -0700

    xds: update to envoy commit id b16ce6d5ea0d2da73e764d145623e7367dc59ed3

--- xds/third_party/envoy/import.sh
+++ xds/third_party/envoy/import.sh
@@ -18,7 +18,7 @@
 set -e
 BRANCH=master
 # import VERSION from one of the google internal CLs
-VERSION=c0ab3a4374144728c1e193fc2d43951ed36ccdb7
+VERSION=b16ce6d5ea0d2da73e764d145623e7367dc59ed3
 GIT_REPO="https://github.com/envoyproxy/envoy.git"
 GIT_BASE_DIR=envoy
 SOURCE_PROTO_BASE_DIR=envoy/api
@@ -33,12 +33,14 @@ envoy/api/v2/cluster/circuit_breaker.proto
 envoy/api/v2/cluster/filter.proto
 envoy/api/v2/cluster/outlier_detection.proto
 envoy/api/v2/core/address.proto
+envoy/api/v2/core/backoff.proto
 envoy/api/v2/core/base.proto
 envoy/api/v2/core/config_source.proto
 envoy/api/v2/core/grpc_service.proto
 envoy/api/v2/core/health_check.proto
 envoy/api/v2/core/http_uri.proto
 envoy/api/v2/core/protocol.proto
+envoy/api/v2/core/socket_option.proto
 envoy/api/v2/discovery.proto
 envoy/api/v2/eds.proto
 envoy/api/v2/endpoint.proto
@@ -59,6 +61,7 @@ envoy/api/v2/srds.proto
 envoy/config/filter/accesslog/v2/accesslog.proto
 envoy/config/filter/network/http_connection_manager/v2/http_connection_manager.proto
 envoy/config/listener/v2/api_listener.proto
+envoy/config/trace/v2/trace.proto
 envoy/service/discovery/v2/ads.proto
 envoy/service/discovery/v2/sds.proto
 envoy/service/load_stats/v2/lrs.proto
