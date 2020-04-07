54f9e29cdb205062158e19d9dc1a567afd61a318
Chengyuan Zhang <chengyuanzhang@google.com>
2020-04-07 22:31:36 +0000

    xds: fix lint warnings (#6907)

--- xds/src/main/java/io/grpc/xds/XdsNameResolver.java
+++ xds/src/main/java/io/grpc/xds/XdsNameResolver.java
@@ -148,7 +148,6 @@ final class XdsNameResolver extends NameResolver {
       this.listener = listener;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public void onConfigChanged(ConfigUpdate update) {
       Map<String, ?> rawLbConfig;
@@ -220,7 +219,7 @@ final class XdsNameResolver extends NameResolver {
     }
   }
 
-  private static Map<String, ?> generateXdsRoutingRawConfig(List<Route> routesUpdate) {
+  private static ImmutableMap<String, ?> generateXdsRoutingRawConfig(List<Route> routesUpdate) {
     List<Object> routes = new ArrayList<>(routesUpdate.size());
     Map<String, Object> actions = new LinkedHashMap<>();
     Map<RouteAction, String> exitingActions = new HashMap<>();
@@ -275,7 +274,7 @@ final class XdsNameResolver extends NameResolver {
         ImmutableMap.of("route", routes, "action", actions));
   }
 
-  private static Map<String, ?> generateWeightedTargetRawConfig(
+  private static ImmutableMap<String, ?> generateWeightedTargetRawConfig(
       List<ClusterWeight> clusterWeights) {
     Map<String, Object> targets = new LinkedHashMap<>();
     for (ClusterWeight clusterWeight : clusterWeights) {
@@ -292,7 +291,7 @@ final class XdsNameResolver extends NameResolver {
         ImmutableMap.of("targets", targets));
   }
 
-  private static Map<String, ?> generateCdsRawConfig(String clusterName) {
+  private static ImmutableMap<String, ?> generateCdsRawConfig(String clusterName) {
     return ImmutableMap.of(XdsLbPolicies.CDS_POLICY_NAME, ImmutableMap.of("cluster", clusterName));
   }
 
