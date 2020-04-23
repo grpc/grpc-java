762d755f96f002fd9f6364f954f35eee19340021
Salwa <salwa.fathallah@hotmail.fr>
2020-04-23 08:24:19 +0100

    travis: update misspell script

--- tools/spelling.sh
+++ tools/spelling.sh
@@ -5,14 +5,14 @@ if [[ $1 == "correct" ]]; then
     MISSPELL_ARGS="-w"
 fi
 
-sh ./install-misspell.sh &>/dev/null
+bash ./tools/install-misspell.sh &>/dev/null
 wait
 
-OUTPUT=$(git ls-files | xargs ./bin/misspell "$MISSPELL_ARGS")
+OUTPUT=$(git ls-files -m | xargs ./bin/misspell "$MISSPELL_ARGS")
 rm -rf ./bin
 
-if [[ ! -z "${OUTPUT// }" ]]; then
+if [[ -n "${OUTPUT// }" && (-z "${MISSPELL_ARGS// }") ]]; then
   echo "ERROR: ${OUTPUT}"
-  echo "Please correct them by running ./tools/spelling.sh correct"
+  echo "Please correct them by running bash ./tools/spelling.sh correct"
   exit 1
 fi
