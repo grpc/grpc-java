e5443068f6a727d4e8e1ed318fddb30000cea859
Chengyuan Zhang <chengyuanzhang@google.com>
2020-06-01 00:33:27 +0000

    xds: fix lint warnings (#7074)

--- xds/src/test/java/io/grpc/xds/CdsLoadBalancerTest.java
+++ xds/src/test/java/io/grpc/xds/CdsLoadBalancerTest.java
@@ -335,7 +335,6 @@ public class CdsLoadBalancerTest {
   }
 
   @Test
-  @SuppressWarnings({"unchecked"})
   public void handleCdsConfigs_withUpstreamTlsContext()  {
     assertThat(xdsClient).isNull();
     ResolvedAddresses resolvedAddresses1 =
