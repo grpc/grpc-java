7d618141cb66d57ed8d4e2e816a93c1764096ebf
Chengyuan Zhang <chengyuanzhang@google.com>
2020-06-04 16:55:29 -0700

    Replace bazel.sh with xds.sh to run the CI for the PR.

--- buildscripts/kokoro/bazel.sh
+++ buildscripts/kokoro/bazel.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
 set -exu -o pipefail
-cat /VERSION
+if [[ -f /VERSION ]]; then
+  cat /VERSION
+fi
 
-use_bazel.sh 1.0.1
-bazel version
+cd github
 
-cd github/grpc-java
-bazel build ...
+pushd grpc-java/interop-testing
+branch=$(git branch --all --no-color --contains "${KOKORO_GITHUB_COMMIT}" \
+      | grep -v HEAD | head -1)
+shopt -s extglob
+branch="${branch//[[:space:]]}"
+branch="${branch##remotes/origin/}"
+shopt -u extglob
+../gradlew installDist -x test -PskipCodegen=true -PskipAndroid=true
+popd
 
-cd examples
-bazel clean
-bazel build ...
+git clone -b "${branch}" --single-branch --depth=1 https://github.com/grpc/grpc.git
+
+grpc/tools/run_tests/helper_scripts/prep_xds.sh
+JAVA_OPTS=-Djava.util.logging.config.file=grpc-java/buildscripts/xds_logging.properties \
+  python3 grpc/tools/run_tests/run_xds_tests.py \
+    --test_case=all \
+    --project_id=grpc-testing \
+    --source_image=projects/grpc-testing/global/images/xds-test-server \
+    --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
+    --gcp_suffix=$(date '+%s') \
+    --verbose \
+    --client_cmd="grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-client \
+      --server=xds:///{server_uri} \
+      --stats_port={stats_port} \
+      --qps={qps}"
