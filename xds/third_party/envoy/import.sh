1f9d05ca1c2a436e3cc9ee7517cd8e6d2877eb2e
Cindy Xue <cindyxue@google.com>
2020-06-10 17:43:07 -0700

    Review comments #1

--- xds/third_party/envoy/import.sh
+++ xds/third_party/envoy/import.sh
@@ -81,11 +81,6 @@ envoy/type/range.proto
 envoy/type/semantic_version.proto
 envoy/type/tracing/v2/custom_tag.proto
 )
-# import info for GoogleAPIs dependency
-VERSION_GOOGLEAPIS=ca1372c6d7bcb199638ebfdb40d2b2660bab7b88
-GIT_REPO_GOOGLEAPIS="https://github.com/googleapis/googleapis.git"
-BASE_DIR_GOOGLEAPIS=googleapis
-FILE_GOOGLEAPIS=google/api/expr/v1alpha1/syntax.proto
 
 pushd `git rev-parse --show-toplevel`/xds/third_party/envoy
 
@@ -94,18 +89,12 @@ tmpdir="$(mktemp -d)"
 trap "rm -rf ${tmpdir}" EXIT
 
 pushd "${tmpdir}"
-git clone -b $BRANCH $GIT_REPO_GOOGLEAPIS
-trap "rm -rf $BASE_DIR_GOOGLEAPIS" EXIT
-cd "$BASE_DIR_GOOGLEAPIS"
-git checkout $VERSION_GOOGLEAPIS
-cd "../"
 git clone -b $BRANCH $GIT_REPO
 trap "rm -rf $GIT_BASE_DIR" EXIT
 cd "$GIT_BASE_DIR"
 git checkout $VERSION
 popd
 
-cp -p "${tmpdir}/${BASE_DIR_GOOGLEAPIS}/LICENSE" LICENSE
 cp -p "${tmpdir}/${GIT_BASE_DIR}/LICENSE" LICENSE
 cp -p "${tmpdir}/${GIT_BASE_DIR}/NOTICE" NOTICE
 
@@ -119,9 +108,6 @@ do
   mkdir -p "$(dirname "${file}")"
   cp -p "${tmpdir}/${SOURCE_PROTO_BASE_DIR}/${file}" "${file}"
 done
-# copy proto files to project directory from GoogleAPIs
-mkdir -p "$(dirname "${FILE_GOOGLEAPIS}")"
-cp -p "${tmpdir}/${BASE_DIR_GOOGLEAPIS}/${FILE_GOOGLEAPIS}" "${FILE_GOOGLEAPIS}"
 popd
 
 popd
