6cd2a968ba4c6cdced295c9fc3220b7dd39c2795
Cindy Xue <cindyxue@google.com>
2020-06-22 20:20:13 -0700

    Init test branch for Cel library

--- xds/src/main/java/io/grpc/xds/XdsCelLibraries.java
+++ xds/src/main/java/io/grpc/xds/XdsCelLibraries.java
@@ -1,72 +1,93 @@
-package io.grpc.xds;
+/*
+ * Copyright 2020 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import io.grpc.Metadata;
-// import io.grpc.xds.InterpreterException;
+package io.grpc.xds;
 
 import com.google.api.expr.v1alpha1.Expr;
 import com.google.common.collect.ImmutableMap;
-
-import javax.annotation.Nullable;
-import java.util.Map;
+import io.grpc.Metadata;
+import io.grpc.xds.InterpreterException;
 import java.util.List;
-import java.lang.Exception;
-
+import java.util.Map;
+import javax.annotation.Nullable;
 
 /** Represent a mock Cel libraries which contains functions needed for CEL evaluation engine. */
 public interface XdsCelLibraries {
-    public interface Interpretable {
-        Object eval(Activation activation) throws InterpreterException;
-    }
 
-    public interface Interpreter {
-        Interpretable createInterpretable(Expr checkedExpr) throws InterpreterException;
-    }
+  /** Represent an expression which can be interpreted repeatedly using a given activation. */
+  public interface Interpretable {
 
-    /** An object which implements dispatching of function calls. */
-    public interface Dispatcher {
+    /** Runs interpretation with the given activation which supplies name/value bindings. */
+    Object eval(Activation activation) throws InterpreterException;
+  }
 
-        /**
-         * Invokes a function based on given parameters.
-         *
-         * @param metadata Metadata used for error reporting.
-         * @param exprId Expression identifier which can be used together with {@code metadata} to get
-         *     information about the dispatch target for error reporting.
-         * @param functionName the logical name of the function being invoked.
-         * @param overloadIds A list of function overload ids. The dispatcher selects the unique overload
-         *     from this list with matching arguments.
-         * @param args The arguments to pass to the function.
-         * @return The result of the function call.
-         * @throws InterpreterException if something goes wrong.
-         */
-        Object dispatch(
-            Metadata metadata, long exprId, String functionName, List<String> overloadIds, Object[] args)
-            throws InterpreterException;
-    }
+  /** Interface to a CEL interpreter. */
+  public interface Interpreter {
+    
+    /**
+    * Creates an interpretable for the given expression.
+    * 
+    * <p>This method may run pre-processing and partial evaluation of the expression it gets passed.
+    */
+    Interpretable createInterpretable(Expr checkedExpr) throws InterpreterException;
+  }
 
-    /** An object which allows to bind names to values. */
-    public abstract class Activation {
+  /** An object which implements dispatching of function calls. */
+  public interface Dispatcher {
 
-        /** Resolves the given name to its value. Returns null if resolution fails. */
+    /**
+     * Invokes a function based on given parameters.
+     *
+     * @param metadata Metadata used for error reporting.
+     * @param exprId Expression identifier which can be used together with {@code metadata} to get
+     *     information about the dispatch target for error reporting.
+     * @param functionName the logical name of the function being invoked.
+     * @param overloadIds A list of function overload ids. The dispatcher selects the unique 
+     *     overload from this list with matching arguments.
+     * @param args The arguments to pass to the function.
+     * @return The result of the function call.
+     * @throws InterpreterException if something goes wrong.
+     */
+    Object dispatch(Metadata metadata, long exprId, 
+        String functionName, List<String> overloadIds, Object[] args)
+        throws InterpreterException;
+  }
+
+  /** An object which allows to bind names to values. */
+  public abstract class Activation {
+
+    /** Resolves the given name to its value. Returns null if resolution fails. */
+    @Nullable
+    public abstract Object resolve(String name);
+
+    /** Creates a binder backed up by a map. */
+    public static Activation copyOf(Map<String, ?> map) {
+      final ImmutableMap<String, Object> copy = ImmutableMap.copyOf(map);
+      return new Activation() {
         @Nullable
-        public abstract Object resolve(String name);
-
-        /** Creates a binder backed up by a map. */
-        public static Activation copyOf(Map<String, ?> map) {
-            final ImmutableMap<String, Object> copy = ImmutableMap.copyOf(map);
-            return new Activation() {
-                @Nullable
-                @Override
-                public Object resolve(String name) {
-                  return copy.get(name);
-                }
-          
-                @Override
-                public String toString() {
-                  return copy.toString();
-                }
-            };
+        @Override
+        public Object resolve(String name) {
+          return copy.get(name);
         }
+    
+        @Override
+        public String toString() {
+          return copy.toString();
+        }
+      };
     }
+  }
 }
-
-class InterpreterException extends Exception {}
--- xds/src/main/java/io/grpc/xds/XdsCelLibrariesDefault.java
+++ xds/src/main/java/io/grpc/xds/XdsCelLibrariesDefault.java
@@ -1,77 +1,104 @@
-package io.grpc.xds;
+/*
+ * Copyright 2020 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import io.grpc.xds.XdsCelLibraries;
-// import io.grpc.xds.InterpreterException;
-import io.grpc.ServerCall;
-import io.grpc.Metadata;
+package io.grpc.xds;
 
 import com.google.api.expr.v1alpha1.Expr;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableSet;
-import com.google.protobuf.MessageLite;
-
-import javax.annotation.Nullable;
-import java.util.ArrayList;
+import io.grpc.Metadata;
+import io.grpc.xds.InterpreterException;
+import io.grpc.xds.XdsCelLibraries;
+import java.lang.String;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Default implementation of the Cel libraries.
+ * 
+ */
 public class XdsCelLibrariesDefault implements XdsCelLibraries {
-    public static void main(String[] args) {
-        XdsCelLibraries.Dispatcher dispatcher = DefaultDispatcher.create();
-        XdsCelLibraries.Interpreter interpreter = new DefaultInterpreter();
-        Expr.Builder exprBuilder = Expr.newBuilder();
-        Expr checkedResult = exprBuilder.build();
 
-        Map<String, ?> apiAttributes = new HashMap<>();
-        XdsCelLibraries.Activation activation = XdsCelLibraries.Activation.copyOf(apiAttributes);
+  /** Main launches the server from the command line. */
+  public static void main(String[] args) throws InterpreterException {
+    XdsCelLibraries.Dispatcher dispatcher = DefaultDispatcher.create();
+    XdsCelLibraries.Interpreter interpreter = new DefaultInterpreter();
+    Expr.Builder exprBuilder = Expr.newBuilder();
+    Expr checkedResult = exprBuilder.build();
 
-        try {
-            Object result = interpreter.createInterpretable(checkedResult).eval(activation);
-        } catch (InterpreterException e) {
+    Map<String, Object> map = new HashMap<>();
+    map.put("requestUrlPath", new Object());
+    map.put("requestHost", new Object());
+    map.put("requestMethod", new Object());
+    map.put("requestHeaders", new Object());
+    map.put("sourceAddress", new Object());
+    map.put("sourcePort", new Object());
+    map.put("destinationAddress", new Object());
 
-        }
-        
+    // ImmutableMap<String, Object> apiAttributes = ImmutableMap.builder()
+    //     .put("requestUrlPath", new Object())
+    //     .put("requestHost", new Object())
+    //     .put("requestMethod", new Object())
+    //     .put("requestHeaders", new Object())
+    //     .put("sourceAddress", new Object())
+    //     .put("sourcePort", new Object())
+    //     .build();
+
+    ImmutableMap<String, Object> apiAttributes = ImmutableMap.copyOf(map);
+
+    XdsCelLibraries.Activation activation = XdsCelLibraries.Activation.copyOf(apiAttributes);
+    Object result = interpreter.createInterpretable(checkedResult).eval(activation);
+  }
+
+  public static class DefaultInterpreter implements XdsCelLibraries.Interpreter {
+    @Override
+    public XdsCelLibraries.Interpretable createInterpretable(Expr expr) 
+    throws InterpreterException {
+      return new DefaultInterpretable(expr);
     }
 
-    public static class DefaultInterpreter implements XdsCelLibraries.Interpreter {
-        public DefaultInterpreter() {
+    private class DefaultInterpretable implements XdsCelLibraries.Interpretable {
+      private final Expr expr;
+      private final Metadata metadata;
 
-        }
+      public DefaultInterpretable(Expr expr) {
+        this.metadata = new Metadata();
+        this.expr = expr;
+      }
 
-        @Override
-        public XdsCelLibraries.Interpretable createInterpretable(Expr expr) throws InterpreterException {
-            return new DefaultInterpretable(expr);
-        }
+      @Override
+      public Object eval(XdsCelLibraries.Activation activation) throws InterpreterException {
+        return new Object();
+      }
+    }
+  }
 
-        private class DefaultInterpretable implements XdsCelLibraries.Interpretable {
-            private final Expr expr;
-            private final Metadata metadata;
+  public static class DefaultDispatcher implements XdsCelLibraries.Dispatcher {
 
-            public DefaultInterpretable(Expr expr) {
-                this.metadata = new Metadata();
-                this.expr = expr;
-            }
-
-            @Override
-            public Object eval(XdsCelLibraries.Activation activation) throws InterpreterException {
-                return new Object();
-            }
-        }
+    /** Creates a new dispatcher with all standard functions. */
+    public static DefaultDispatcher create() {
+      return new DefaultDispatcher();
     }
 
-    public static class DefaultDispatcher implements XdsCelLibraries.Dispatcher {
-        public static DefaultDispatcher create() {
-            return new DefaultDispatcher();
-        }
-
-        @Override
-        public Object dispatch(
-            Metadata metadata, long exprId, String functionName, List<String> overloadIds, Object[] args) 
-            throws InterpreterException {
-                return new Object();
-        }
+    @Override
+    public Object dispatch(Metadata metadata, long exprId, 
+        String functionName, List<String> overloadIds, Object[] args) 
+        throws InterpreterException {
+      return new Object();
     }
+  }
 }
 
