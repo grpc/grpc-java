28146fae25f65221648f3df62732a7a808d0d8c1
Penn (Dapeng) Zhang <zdapeng@google.com>
2020-07-08 15:42:39 -0700

    one more fix

--- xds/src/main/java/io/grpc/xds/EnvoyProtoData.java
+++ xds/src/main/java/io/grpc/xds/EnvoyProtoData.java
@@ -418,6 +418,7 @@ final class EnvoyProtoData {
           break;
         case MILLION:
           break;
+        case UNRECOGNIZED:
         default:
           throw new IllegalArgumentException("Unknown denominator type of " + percent);
       }
