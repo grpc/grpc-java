6cba0a552ac1f9e6b5537f6a0fa6d85664c35af9
ZhenLian <zhenlian@google.com>
2020-07-17 17:59:54 -0700

    xds: change package name for CEL lib

--- xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Interpretable.java
+++ xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Interpretable.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.grpc.xds.internal.cel;
+package io.grpc.xds.internal.rbac.engine.cel;
 
 /**
  * Represent an expression which can be interpreted repeatedly using a given activation.
--- xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Metadata.java
+++ xds/src/main/java/io/grpc/xds/internal/rbac/engine/cel/Metadata.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.grpc.xds.internal.cel;
+package io.grpc.xds.internal.rbac.engine.cel;
 
 import java.lang.String;
 
