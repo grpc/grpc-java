69ea958acc14b251fa46aad7b72613f63ab9afb8
Chengyuan Zhang <chengyuanzhang@google.com>
2020-08-11 18:18:12 -0700

    Fix parameter comments that do not match the formal parameter name.

--- xds/src/test/java/io/grpc/xds/XdsClientImplTest.java
+++ xds/src/test/java/io/grpc/xds/XdsClientImplTest.java
@@ -744,43 +744,42 @@ public class XdsClientImplTest {
     verify(configWatcher).onConfigChanged(configUpdateCaptor.capture());
     List<EnvoyProtoData.Route> routes = configUpdateCaptor.getValue().getRoutes();
     assertThat(routes).hasSize(4);
-    assertThat(routes.get(0)).isEqualTo(
-        new EnvoyProtoData.Route(
-            // path match with cluster route
-            new io.grpc.xds.RouteMatch(
-                /* prefix= */ null,
-                /* path= */ "/service1/method1"),
-            new EnvoyProtoData.RouteAction(
-                TimeUnit.SECONDS.toNanos(15L), "cl1.googleapis.com", null)));
-    assertThat(routes.get(1)).isEqualTo(
-        new EnvoyProtoData.Route(
-            // path match with weighted cluster route
-            new io.grpc.xds.RouteMatch(
-                /* prefix= */ null,
-                /* path= */ "/service2/method2"),
-            new EnvoyProtoData.RouteAction(
-                TimeUnit.SECONDS.toNanos(15L),
-                null,
-                ImmutableList.of(
-                    new EnvoyProtoData.ClusterWeight("cl21.googleapis.com", 30),
-                    new EnvoyProtoData.ClusterWeight("cl22.googleapis.com", 70)
-                ))));
-    assertThat(routes.get(2)).isEqualTo(
-        new EnvoyProtoData.Route(
-            // prefix match with cluster route
-            new io.grpc.xds.RouteMatch(
-                /* prefix= */ "/service1/",
-                /* path= */ null),
-            new EnvoyProtoData.RouteAction(
-                TimeUnit.SECONDS.toNanos(15L), "cl1.googleapis.com", null)));
-    assertThat(routes.get(3)).isEqualTo(
-        new EnvoyProtoData.Route(
-            // default match with cluster route
-            new io.grpc.xds.RouteMatch(
-                /* prefix= */ "",
-                /* path= */ null),
-            new EnvoyProtoData.RouteAction(
-                TimeUnit.SECONDS.toNanos(15L), "cluster.googleapis.com", null)));
+    assertThat(routes.get(0))
+        .isEqualTo(
+            new EnvoyProtoData.Route(
+                // path match with cluster route
+                new io.grpc.xds.RouteMatch(
+                    /* pathPrefixMatch= */ null,/* pathExactMatch= */ "/service1/method1"),
+                new EnvoyProtoData.RouteAction(
+                    TimeUnit.SECONDS.toNanos(15L), "cl1.googleapis.com", null)));
+    assertThat(routes.get(1))
+        .isEqualTo(
+            new EnvoyProtoData.Route(
+                // path match with weighted cluster route
+                new io.grpc.xds.RouteMatch(
+                    /* pathPrefixMatch= */ null,/* pathExactMatch= */ "/service2/method2"),
+                new EnvoyProtoData.RouteAction(
+                    TimeUnit.SECONDS.toNanos(15L),
+                    null,
+                    ImmutableList.of(
+                        new EnvoyProtoData.ClusterWeight("cl21.googleapis.com", 30),
+                        new EnvoyProtoData.ClusterWeight("cl22.googleapis.com", 70)))));
+    assertThat(routes.get(2))
+        .isEqualTo(
+            new EnvoyProtoData.Route(
+                // prefix match with cluster route
+                new io.grpc.xds.RouteMatch(
+                    /* pathPrefixMatch= */ "/service1/",/* pathExactMatch= */ null),
+                new EnvoyProtoData.RouteAction(
+                    TimeUnit.SECONDS.toNanos(15L), "cl1.googleapis.com", null)));
+    assertThat(routes.get(3))
+        .isEqualTo(
+            new EnvoyProtoData.Route(
+                // default match with cluster route
+                new io.grpc.xds.RouteMatch(
+                    /* pathPrefixMatch= */ "",/* pathExactMatch= */ null),
+                new EnvoyProtoData.RouteAction(
+                    TimeUnit.SECONDS.toNanos(15L), "cluster.googleapis.com", null)));
   }
 
   /**
