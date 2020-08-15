19465bfa3c2ff27208dc6c78c2d6ecef361231b4
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2020-08-14 17:41:16 -0700

    address review comment: make method protected final

--- xds/src/main/java/io/grpc/xds/internal/certprovider/CertProviderClientSslContextProvider.java
+++ xds/src/main/java/io/grpc/xds/internal/certprovider/CertProviderClientSslContextProvider.java
@@ -54,7 +54,7 @@ final class CertProviderClientSslContextProvider extends CertProviderSslContextP
   }
 
   @Override
-  public SslContextBuilder getSslContextBuilder(
+  protected final SslContextBuilder getSslContextBuilder(
           CertificateValidationContext certificateValidationContextdationContext)
       throws CertStoreException {
     SslContextBuilder sslContextBuilder =
