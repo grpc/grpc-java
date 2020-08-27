68aab1af647afcd43fa7a072c182c2490d51c2f5
Chengyuan Zhang <chengyuanzhang@google.com>
2020-08-26 17:32:46 -0700

    Fix log typo.

--- xds/src/main/java/io/grpc/xds/CdsLoadBalancer.java
+++ xds/src/main/java/io/grpc/xds/CdsLoadBalancer.java
@@ -229,7 +229,7 @@ final class CdsLoadBalancer extends LoadBalancer {
 
     @Override
     public void onError(Status error) {
-      logger.log(XdsLogLevel.WARNING, "Received error from xDS client {0}: {1}", error);
+      logger.log(XdsLogLevel.WARNING, "Received error from xDS client {0}: {1}", xdsClient, error);
       if (lb == null) {
         helper.updateBalancingState(TRANSIENT_FAILURE, new ErrorPicker(error));
       }
