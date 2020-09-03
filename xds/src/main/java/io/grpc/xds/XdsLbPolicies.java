79b65a6c8754b8f6f3f05d5805db78784b2347f5
Chengyuan Zhang <chengyuanzhang@google.com>
2020-09-03 10:12:40 -0700

    Add missing LB policies for service loading.

--- xds/src/main/java/io/grpc/xds/XdsLbPolicies.java
+++ xds/src/main/java/io/grpc/xds/XdsLbPolicies.java
@@ -20,7 +20,10 @@ final class XdsLbPolicies {
   static final String CLUSTER_MANAGER_POLICY_NAME = "cluster_manager_experimental";
   static final String CDS_POLICY_NAME = "cds_experimental";
   static final String EDS_POLICY_NAME = "eds_experimental";
+  static final String PRIORITY_POLICY_NAME = "priority_experimental";
   static final String WEIGHTED_TARGET_POLICY_NAME = "weighted_target_experimental";
+  static final String LRS_POLICY_NAME = "lrs_experimental";
+  // TODO(chengyuanzhang): delete routing policy.
   static final String XDS_ROUTING_POLICY_NAME = "xds_routing_experimental";
 
   private XdsLbPolicies() {}
