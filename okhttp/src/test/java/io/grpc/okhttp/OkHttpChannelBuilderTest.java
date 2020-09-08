5c75b85d73e00e4967bfca359522ed3cc0e2fb44
Sergii Tkachenko <sergiitk@google.com>
2020-09-08 13:20:29 -0400

    Add checkAuthorityOverrideAllowsInvalidAuthority test()

--- okhttp/src/test/java/io/grpc/okhttp/OkHttpChannelBuilderTest.java
+++ okhttp/src/test/java/io/grpc/okhttp/OkHttpChannelBuilderTest.java
@@ -81,6 +81,18 @@ public class OkHttpChannelBuilderTest {
   }
 
   @Test
+  @Deprecated
+  public void checkAuthorityOverrideAllowsInvalidAuthority() {
+    OkHttpChannelBuilder builder = new OkHttpChannelBuilder("good", 1234) {
+      @Override
+      protected String checkAuthority(String authority) {
+        return authority;
+      }
+    };
+    builder.overrideAuthority("[invalidauthority").usePlaintext().buildTransportFactory();
+  }
+
+  @Test
   public void disableCheckAuthorityAllowsInvalidAuthority() {
     OkHttpChannelBuilder builder = new OkHttpChannelBuilder("good", 1234)
         .disableCheckAuthority();
