5bd6695a47e69d798e79b01b50ac2a55fcd43b2a
Penn (Dapeng) Zhang <zdapeng@google.com>
2020-10-08 17:20:22 -0700

    rls: fix RPC hanging if lookup request fails

--- rls/src/test/java/io/grpc/rls/CachingRlsLbClientTest.java
+++ rls/src/test/java/io/grpc/rls/CachingRlsLbClientTest.java
@@ -25,7 +25,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -41,13 +40,16 @@ import io.grpc.EquivalentAddressGroup;
 import io.grpc.ForwardingChannelBuilder;
 import io.grpc.LoadBalancer;
 import io.grpc.LoadBalancer.Helper;
+import io.grpc.LoadBalancer.PickResult;
 import io.grpc.LoadBalancer.SubchannelPicker;
 import io.grpc.LoadBalancerProvider;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.Metadata;
 import io.grpc.NameResolver;
+import io.grpc.NameResolver.ConfigOrError;
 import io.grpc.Status;
+import io.grpc.Status.Code;
 import io.grpc.SynchronizationContext;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
@@ -102,6 +104,7 @@ public class CachingRlsLbClientTest {
 
   private static final RouteLookupConfig ROUTE_LOOKUP_CONFIG = getRouteLookupConfig();
   private static final int SERVER_LATENCY_MILLIS = 10;
+  private static final String DEFAULT_TARGET = "fallback.cloudbigtable.googleapis.com";
 
   @Rule
   public final MockitoRule mocks = MockitoJUnit.rule();
@@ -278,7 +281,9 @@ public class CachingRlsLbClientTest {
     rlsServerImpl.setLookupTable(
         ImmutableMap.of(
             routeLookupRequest,
-            new RouteLookupResponse(ImmutableList.of("target"), "header-rls-data-value")));
+            new RouteLookupResponse(
+                ImmutableList.of("primary.cloudbigtable.googleapis.com"),
+                "header-rls-data-value")));
 
     // valid channel
     CachedRouteLookupResponse resp = getInSyncContext(routeLookupRequest);
@@ -298,11 +303,13 @@ public class CachingRlsLbClientTest {
     assertThat(stateCaptor.getAllValues())
         .containsExactly(ConnectivityState.CONNECTING, ConnectivityState.READY);
     Metadata headers = new Metadata();
-    pickerCaptor.getValue().pickSubchannel(
+    PickResult pickResult = pickerCaptor.getValue().pickSubchannel(
         new PickSubchannelArgsImpl(
             TestMethodDescriptors.voidMethod().toBuilder().setFullMethodName("foo/bar").build(),
             headers,
             CallOptions.DEFAULT));
+    assertThat(pickResult.getStatus().isOk()).isTrue();
+    assertThat(pickResult.getSubchannel()).isNotNull();
     assertThat(headers.get(RLS_DATA_KEY)).isEqualTo("header-rls-data-value");
 
     // move backoff further back to only test error behavior
@@ -310,7 +317,7 @@ public class CachingRlsLbClientTest {
     // try to get invalid
     RouteLookupRequest invalidRouteLookupRequest =
         new RouteLookupRequest(
-            "unknown_server", "/doesn/exists", "grpc", ImmutableMap.<String, String>of());
+            "service1", "/doesn/exists", "grpc", ImmutableMap.<String, String>of());
     CachedRouteLookupResponse errorResp = getInSyncContext(invalidRouteLookupRequest);
     assertThat(errorResp.isPending()).isTrue();
     fakeTimeProvider.forwardTime(SERVER_LATENCY_MILLIS, TimeUnit.MILLISECONDS);
@@ -318,8 +325,19 @@ public class CachingRlsLbClientTest {
     errorResp = getInSyncContext(invalidRouteLookupRequest);
     assertThat(errorResp.hasError()).isTrue();
 
-    inOrder.verify(helper, never())
-        .updateBalancingState(any(ConnectivityState.class), any(SubchannelPicker.class));
+    // Channel is still READY because the subchannel for method /foo/bar is still READY.
+    // Method /doesn/exists will use fallback child balancer and fail immediately.
+    inOrder.verify(helper)
+        .updateBalancingState(eq(ConnectivityState.READY), pickerCaptor.capture());
+    pickResult = pickerCaptor.getValue().pickSubchannel(
+        new PickSubchannelArgsImpl(
+            TestMethodDescriptors.voidMethod().toBuilder()
+                .setFullMethodName("doesn/exists")
+                .build(),
+            headers,
+            CallOptions.DEFAULT));
+    assertThat(pickResult.getStatus().getCode()).isEqualTo(Code.UNAVAILABLE);
+    assertThat(pickResult.getStatus().getDescription()).isEqualTo("fallback not available");
   }
 
   @Test
@@ -370,7 +388,7 @@ public class CachingRlsLbClientTest {
         /* staleAgeInMillis= */ TimeUnit.SECONDS.toMillis(240),
         /* cacheSizeBytes= */ 1000,
         /* validTargets= */ ImmutableList.of("a valid target"),
-        /* defaultTarget= */ "us_east_1.cloudbigtable.googleapis.com");
+        DEFAULT_TARGET);
   }
 
   private static BackoffPolicy createBackoffPolicy(final long delay, final TimeUnit unit) {
@@ -395,6 +413,10 @@ public class CachingRlsLbClientTest {
     }
   }
 
+  /**
+   * A load balancer that immediately goes to READY when using the rls response target and
+   * immediately fails when using the fallback target.
+   */
   private static final class TestLoadBalancerProvider extends LoadBalancerProvider {
 
     @Override
@@ -413,20 +435,38 @@ public class CachingRlsLbClientTest {
     }
 
     @Override
+    public ConfigOrError parseLoadBalancingPolicyConfig(
+        Map<String, ?> rawLoadBalancingPolicyConfig) {
+      return ConfigOrError.fromConfig(rawLoadBalancingPolicyConfig);
+    }
+
+    @Override
     public LoadBalancer newLoadBalancer(final Helper helper) {
       return new LoadBalancer() {
 
         @Override
         public void handleResolvedAddresses(ResolvedAddresses resolvedAddresses) {
-          // TODO: make the picker accessible
-          helper.updateBalancingState(
-              ConnectivityState.READY,
-              new SubchannelPicker() {
-                @Override
-                public PickResult pickSubchannel(PickSubchannelArgs args) {
-                  return PickResult.withSubchannel(mock(Subchannel.class));
-                }
-              });
+          Map<?, ?> config = (Map<?, ?>) resolvedAddresses.getLoadBalancingPolicyConfig();
+          if (DEFAULT_TARGET.equals(config.get("target"))) {
+            helper.updateBalancingState(
+                ConnectivityState.TRANSIENT_FAILURE,
+                new SubchannelPicker() {
+                  @Override
+                  public PickResult pickSubchannel(PickSubchannelArgs args) {
+                    return PickResult.withError(
+                        Status.UNAVAILABLE.withDescription("fallback not available"));
+                  }
+                });
+          } else {
+            helper.updateBalancingState(
+                ConnectivityState.READY,
+                new SubchannelPicker() {
+                  @Override
+                  public PickResult pickSubchannel(PickSubchannelArgs args) {
+                    return PickResult.withSubchannel(mock(Subchannel.class));
+                  }
+                });
+          }
         }
 
         @Override
