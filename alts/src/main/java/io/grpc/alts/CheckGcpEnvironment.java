24132c801bd78a21621b124ec243e87be23580e7
Elliotte Rusty Harold <elharo@google.com>
2020-12-23 12:25:06 -0500

    import Locale

--- alts/src/main/java/io/grpc/alts/CheckGcpEnvironment.java
+++ alts/src/main/java/io/grpc/alts/CheckGcpEnvironment.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
