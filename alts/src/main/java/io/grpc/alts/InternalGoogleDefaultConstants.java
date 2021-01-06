f3f84c6edb8ea18a3d9bbe0b07ee4dd6b64a4dbf
Chengyuan Zhang <chengyuanzhang@google.com>
2021-01-06 14:54:36 -0800

    Define internal constants for xDS cluster name attribute.

---
+++ alts/src/main/java/io/grpc/alts/InternalGoogleDefaultConstants.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.alts;
+
+import io.grpc.Attributes;
+import io.grpc.EquivalentAddressGroup;
+import io.grpc.Internal;
+
+/**
+ * Internal constants used with {@link GoogleDefaultChannelCredentials}. This is intended for
+ * usage internal to the gRPC team. If you *really* think you need to use this, contact the
+ * gRPC team first.
+ */
+@Internal
+public class InternalGoogleDefaultConstants {
+
+  /**
+   * Name of the cluster (in xDS concept) that provides this EquivalentAddressGroup.
+   */
+  @EquivalentAddressGroup.Attr
+  public static final Attributes.Key<String> ATTR_XDS_CLUSTER_NAME =
+      Attributes.Key.create("io.grpc.alts.xdsClusterName");
+
+  /**
+   * Cluster name (in xDS concept) representing a Google CFE.
+   */
+  public static final String GOOGLE_CFE_CLUSTER_NAME = "google_cfe";
+
+  private InternalGoogleDefaultConstants() {}
+}
