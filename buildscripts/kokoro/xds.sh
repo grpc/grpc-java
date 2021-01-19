56fc8b8626a25c1a123100b569e9ae9c2f33cd22
ZHANG Dapeng <zdapeng@google.com>
2021-01-19 15:01:21 -0800

    buildscripts: add xds_v3 buildscripts

--- buildscripts/kokoro/xds.sh
+++ buildscripts/kokoro/xds.sh
@@ -30,14 +30,15 @@ JAVA_OPTS=-Djava.util.logging.config.file=grpc-java/buildscripts/xds_logging.pro
   python3 grpc/tools/run_tests/run_xds_tests.py \
     --test_case="all,path_matching,header_matching,circuit_breaking" \
     --project_id=grpc-testing \
+    --project_num=830293263384 \
     --source_image=projects/grpc-testing/global/images/xds-test-server-3 \
     --path_to_server_binary=/java_server/grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-server \
     --gcp_suffix=$(date '+%s') \
     --verbose \
+    ${XDS_V3_OPT} \
     --client_cmd="grpc-java/interop-testing/build/install/grpc-interop-testing/bin/xds-test-client \
       --server=xds:///{server_uri} \
       --stats_port={stats_port} \
       --qps={qps} \
       {rpcs_to_send} \
       {metadata_to_send}"
-
