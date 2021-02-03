f36d515e6409ff1a3eadc74dc87ced308d91f29c
Chengyuan Zhang <chengyuanzhang@google.com>
2021-02-03 15:52:55 -0800

    Create autovalue classes for stats.

---
+++ xds/src/main/java/io/grpc/xds/Stats.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2021 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.xds;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+import jdk.internal.jline.internal.Nullable;
+
+final class Stats {
+  private Stats() {}
+
+  @AutoValue
+  abstract static class ClusterStats {
+    abstract String clusterName();
+
+    @Nullable
+    abstract String clusterServiceName();
+
+    abstract ImmutableList<UpstreamLocalityStats> upstreamLocalityStatsList();
+
+    abstract ImmutableList<DroppedRequests> droppedRequestsList();
+
+    abstract long totalDroppedRequests();
+
+    abstract long loadReportIntervalNano();
+
+    static Builder newBuilder() {
+      return new AutoValue_Stats_ClusterStats.Builder()
+          .totalDroppedRequests(0L)  // default initialization
+          .loadReportIntervalNano(0L);
+    }
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder clusterName(String clusterName);
+
+      abstract Builder clusterServiceName(String clusterServiceName);
+
+      abstract ImmutableList.Builder<UpstreamLocalityStats> upstreamLocalityStatsListBuilder();
+
+      abstract ImmutableList.Builder<DroppedRequests> droppedRequestsListBuilder();
+
+      abstract Builder totalDroppedRequests(long totalDroppedRequests);
+
+      abstract Builder loadReportIntervalNano(long loadReportIntervalNano);
+
+      abstract long loadReportIntervalNano();
+
+      abstract ClusterStats build();
+    }
+  }
+
+  @AutoValue
+  abstract static class DroppedRequests {
+    abstract String category();
+
+    abstract long droppedCount();
+
+    static DroppedRequests create(String category, long droppedCount) {
+      return new AutoValue_Stats_DroppedRequests(category, droppedCount);
+    }
+  }
+
+  @AutoValue
+  abstract static class UpstreamLocalityStats {
+    abstract Locality locality();
+
+    abstract long totalIssuedRequests();
+
+    abstract long totalSuccessfulRequests();
+
+    abstract long totalErrorRequests();
+
+    abstract long totalRequestsInProgress();
+
+    static UpstreamLocalityStats create(Locality locality, long totalIssuedRequests,
+        long totalSuccessfulRequests, long totalErrorRequests, long totalRequestsInProgress) {
+      checkNotNull(locality, "locality");
+      return new AutoValue_Stats_UpstreamLocalityStats(locality, totalIssuedRequests,
+          totalSuccessfulRequests, totalErrorRequests, totalRequestsInProgress);
+    }
+  }
+}
