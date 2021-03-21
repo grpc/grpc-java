6eff2b8a44c31e22941c9baecedd2837d08ccef3
Mark Brophy <mbrophy@google.com>
2021-03-21 20:58:19 +0000

    Actually include the recent BindServiceFlags class & test

---
+++ binder/src/test/java/io/grpc/binder/util/BindServiceFlagsTest.java
@@ -0,0 +1,79 @@
+package com.google.frameworks.client.data.android.binder;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Context;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class BindServiceFlagsTest {
+
+  @Test
+  public void shouldAutoCreateByDefault() {
+    assertThat(BindServiceFlags.DEFAULTS.toInteger() & Context.BIND_AUTO_CREATE).isNotEqualTo(0);
+  }
+
+  @Test
+  public void shouldCheckForInequality() {
+    assertThat(BindServiceFlags.newBuilder().setAutoCreate(true).build())
+        .isNotEqualTo(BindServiceFlags.newBuilder().build());
+  }
+
+  @Test
+  public void shouldCheckForEquality() {
+    assertThat(BindServiceFlags.DEFAULTS).isEqualTo(BindServiceFlags.DEFAULTS.toBuilder().build());
+  }
+
+  @Test
+  public void shouldReflectResetFlags() {
+    assertThat(
+            BindServiceFlags.newBuilder()
+                .setAutoCreate(true)
+                .setAutoCreate(false)
+                .setAutoCreate(true)
+                .build()
+                .toInteger())
+        .isEqualTo(Context.BIND_AUTO_CREATE);
+  }
+
+  @Test
+  public void shouldReflectReclearedFlags() {
+    assertThat(
+            BindServiceFlags.newBuilder()
+                .setAutoCreate(false)
+                .setAutoCreate(true)
+                .setAutoCreate(false)
+                .build()
+                .toInteger())
+        .isEqualTo(0);
+  }
+
+  @Test
+  public void shouldReflectSetFlags() {
+    assertThat(
+            BindServiceFlags.newBuilder()
+                .setAutoCreate(true)
+                .setAdjustWithActivity(true)
+                .setAboveClient(true)
+                .setAllowOomManagement(true)
+                .setImportant(true)
+                .setIncludeCapabilities(true)
+                .setNotForeground(true)
+                .setNotPerceptible(true)
+                .setWaivePriority(true)
+                .build()
+                .toInteger())
+        .isEqualTo(
+            Context.BIND_AUTO_CREATE
+                | Context.BIND_ADJUST_WITH_ACTIVITY
+                | Context.BIND_ABOVE_CLIENT
+                | Context.BIND_ALLOW_OOM_MANAGEMENT
+                | Context.BIND_IMPORTANT
+                | Context.BIND_INCLUDE_CAPABILITIES
+                | Context.BIND_NOT_FOREGROUND
+                | Context.BIND_NOT_PERCEPTIBLE
+                | Context.BIND_WAIVE_PRIORITY);
+  }
+}
