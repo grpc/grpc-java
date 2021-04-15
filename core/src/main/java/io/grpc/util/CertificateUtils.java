979f7cc04783c7e96f61952c77bbc540848fac2d
ZhenLian <zhenlian@google.com>
2021-04-15 15:01:19 -0700

    resolve some comments

--- core/src/main/java/io/grpc/util/CertificateUtils.java
+++ core/src/main/java/io/grpc/util/CertificateUtils.java
@@ -39,46 +39,45 @@ import java.util.regex.Pattern;
  */
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/8024")
 public final class CertificateUtils {
-  private static CertificateFactory factory;
   private static final Pattern KEY_PATTERN = Pattern.compile(
-      "-+BEGIN\\s+.*PRIVATE\\s+KEY[^-]*-+(?:\\s|\\r|\\n)+" + // Header
+      "-----BEGIN\\s+.*PRIVATE\\s+KEY-----" + // Header
           "([a-z0-9+/=\\r\\n]+)" +                             // Base64 text
-          "-+END\\s+.*PRIVATE\\s+KEY[^-]*-+",                  // Footer
+          "-----END\\s+.*PRIVATE\\s+KEY-----",                  // Footer
       Pattern.CASE_INSENSITIVE);
-  private static final BaseEncoding BASE64_ENCODING = BaseEncoding.base64();
-
-  private static synchronized void initInstance() throws CertificateException {
-    if (factory == null) {
-      factory = CertificateFactory.getInstance("X.509");
-    }
-  }
 
   /**
-   * Generates X509Certificate array from a file on disk.
+   * Generates X509Certificate array from a PEM file.
+   * The PEM file should contain one or more items in Base64 encoding, each with
+   * plain-text headers and footers
+   * (e.g. -----BEGIN CERTIFICATE----- and -----END CERTIFICATE-----).
    *
    * @param inputStream is a {@link InputStream} from the certificate files
    */
   public static X509Certificate[] getX509Certificates(InputStream inputStream)
       throws CertificateException {
-    initInstance();
+    CertificateFactory factory = CertificateFactory.getInstance("X.509");
     Collection<? extends Certificate> certs = factory.generateCertificates(inputStream);
     return certs.toArray(new X509Certificate[0]);
   }
 
-  /** Generates a {@link PrivateKey} from the {@link InputStream}. */
+  /**
+   * Generates a {@link PrivateKey} from a PEM file.
+   * The key should be PKCS #8 formatted.
+   * The PEM file should contain one item in Base64 encoding, with plain-text headers and footers
+   * (e.g. -----BEGIN PRIVATE KEY----- and -----END PRIVATE KEY-----).
+   *
+   * @param inputStream is a {@link InputStream} from the private key file
+   */
   public static PrivateKey getPrivateKey(InputStream inputStream)
       throws Exception {
     String key = CharStreams.toString(new InputStreamReader(inputStream, Charsets.UTF_8));
     Matcher m = KEY_PATTERN.matcher(key);
-    if (!m.find()) {
+    if (!m.find() || m.groupCount() != 1) {
       throw new KeyException("could not find a PKCS #8 private key in input stream");
     }
-    // Using System.lineSeparator() here would cause
-    // [Undefined reference | android-api-level-14-4.0_r4] io.grpc.util.(CertificateUtils.java:76)
-    // Is it not supported on Android?
     String keyContent =
         m.group(1).replace(System.getProperty("line.separator"), "");
-    byte[] decodedKeyBytes = BASE64_ENCODING.decode(keyContent);
+    byte[] decodedKeyBytes = BaseEncoding.base64().decode(keyContent);
     KeyFactory keyFactory = KeyFactory.getInstance("RSA");
     PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decodedKeyBytes);
     return keyFactory.generatePrivate(keySpec);
