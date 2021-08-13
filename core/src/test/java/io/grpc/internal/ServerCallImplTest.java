c6f67a468586fb67d1f76dad4d3e7cfebe0386e0
Terry Wilson <tmwilson@google.com>
2021-08-13 16:41:18 -0700

    Fix import order in ServerCallImplTest.java.

--- core/src/test/java/io/grpc/internal/ServerCallImplTest.java
+++ core/src/test/java/io/grpc/internal/ServerCallImplTest.java
@@ -18,8 +18,8 @@ package io.grpc.internal;
 
 import static com.google.common.base.Charsets.UTF_8;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
