7f9970e78e551b9dcbcfaae30104a326148f770a
Eric Anderson <ejona@google.com>
2021-08-25 12:10:44 -0700

    stub: Document StreamObserver is an async API
    Missing docs were brought up in #8423

--- stub/src/main/java/io/grpc/stub/StreamObserver.java
+++ stub/src/main/java/io/grpc/stub/StreamObserver.java
@@ -31,6 +31,11 @@ package io.grpc.stub;
  * not need to be synchronized together; incoming and outgoing directions are independent.
  * Since individual {@code StreamObserver}s are not thread-safe, if multiple threads will be
  * writing to a {@code StreamObserver} concurrently, the application must synchronize calls.
+ *
+ * <p>This API is asynchronous, so methods may return before the operation completes. gRPC
+ * guarantees it does not block on I/O in its implementation, but applications are allowed to
+ * perform blocking operations in their implementations. However, doing so will delay other
+ * callbacks because the methods cannot be called concurrently.
  */
 public interface StreamObserver<V>  {
   /**
