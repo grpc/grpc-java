6e89919e3265eadd33dc7e154ba291d46563f286
Eric Anderson <ejona@google.com>
2021-09-13 11:30:19 -0700

    netty: Requests with Connection header are malformed
    Although this is part of HTTP/2 and should have already been handled
    already, it was noticed as part of RBAC work to avoid matching
    hop-by-hop headers. See gRFC A41.
    
    Also add a warning if creating Metadata.Key for "Connection". Use this
    to try to help diagnose a client if it happens to blindly copy headers
    from HTTP/1, as PROTOCOL_ERROR is hard to debug.

--- netty/src/main/java/io/grpc/netty/NettyServerHandler.java
+++ netty/src/main/java/io/grpc/netty/NettyServerHandler.java
@@ -26,6 +26,7 @@ import static io.grpc.netty.Utils.CONTENT_TYPE_HEADER;
 import static io.grpc.netty.Utils.HTTP_METHOD;
 import static io.grpc.netty.Utils.TE_HEADER;
 import static io.grpc.netty.Utils.TE_TRAILERS;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
 import static io.netty.handler.codec.http2.DefaultHttp2LocalFlowController.DEFAULT_WINDOW_UPDATE_RATIO;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -375,6 +376,12 @@ class NettyServerHandler extends AbstractNettyHandler {
   private void onHeadersRead(ChannelHandlerContext ctx, int streamId, Http2Headers headers)
       throws Http2Exception {
     try {
+      // Connection-specific header fields makes a request malformed. Ideally this would be handled
+      // by Netty. RFC 7540 section 8.1.2.2
+      if (headers.contains(CONNECTION)) {
+        resetStream(ctx, streamId, Http2Error.PROTOCOL_ERROR.code(), ctx.newPromise());
+        return;
+      }
 
       // Remove the leading slash of the path and get the fully qualified method name
       CharSequence path = headers.path();
