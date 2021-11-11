c8182898e53b133eb556264949c889e47be52b4e
yifeizhuang <zivy@google.com>
2021-11-11 11:58:45 -0800

    xds: remove filter chain uuid name generator (#8663) (#8688)
    Generating a uuid in filterChain breaks the de-duplication detection which causes XdsServer to cycle connections, so removing it.
    An empty name is now allowed. The name is currently only used for debug purpose.

--- xds/src/test/java/io/grpc/xds/ClientXdsClientDataTest.java
+++ xds/src/test/java/io/grpc/xds/ClientXdsClientDataTest.java
@@ -1587,7 +1587,7 @@ public class ClientXdsClientDataTest {
   }
 
   @Test
-  public void parseFilterChain_noName_generatedUuid() throws ResourceInvalidException {
+  public void parseFilterChain_noName() throws ResourceInvalidException {
     FilterChain filterChain1 =
         FilterChain.newBuilder()
             .setFilterChainMatch(FilterChainMatch.getDefaultInstance())
@@ -1615,7 +1615,7 @@ public class ClientXdsClientDataTest {
     EnvoyServerProtoData.FilterChain parsedFilterChain2 = ClientXdsClient.parseFilterChain(
         filterChain2, new HashSet<String>(), null, filterRegistry, null,
         null, true /* does not matter */);
-    assertThat(parsedFilterChain1.getName()).isNotEqualTo(parsedFilterChain2.getName());
+    assertThat(parsedFilterChain1.getName()).isEqualTo(parsedFilterChain2.getName());
   }
 
   @Test
