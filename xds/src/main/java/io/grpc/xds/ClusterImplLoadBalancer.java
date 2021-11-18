2f4a1f9f1b7a1dbe1feacf8ccaa755e933704039
yifeizhuang <zivy@google.com>
2021-11-18 11:59:22 -0800

    xds: fix clusterImplLoadBalancer NPE when lrs is null

--- xds/src/main/java/io/grpc/xds/ClusterImplLoadBalancer.java
+++ xds/src/main/java/io/grpc/xds/ClusterImplLoadBalancer.java
@@ -317,10 +317,13 @@ final class ClusterImplLoadBalancer extends LoadBalancer {
                   "Cluster max concurrent requests limit exceeded"));
             }
           }
+          ClientStreamTracer.Factory tracerFactory = result.getStreamTracerFactory();
           final ClusterLocalityStats stats =
               result.getSubchannel().getAttributes().get(ATTR_CLUSTER_LOCALITY_STATS);
-          ClientStreamTracer.Factory tracerFactory = new CountingStreamTracerFactory(
+          if (stats != null) {
+            tracerFactory = new CountingStreamTracerFactory(
               stats, inFlights, result.getStreamTracerFactory());
+          }
           return PickResult.withSubchannel(result.getSubchannel(), tracerFactory);
         }
         return result;
