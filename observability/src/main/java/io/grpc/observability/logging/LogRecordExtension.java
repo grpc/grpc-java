c77701f8fe4a2e24a1a46f413aa6f13b886906e4
Vindhya Ningegowda <vindhyan@google.com>
2022-02-03 15:23:46 -0800

    Updated GrpcLogRecord Proto

---
+++ observability/src/main/java/io/grpc/observability/logging/LogRecordExtension.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.observability.logging;
+
+import io.grpc.Internal;
+import io.grpc.observabilitylog.v1.GrpcLogRecord;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+@Internal
+public final class LogRecordExtension extends LogRecord {
+  public GrpcLogRecord grpcLogRecord;
+
+  public LogRecordExtension(Level level, String msg) {
+    super(level, msg);
+  }
+
+  // One option is to return proto builder
+  // Have to check if this is allowed when GrpcLogRecord is final
+  public GrpcLogRecord.Builder getGrpcLogRecordBuilder() {
+    return GrpcLogRecord.newBuilder();
+  }
+
+  public void setGrpcLogRecord(GrpcLogRecord record) {
+    grpcLogRecord = record;
+  }
+
+  public GrpcLogRecord getGrpcLogRecord() {
+    return grpcLogRecord;
+  }
+
+  private static final long serialVersionUID = 1L;
+}
