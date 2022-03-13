322846d4078b236f7625d3b978cea3a7d0e7d4df
Vindhya Ningegowda <vindhyan@google.com>
2022-03-13 12:41:42 -0700

    added sample logging tests using interceptors

---
+++ observability/src/test/java/io/grpc/observability/LoggingTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2022 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.observability;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.observability.interceptors.InternalLoggingChannelInterceptor;
+import io.grpc.observability.interceptors.InternalLoggingServerInterceptor;
+import io.grpc.observability.logging.GcpLogSink;
+import io.grpc.observability.logging.Sink;
+import io.grpc.testing.GrpcCleanupRule;
+import io.grpc.testing.protobuf.SimpleServiceGrpc;
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LoggingTest {
+
+  @Rule
+  public final GrpcCleanupRule cleanupRule = new GrpcCleanupRule();
+
+  private static final String PROJECT_ID = "project-id";
+
+  public void clientServer_interceptorCalled()
+      throws IOException {
+    Sink sink = GcpLogSink.getInstance(PROJECT_ID);
+    LoggingServerProvider.init(
+        new InternalLoggingServerInterceptor.FactoryImpl(sink));
+    Server server = ServerBuilder.forPort(0).addService(new LoggingTestHelper.SimpleServiceImpl())
+        .build().start();
+    int port = cleanupRule.register(server).getPort();
+    LoggingChannelProvider.init(
+        new InternalLoggingChannelInterceptor.FactoryImpl(sink));
+    ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", port)
+        .usePlaintext().build();
+    SimpleServiceGrpc.SimpleServiceBlockingStub stub = SimpleServiceGrpc.newBlockingStub(
+        cleanupRule.register(channel));
+    assertThat(LoggingTestHelper.unaryRpc("buddy", stub))
+        .isEqualTo("Hello buddy");
+    GcpLogSink.getInstance(PROJECT_ID).close();
+    LoggingChannelProvider.finish();
+    LoggingServerProvider.finish();
+  }
+}
