8932c121c3e9fb0b0020e332224ac01c1950211d
Sanjay Pujare <sanjaypujare@users.noreply.github.com>
2022-03-30 09:50:12 -0700

    rename io.grpc.observability package to io.grpc.gcp.observability

---
+++ gcp-observability/src/test/java/io/grpc/gcp/observability/LoggingTest.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2022 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.gcp.observability;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import com.google.common.collect.ImmutableMap;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.MethodDescriptor;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.gcp.observability.interceptors.ConfigFilterHelper;
+import io.grpc.gcp.observability.interceptors.ConfigFilterHelper.FilterParams;
+import io.grpc.gcp.observability.interceptors.InternalLoggingChannelInterceptor;
+import io.grpc.gcp.observability.interceptors.InternalLoggingServerInterceptor.FactoryImpl;
+import io.grpc.gcp.observability.interceptors.LogHelper;
+import io.grpc.gcp.observability.logging.GcpLogSink;
+import io.grpc.gcp.observability.logging.Sink;
+import io.grpc.internal.TimeProvider;
+import io.grpc.observabilitylog.v1.GrpcLogRecord;
+import io.grpc.observabilitylog.v1.GrpcLogRecord.EventType;
+import io.grpc.testing.GrpcCleanupRule;
+import io.grpc.testing.protobuf.SimpleServiceGrpc;
+import java.io.IOException;
+import java.util.Map;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+@RunWith(JUnit4.class)
+public class LoggingTest {
+
+  @Rule
+  public final GrpcCleanupRule cleanupRule = new GrpcCleanupRule();
+
+  private static final String PROJECT_ID = "PROJECT";
+  private static final Map<String, String> locationTags = ImmutableMap.of(
+      "project_id", "PROJECT",
+      "location", "us-central1-c",
+      "cluster_name", "grpc-observability-cluster",
+      "namespace_name", "default" ,
+      "pod_name", "app1-6c7c58f897-n92c5");
+  private static final Map<String, String> customTags = ImmutableMap.of(
+      "KEY1", "Value1",
+      "KEY2", "VALUE2");
+  private static final int flushLimit = 100;
+
+  /**
+   * Cloud logging test using LoggingChannelProvider and LoggingServerProvider.
+   * 
+   * <p> Ignoring test, because it calls external CLoud Logging APIs.
+   * To test cloud logging setup, 
+   * 1. Set up Cloud Logging Auth credentials
+   * 2. Assign permissions to service account to write logs to project specified by 
+   * variable PROJECT_ID
+   * 3. Comment @Ignore annotation
+   * </p>
+   */
+  @Ignore
+  @Test
+  public void clientServer_interceptorCalled_logAlways()
+      throws IOException {
+    Sink sink = new GcpLogSink(PROJECT_ID, locationTags, customTags, flushLimit);
+    LogHelper spyLogHelper = spy(new LogHelper(sink, TimeProvider.SYSTEM_TIME_PROVIDER));
+    ConfigFilterHelper mockFilterHelper = mock(ConfigFilterHelper.class);
+    FilterParams logAlwaysFilterParams =
+        FilterParams.create(true, 0, 0);
+    when(mockFilterHelper.isMethodToBeLogged(any(MethodDescriptor.class)))
+        .thenReturn(logAlwaysFilterParams);
+    when(mockFilterHelper.isEventToBeLogged(any(GrpcLogRecord.EventType.class)))
+        .thenReturn(true);
+    LoggingServerProvider.init(
+        new FactoryImpl(spyLogHelper, mockFilterHelper));
+    Server server = ServerBuilder.forPort(0).addService(new LoggingTestHelper.SimpleServiceImpl())
+        .build().start();
+    int port = cleanupRule.register(server).getPort();
+    LoggingChannelProvider.init(
+        new InternalLoggingChannelInterceptor.FactoryImpl(spyLogHelper, mockFilterHelper));
+    SimpleServiceGrpc.SimpleServiceBlockingStub stub = SimpleServiceGrpc.newBlockingStub(
+        cleanupRule.register(ManagedChannelBuilder.forAddress("localhost", port)
+            .usePlaintext().build()));
+    assertThat(LoggingTestHelper.makeUnaryRpcViaClientStub("buddy", stub))
+        .isEqualTo("Hello buddy");
+    assertThat(Mockito.mockingDetails(spyLogHelper).getInvocations().size()).isGreaterThan(11);
+    sink.close();
+    LoggingChannelProvider.shutdown();
+    LoggingServerProvider.shutdown();
+  }
+
+  @Test
+  public void clientServer_interceptorCalled_logNever() throws IOException {
+    Sink mockSink = mock(GcpLogSink.class);
+    LogHelper spyLogHelper = spy(new LogHelper(mockSink, TimeProvider.SYSTEM_TIME_PROVIDER));
+    ConfigFilterHelper mockFilterHelper = mock(ConfigFilterHelper.class);
+    FilterParams logNeverFilterParams =
+        FilterParams.create(false, 0, 0);
+    when(mockFilterHelper.isMethodToBeLogged(any(MethodDescriptor.class)))
+        .thenReturn(logNeverFilterParams);
+    when(mockFilterHelper.isEventToBeLogged(any(GrpcLogRecord.EventType.class)))
+        .thenReturn(true);
+    LoggingServerProvider.init(
+        new FactoryImpl(spyLogHelper, mockFilterHelper));
+    Server server = ServerBuilder.forPort(0).addService(new LoggingTestHelper.SimpleServiceImpl())
+        .build().start();
+    int port = cleanupRule.register(server).getPort();
+    LoggingChannelProvider.init(
+        new InternalLoggingChannelInterceptor.FactoryImpl(spyLogHelper, mockFilterHelper));
+    SimpleServiceGrpc.SimpleServiceBlockingStub stub = SimpleServiceGrpc.newBlockingStub(
+        cleanupRule.register(ManagedChannelBuilder.forAddress("localhost", port)
+            .usePlaintext().build()));
+    assertThat(LoggingTestHelper.makeUnaryRpcViaClientStub("buddy", stub))
+        .isEqualTo("Hello buddy");
+    verifyNoInteractions(spyLogHelper);
+    verifyNoInteractions(mockSink);
+    LoggingChannelProvider.shutdown();
+    LoggingServerProvider.shutdown();
+  }
+
+  @Test
+  public void clientServer_interceptorCalled_doNotLogMessageEvents() throws IOException {
+    LogHelper mockLogHelper = mock(LogHelper.class);
+    ConfigFilterHelper mockFilterHelper2 = mock(ConfigFilterHelper.class);
+    FilterParams logAlwaysFilterParams =
+        FilterParams.create(true, 0, 0);
+    when(mockFilterHelper2.isMethodToBeLogged(any(MethodDescriptor.class)))
+        .thenReturn(logAlwaysFilterParams);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_REQUEST_HEADER))
+        .thenReturn(true);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_RESPONSE_HEADER))
+        .thenReturn(true);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_HALF_CLOSE))
+        .thenReturn(true);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_TRAILER))
+        .thenReturn(true);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_CANCEL))
+        .thenReturn(true);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_REQUEST_MESSAGE))
+        .thenReturn(false);
+    when(mockFilterHelper2.isEventToBeLogged(EventType.GRPC_CALL_RESPONSE_MESSAGE))
+        .thenReturn(false);
+    LoggingServerProvider.init(
+        new FactoryImpl(mockLogHelper, mockFilterHelper2));
+    Server server = ServerBuilder.forPort(0).addService(new LoggingTestHelper.SimpleServiceImpl())
+        .build().start();
+    int port = cleanupRule.register(server).getPort();
+    LoggingChannelProvider.init(
+        new InternalLoggingChannelInterceptor.FactoryImpl(mockLogHelper, mockFilterHelper2));
+    SimpleServiceGrpc.SimpleServiceBlockingStub stub = SimpleServiceGrpc.newBlockingStub(
+        cleanupRule.register(ManagedChannelBuilder.forAddress("localhost", port)
+            .usePlaintext().build()));
+    assertThat(LoggingTestHelper.makeUnaryRpcViaClientStub("buddy", stub))
+        .isEqualTo("Hello buddy");
+    // Total number of calls should have been 14 (6 from client and 6 from server)
+    // Since cancel is not invoked, it will be 12.
+    // Request message(Total count:2 (1 from client and 1 from server) and Response message(count:2)
+    // events are not in the event_types list, i.e  14 - 2(cancel) - 2(req_msg) - 2(resp_msg) = 8
+    assertThat(Mockito.mockingDetails(mockLogHelper).getInvocations().size()).isEqualTo(8);
+    LoggingChannelProvider.shutdown();
+    LoggingServerProvider.shutdown();
+  }
+}
---
+++ gcp-observability/src/test/java/io/grpc/gcp/observability/logging/GcpLogSinkTest.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2022 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.grpc.gcp.observability.logging;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyIterable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import com.google.cloud.MonitoredResource;
+import com.google.cloud.logging.LogEntry;
+import com.google.cloud.logging.Logging;
+import com.google.common.collect.ImmutableMap;
+import com.google.protobuf.Duration;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import com.google.protobuf.util.Durations;
+import io.grpc.observabilitylog.v1.GrpcLogRecord;
+import io.grpc.observabilitylog.v1.GrpcLogRecord.EventLogger;
+import io.grpc.observabilitylog.v1.GrpcLogRecord.EventType;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.ArgumentCaptor;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * Tests for {@link GcpLogSink}.
+ */
+@RunWith(JUnit4.class)
+public class GcpLogSinkTest {
+
+  @Rule
+  public final MockitoRule mockito = MockitoJUnit.rule();
+
+  private static final Map<String, String> locationTags = ImmutableMap.of("project_id", "PROJECT",
+      "location", "us-central1-c",
+      "cluster_name", "grpc-observability-cluster",
+      "namespace_name", "default" ,
+      "pod_name", "app1-6c7c58f897-n92c5");
+  private static final Map<String, String> customTags = ImmutableMap.of("KEY1", "Value1",
+      "KEY2", "VALUE2");
+  private static final int flushLimit = 10;
+  private final long seqId = 1;
+  private final String serviceName = "service";
+  private final String methodName = "method";
+  private final String authority = "authority";
+  private final Duration timeout = Durations.fromMillis(1234);
+  private final String rpcId = "d155e885-9587-4e77-81f7-3aa5a443d47f";
+  private final GrpcLogRecord logProto = GrpcLogRecord.newBuilder()
+      .setSequenceId(seqId)
+      .setServiceName(serviceName)
+      .setMethodName(methodName)
+      .setAuthority(authority)
+      .setTimeout(timeout)
+      .setEventType(EventType.GRPC_CALL_REQUEST_HEADER)
+      .setEventLogger(EventLogger.LOGGER_CLIENT)
+      .setRpcId(rpcId)
+      .build();
+  private final Struct expectedStructLogProto = Struct.newBuilder()
+      .putFields("sequence_id", Value.newBuilder().setStringValue(String.valueOf(seqId)).build())
+      .putFields("service_name", Value.newBuilder().setStringValue(serviceName).build())
+      .putFields("method_name", Value.newBuilder().setStringValue(methodName).build())
+      .putFields("authority", Value.newBuilder().setStringValue(authority).build())
+      .putFields("timeout", Value.newBuilder().setStringValue("1.234s").build())
+      .putFields("event_type", Value.newBuilder().setStringValue(
+          String.valueOf(EventType.GRPC_CALL_REQUEST_HEADER)).build())
+      .putFields("event_logger", Value.newBuilder().setStringValue(
+          String.valueOf(EventLogger.LOGGER_CLIENT)).build())
+      .putFields("rpc_id", Value.newBuilder().setStringValue(rpcId).build())
+      .build();
+  private Logging mockLogging;
+
+  @Before
+  public void setUp() {
+    mockLogging = mock(Logging.class);
+  }
+
+  @Test
+  public void createSink() {
+    Sink mockSink = new GcpLogSink(mockLogging, locationTags, customTags, flushLimit);
+    assertThat(mockSink).isInstanceOf(GcpLogSink.class);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void verifyWrite() throws Exception {
+    Sink mockSink = new GcpLogSink(mockLogging, locationTags, customTags, flushLimit);
+    mockSink.write(logProto);
+
+    ArgumentCaptor<Collection<LogEntry>> logEntrySetCaptor = ArgumentCaptor.forClass(
+        (Class) Collection.class);
+    verify(mockLogging, times(1)).write(logEntrySetCaptor.capture());
+    for (Iterator<LogEntry> it = logEntrySetCaptor.getValue().iterator(); it.hasNext(); ) {
+      LogEntry entry = it.next();
+      System.out.println(entry);
+      assertEquals(entry.getPayload().getData(), expectedStructLogProto);
+    }
+    verifyNoMoreInteractions(mockLogging);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void verifyWriteWithTags() {
+    GcpLogSink mockSink = new GcpLogSink(mockLogging, locationTags, customTags, flushLimit);
+    MonitoredResource expectedMonitoredResource = GcpLogSink.getResource(locationTags);
+    mockSink.write(logProto);
+
+    ArgumentCaptor<Collection<LogEntry>> logEntrySetCaptor = ArgumentCaptor.forClass(
+        (Class) Collection.class);
+    verify(mockLogging, times(1)).write(logEntrySetCaptor.capture());
+    System.out.println(logEntrySetCaptor.getValue());
+    for (Iterator<LogEntry> it = logEntrySetCaptor.getValue().iterator(); it.hasNext(); ) {
+      LogEntry entry = it.next();
+      assertEquals(entry.getResource(), expectedMonitoredResource);
+      assertEquals(entry.getLabels(), customTags);
+      assertEquals(entry.getPayload().getData(), expectedStructLogProto);
+    }
+    verifyNoMoreInteractions(mockLogging);
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void emptyCustomTags_labelsNotSet() {
+    Map<String, String> emptyCustomTags = null;
+    Map<String, String> expectedEmptyLabels = new HashMap<>();
+    GcpLogSink mockSink = new GcpLogSink(mockLogging, locationTags, emptyCustomTags, flushLimit);
+    mockSink.write(logProto);
+
+    ArgumentCaptor<Collection<LogEntry>> logEntrySetCaptor = ArgumentCaptor.forClass(
+        (Class) Collection.class);
+    verify(mockLogging, times(1)).write(logEntrySetCaptor.capture());
+    for (Iterator<LogEntry> it = logEntrySetCaptor.getValue().iterator(); it.hasNext(); ) {
+      LogEntry entry = it.next();
+      assertEquals(entry.getLabels(), expectedEmptyLabels);
+      assertEquals(entry.getPayload().getData(), expectedStructLogProto);
+    }
+  }
+
+  @Test
+  public void verifyFlush() {
+    int lowerFlushLimit = 2;
+    GcpLogSink mockSink = new GcpLogSink(mockLogging, locationTags, customTags, lowerFlushLimit);
+    mockSink.write(logProto);
+    verify(mockLogging, never()).flush();
+    mockSink.write(logProto);
+    verify(mockLogging, times(1)).flush();
+    mockSink.write(logProto);
+    mockSink.write(logProto);
+    verify(mockLogging, times(2)).flush();
+  }
+
+  @Test
+  public void verifyClose() throws Exception {
+    Sink mockSink = new GcpLogSink(mockLogging, locationTags, customTags, flushLimit);
+    mockSink.write(logProto);
+    verify(mockLogging, times(1)).write(anyIterable());
+    mockSink.close();
+    verify(mockLogging).close();
+    verifyNoMoreInteractions(mockLogging);
+  }
+}
