f04a49a7bdba23ce54b46d36a8189d9f2fb1a6e7
Kurt Alfred Kluever <kak@google.com>
2022-03-31 22:49:12 -0400

    Use try/catch idiom instead of @Test(expected = ...) (#9037)

--- xds/src/test/java/io/grpc/xds/LeastRequestLoadBalancerTest.java
+++ xds/src/test/java/io/grpc/xds/LeastRequestLoadBalancerTest.java
@@ -28,6 +28,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
@@ -575,10 +576,14 @@ public class LeastRequestLoadBalancerTest {
     assertThat(pickers.hasNext()).isFalse();
   }
 
-  @Test(expected = IllegalArgumentException.class)
+  @Test
   public void readyPicker_emptyList() {
-    // ready picker list must be non-empty
-    new ReadyPicker(Collections.<Subchannel>emptyList(), 2, mockRandom);
+    try {
+      // ready picker list must be non-empty
+      new ReadyPicker(Collections.<Subchannel>emptyList(), 2, mockRandom);
+      fail();
+    } catch (IllegalArgumentException expected) {
+    }
   }
 
   @Test
