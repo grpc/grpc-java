061df4675f554cba139c16567390a175da4e5567
Oliver <wqdyxnbd@163.com>
2022-04-10 21:01:55 +0800

    checkstyle

--- core/src/main/java/io/grpc/util/CertificateUtils.java
+++ core/src/main/java/io/grpc/util/CertificateUtils.java
@@ -22,7 +22,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
