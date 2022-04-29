5088f830b0efbba92d7fd9ab61af4bd159014501
Sergii Tkachenko <sergiitk@google.com>
2022-04-28 18:16:20 -0700

    binder, xds: address minor linter fixes

--- binder/src/main/java/io/grpc/binder/SecurityPolicies.java
+++ binder/src/main/java/io/grpc/binder/SecurityPolicies.java
@@ -28,9 +28,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.errorprone.annotations.CheckReturnValue;
 import io.grpc.ExperimentalApi;
 import io.grpc.Status;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 /** Static factory methods for creating standard security policies. */
 @CheckReturnValue
