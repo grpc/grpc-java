129c647a2d8549f1f6f6222c4dbdf98048a02bc7
sangyongchoi <csy7792@naver.com>
2022-07-01 01:35:58 +0900

    feat: modify request validator and add copyright

--- api/src/main/java/io/grpc/RequestValidationListener.java
+++ api/src/main/java/io/grpc/RequestValidationListener.java
@@ -1,6 +1,20 @@
-package io.grpc;
+/*
+ * Copyright 2016 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import com.google.protobuf.MessageLiteOrBuilder;
+package io.grpc;
 
 public class RequestValidationListener<ReqT, ResT> extends ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT> {
 
@@ -22,33 +36,33 @@ public class RequestValidationListener<ReqT, ResT> extends ForwardingServerCallL
 
     @Override
     public void onMessage(ReqT message) {
-        MessageLiteOrBuilder convertMessage = (MessageLiteOrBuilder) message;
-        RequestValidator<MessageLiteOrBuilder> validator = requestValidatorResolver.find(convertMessage.getClass().getTypeName());
+        RequestValidator<ReqT> validator = (RequestValidator<ReqT>) requestValidatorResolver.find(message.getClass());
 
         if (validator == null) {
             super.onMessage(message);
-        } else {
-            try {
-                ValidationResult validationResult = validator.isValid(convertMessage);
+            return;
+        }
 
-                if (validationResult.isValid()) {
-                    super.onMessage(message);
-                } else {
-                    Status status = Status.INVALID_ARGUMENT
-                            .withDescription("invalid argument. " + validationResult.getMessage());
-                    handleInvalidRequest(status);
-                }
-            } catch (Exception e) {
-                Status status = Status.INTERNAL.withDescription(e.getMessage());
+        try {
+            ValidationResult validationResult = validator.isValid(message);
 
+            if (validationResult.isValid()) {
+                super.onMessage(message);
+            } else {
+                Status status = Status.INVALID_ARGUMENT
+                        .withDescription("invalid argument. " + validationResult.getMessage());
                 handleInvalidRequest(status);
             }
+        } catch (Exception e) {
+            Status status = Status.INTERNAL.withDescription(e.getMessage());
+
+            handleInvalidRequest(status);
         }
     }
 
     private void handleInvalidRequest(Status status) {
         if (!serverCall.isCancelled()) {
-            serverCall.close(status, headers);
+            serverCall.close(status, new Metadata());
         }
     }
 }
--- api/src/main/java/io/grpc/RequestValidatorResolver.java
+++ api/src/main/java/io/grpc/RequestValidatorResolver.java
@@ -1,38 +1,36 @@
+/*
+ * Copyright 2016 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.grpc;
 
-import com.google.protobuf.MessageLiteOrBuilder;
-
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RequestValidatorResolver {
 
-    private List<RequestValidator<MessageLiteOrBuilder>> validators;
+    private Map<Class<?>, RequestValidator<?>> validatorMap;
 
-    private Map<String, RequestValidator<MessageLiteOrBuilder>> validatorMap;
-
-    public RequestValidatorResolver(List<RequestValidator<MessageLiteOrBuilder>> validators) {
-        this.validators = validators;
-
+    public RequestValidatorResolver(List<RequestValidator<?>> validators) {
         validatorMap = validators.stream()
-                .collect(Collectors.toMap(this::getClassName, it -> it));
+                .collect(Collectors.toMap(RequestValidator::getTargetClass, it -> it));
     }
 
-    private String getClassName(RequestValidator<MessageLiteOrBuilder> it) {
-        Type[] genericInterfaces = it.getClass().getGenericInterfaces();
-
-        if (genericInterfaces.length == 0) {
-            return null;
-        }
-
-        return ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments()[0].getTypeName();
-    }
-
-    public RequestValidator<MessageLiteOrBuilder> find(String typeName) {
-        return validatorMap.get(typeName);
+    public RequestValidator<?> find(Class<?> targetClass) {
+        return validatorMap.get(targetClass);
     }
 }
 
