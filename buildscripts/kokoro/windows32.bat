0bfa465e416d1a9a562d9dd9f04d0938042dd023
Eric Anderson <ejona@google.com>
2022-06-30 15:01:16 -0700

    Increase Gradle build memory for Windows and GAE

--- buildscripts/kokoro/windows32.bat
+++ buildscripts/kokoro/windows32.bat
@@ -29,6 +29,7 @@ SET FAIL_ON_WARNINGS=true
 SET VC_PROTOBUF_LIBS=%ESCWORKSPACE%\\grpc-java-helper32\\protobuf-%PROTOBUF_VER%\\cmake\\build\\Release
 SET VC_PROTOBUF_INCLUDE=%ESCWORKSPACE%\\grpc-java-helper32\\protobuf-%PROTOBUF_VER%\\cmake\\build\\include
 SET GRADLE_FLAGS=-PtargetArch=%TARGET_ARCH% -PfailOnWarnings=%FAIL_ON_WARNINGS% -PvcProtobufLibs=%VC_PROTOBUF_LIBS% -PvcProtobufInclude=%VC_PROTOBUF_INCLUDE% -PskipAndroid=true
+SET GRADLE_OPTS="-Dorg.gradle.jvmargs='-Xmx1g'"
 
 cmd.exe /C "%WORKSPACE%\gradlew.bat %GRADLE_FLAGS% build"
 set GRADLEEXIT=%ERRORLEVEL%
