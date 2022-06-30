c46972dde5f07a13ed3fe65d62187825a1f8c932
Eric Anderson <ejona@google.com>
2022-06-30 11:18:49 -0700

    Bump protobuf to 3.21.1 (#9311)
    Fixes #9264

--- buildscripts/make_dependencies.sh
+++ buildscripts/make_dependencies.sh
@@ -3,7 +3,9 @@
 # Build protoc
 set -evux -o pipefail
 
-PROTOBUF_VERSION=3.19.2
+PROTOBUF_VERSION=21.1
+# https://github.com/protocolbuffers/protobuf/issues/10172
+PROTOBUF_VERSION_ISSUE_10172=3.$PROTOBUF_VERSION
 
 # ARCH is x86_64 bit unless otherwise specified.
 ARCH="${ARCH:-x86_64}"
@@ -28,6 +30,7 @@ if [ -f ${INSTALL_DIR}/bin/protoc ]; then
 else
   if [[ ! -d "$DOWNLOAD_DIR"/protobuf-"${PROTOBUF_VERSION}" ]]; then
     curl -Ls https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-all-${PROTOBUF_VERSION}.tar.gz | tar xz -C $DOWNLOAD_DIR
+    mv "$DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION_ISSUE_10172}" "$DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION}"
   fi
   pushd $DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION}
   # install here so we don't need sudo
