grpc/grpc-java

commit e96d04774b3210d049334fe2346c01044c5f20f3

okhttp: Add server implementation

okhttp/src/main/java/io/grpc/okhttp/AsyncSink.java
@@ -164,6 +164,13 @@ final class AsyncSink implements Sink {
     serializingExecutor.execute(new Runnable() {
       @Override
       public void run() {
+        try {
+          if (buffer.size() > 0) {
+            sink.write(buffer, buffer.size());
+          }
+        } catch (IOException e) {
+          transportExceptionHandler.onException(e);
+        }
         buffer.close();
         try {
           if (sink != null) {

api/src/main/java/io/grpc/ServerBuilder.java
@@ -246,7 +246,7 @@ public abstract class ServerBuilder<T extends ServerBuilder<T>> {
   /**
    * Sets the time without read activity before sending a keepalive ping. An unreasonably small
    * value might be increased, and {@code Long.MAX_VALUE} nano seconds or an unreasonably large
-   * value will disable keepalive. The typical default is infinite when supported.
+   * value will disable keepalive. The typical default is two hours when supported.
    *
    * @throws IllegalArgumentException if time is not positive
    * @throws UnsupportedOperationException if unsupported

core/src/main/java/io/grpc/internal/AbstractServerStream.java
@@ -50,7 +50,7 @@ public abstract class AbstractServerStream extends AbstractStream
      * @param flush {@code true} if more data may not be arriving soon
      * @param numMessages the number of messages this frame represents
      */
-    void writeFrame(@Nullable WritableBuffer frame, boolean flush, int numMessages);
+    void writeFrame(WritableBuffer frame, boolean flush, int numMessages);
 
     /**
      * Sends trailers to the remote end point. This call implies end of stream.
@@ -108,7 +108,14 @@ public abstract class AbstractServerStream extends AbstractStream
       WritableBuffer frame, boolean endOfStream, boolean flush, int numMessages) {
     // Since endOfStream is triggered by the sending of trailers, avoid flush here and just flush
     // after the trailers.
-    abstractServerStreamSink().writeFrame(frame, endOfStream ? false : flush, numMessages);
+    if (frame == null) {
+      assert endOfStream;
+      return;
+    }
+    if (endOfStream) {
+      flush = false;
+    }
+    abstractServerStreamSink().writeFrame(frame, flush, numMessages);
   }
 
   @Override

core/src/main/java/io/grpc/internal/GrpcUtil.java
@@ -799,6 +799,12 @@ public final class GrpcUtil {
     }
   }
 
+  /** Reads {@code in} until end of stream. */
+  public static void exhaust(InputStream in) throws IOException {
+    byte[] buf = new byte[256];
+    while (in.read(buf) != -1) {}
+  }
+
   /**
    * Checks whether the given item exists in the iterable.  This is copied from Guava Collect's
    * {@code Iterables.contains()} because Guava Collect is not Android-friendly thus core can't

core/src/test/java/io/grpc/internal/AbstractTransportTest.java
@@ -408,12 +408,13 @@ public abstract class AbstractTransportTest {
     }
     assumeTrue("transport is not using InetSocketAddress", port != -1);
     server.shutdown();
+    assertTrue(serverListener.waitForShutdown(TIMEOUT_MS, TimeUnit.MILLISECONDS));
 
     server = newServer(port, Arrays.asList(serverStreamTracerFactory));
     boolean success;
     Thread.currentThread().interrupt();
     try {
-      server.start(serverListener);
+      server.start(serverListener = new MockServerListener());
       success = true;
     } catch (Exception ex) {
       success = false;

netty/src/main/java/io/grpc/netty/NettyServerHandler.java
@@ -52,6 +52,7 @@ import io.grpc.internal.TransportTracer;
 import io.grpc.netty.GrpcHttp2HeadersUtils.GrpcHttp2ServerHeadersDecoder;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
@@ -854,6 +855,9 @@ class NettyServerHandler extends AbstractNettyHandler {
         keepAliveManager.onDataReceived();
       }
       NettyServerHandler.this.onHeadersRead(ctx, streamId, headers);
+      if (endStream) {
+        NettyServerHandler.this.onDataRead(streamId, Unpooled.EMPTY_BUFFER, 0, endStream);
+      }
     }
 
     @Override

netty/src/main/java/io/grpc/netty/NettyServerStream.java
@@ -108,10 +108,6 @@ class NettyServerStream extends AbstractServerStream {
 
     private void writeFrameInternal(WritableBuffer frame, boolean flush, final int numMessages) {
       Preconditions.checkArgument(numMessages >= 0);
-      if (frame == null) {
-        writeQueue.scheduleFlush();
-        return;
-      }
       ByteBuf bytebuf = ((NettyWritableBuffer) frame).bytebuf().touch();
       final int numBytes = bytebuf.readableBytes();
       // Add the bytes to outbound flow control.

okhttp/build.gradle
@@ -21,7 +21,7 @@ dependencies {
     testImplementation project(':grpc-core').sourceSets.test.output,
             project(':grpc-api').sourceSets.test.output,
             project(':grpc-testing'),
-            project(':grpc-netty'),
+            libraries.netty.codec.http2,
             libraries.okhttp
     signature "org.codehaus.mojo.signature:java17:1.0@signature"
     signature "net.sf.androidscents.signature:android-api-level-14:4.0_r4@signature"

commit 0a4e8107cc75b667a61addbb33717eeb171e3f22

okhttp: Fix AsyncSink.close() NPE
This fixes a regression introduced in e96d0477. The NullPointerException
only happens on client-side when some other error occurred during
handshaking.

I tried to add a test, but SerializingExecutor catches+logs the
exception and the expected behavior in the circumstance is that close()
is a noop. So the NPE was entirely benign other than annoying log
messages.

okhttp/src/main/java/io/grpc/okhttp/AsyncSink.java
@@ -208,7 +208,7 @@ final class AsyncSink implements Sink {
       @Override
       public void run() {
         try {
-          if (buffer.size() > 0) {
+          if (sink != null && buffer.size() > 0) {
             sink.write(buffer, buffer.size());
           }
         } catch (IOException e) {