31d777e212d600a723152793611b59a4dc573a76
Terry Wilson <tmwilson@google.com>
2022-10-10 13:31:46 -0700

    grpclb: switch to use acceptResolvedAddresses() (#9568)
    This is part of a migration to move all LB implementations from
    handleResolvedAddresses() to this new method.

--- grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
+++ grpclb/src/test/java/io/grpc/grpclb/GrpclbLoadBalancerTest.java
@@ -2735,7 +2735,7 @@ public class GrpclbLoadBalancerTest {
     syncContext.execute(new Runnable() {
       @Override
       public void run() {
-        balancer.handleResolvedAddresses(
+        balancer.acceptResolvedAddresses(
             ResolvedAddresses.newBuilder()
                 .setAddresses(backendAddrs)
                 .setAttributes(attrs)
