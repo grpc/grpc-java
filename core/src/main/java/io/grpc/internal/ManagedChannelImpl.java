db267e5e675ebbdaefc678410bbd01dd6b52f13d
RapperCL <775523362@qq.com>
2022-10-22 23:50:21 +0800

    args check bug

--- core/src/main/java/io/grpc/internal/ManagedChannelImpl.java
+++ core/src/main/java/io/grpc/internal/ManagedChannelImpl.java
@@ -1942,14 +1942,14 @@ final class ManagedChannelImpl extends ManagedChannel implements
     ScheduledHandle delayedShutdownTask;
 
     SubchannelImpl(CreateSubchannelArgs args, LbHelperImpl helper) {
+      this.args = checkNotNull(args, "args");
+      this.helper = checkNotNull(helper, "helper");
       addressGroups = args.getAddresses();
       if (authorityOverride != null) {
         List<EquivalentAddressGroup> eagsWithoutOverrideAttr =
             stripOverrideAuthorityAttributes(args.getAddresses());
         args = args.toBuilder().setAddresses(eagsWithoutOverrideAttr).build();
       }
-      this.args = checkNotNull(args, "args");
-      this.helper = checkNotNull(helper, "helper");
       subchannelLogId = InternalLogId.allocate("Subchannel", /*details=*/ authority());
       subchannelTracer = new ChannelTracer(
           subchannelLogId, maxTraceEvents, timeProvider.currentTimeNanos(),
