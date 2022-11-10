356626c068c925288b709ff4b7e514d0a600c50d
pandaapo <1052156701@qq.com>
2022-11-11 07:39:12 +0800

    Fix issue: grpc-previous-rpc-attempts not added to initial response metadata

--- core/src/test/java/io/grpc/internal/RetriableStreamTest.java
+++ core/src/test/java/io/grpc/internal/RetriableStreamTest.java
@@ -1165,7 +1165,7 @@ public class RetriableStreamTest {
     Metadata originalHeaders = new Metadata();
     Metadata headers = retriableStream.updateHeaders(originalHeaders, 0);
     assertNotSame(originalHeaders, headers);
-    assertNull(headers.get(GRPC_PREVIOUS_RPC_ATTEMPTS));
+    assertEquals("0", headers.get(GRPC_PREVIOUS_RPC_ATTEMPTS));
 
     headers = retriableStream.updateHeaders(originalHeaders, 345);
     assertEquals("345", headers.get(GRPC_PREVIOUS_RPC_ATTEMPTS));
