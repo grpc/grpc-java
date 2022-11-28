09edbfc33863ca65a1d877ab83916d4e4faee84d
Alexander Polcyn <apolcyn@google.com>
2022-11-28 23:32:27 +0000

    remove unused imports

--- xds/src/main/java/io/grpc/xds/RingHashOptions.java
+++ xds/src/main/java/io/grpc/xds/RingHashOptions.java
@@ -16,38 +16,6 @@
 
 package io.grpc.xds;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-import static io.grpc.ConnectivityState.CONNECTING;
-import static io.grpc.ConnectivityState.IDLE;
-import static io.grpc.ConnectivityState.READY;
-import static io.grpc.ConnectivityState.SHUTDOWN;
-import static io.grpc.ConnectivityState.TRANSIENT_FAILURE;
-
-import com.google.common.base.MoreObjects;
-import com.google.common.collect.Sets;
-import io.grpc.Attributes;
-import io.grpc.ConnectivityState;
-import io.grpc.ConnectivityStateInfo;
-import io.grpc.EquivalentAddressGroup;
-import io.grpc.InternalLogId;
-import io.grpc.LoadBalancer;
-import io.grpc.Status;
-import io.grpc.SynchronizationContext;
-import io.grpc.xds.XdsLogger.XdsLogLevel;
-import io.grpc.xds.XdsSubchannelPickers.ErrorPicker;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicLong;
-import javax.annotation.Nullable;
-
 /**
  * Utility class that provides a way to configure ring hash size limits. This is applicable
  * for clients that use the ring hash load balancing policy. Note that size limits involve
