06e9fab348ddd5656ed3d3ae38aa9024d00621b4
larry-safran <lsafran@google.com>
2022-12-02 14:48:19 -0800

    Add clarifying comment per code review.

--- xds/src/main/java/io/grpc/xds/PriorityLoadBalancer.java
+++ xds/src/main/java/io/grpc/xds/PriorityLoadBalancer.java
@@ -61,6 +61,8 @@ final class PriorityLoadBalancer extends LoadBalancer {
 
   // Includes all active and deactivated children. Mutable. New entries are only added from priority
   // 0 up to the selected priority. An entry is only deleted 15 minutes after its deactivation.
+  // Note that because all configuration updates should be atomic, updates to children can happen
+  // outside of the synchronization context.  Therefore copy values before looping over them.
   private final Map<String, ChildLbState> children = new HashMap<>();
 
   // Following fields are only null initially.
