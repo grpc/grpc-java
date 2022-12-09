2a0b86f7cd2a363cd1d19d56f9c47aead33261d6
yifeizhuang <yifeizhuang@gmail.com>
2022-12-09 15:21:47 -0800

    binder: BinderInternal.setIBinder() is public static (#9743)

--- binder/src/main/java/io/grpc/binder/BinderInternal.java
+++ binder/src/main/java/io/grpc/binder/BinderInternal.java
@@ -28,7 +28,7 @@ public class BinderInternal {
   /**
    * Sets the receiver's {@link IBinder} using {@link IBinderReceiver#set(IBinder)}.
    */
-  static void setIBinder(IBinderReceiver receiver, IBinder binder) {
+  public static void setIBinder(IBinderReceiver receiver, IBinder binder) {
     receiver.set(binder);
   }
 }
