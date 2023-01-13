62d924745d6f41a881f2ea483d201fe5fe83bf55
Eric Anderson <ejona@google.com>
2023-01-13 11:52:44 -0800

    core: Remove unused helper field from SubchannelImpl

--- core/src/main/java/io/grpc/internal/ManagedChannelImpl.java
+++ core/src/main/java/io/grpc/internal/ManagedChannelImpl.java
@@ -1461,7 +1461,7 @@ final class ManagedChannelImpl extends ManagedChannel implements
       syncContext.throwIfNotInThisSynchronizationContext();
       // No new subchannel should be created after load balancer has been shutdown.
       checkState(!terminating, "Channel is being terminated");
-      return new SubchannelImpl(args, this);
+      return new SubchannelImpl(args);
     }
 
     @Override
@@ -1933,7 +1933,6 @@ final class ManagedChannelImpl extends ManagedChannel implements
 
   private final class SubchannelImpl extends AbstractSubchannel {
     final CreateSubchannelArgs args;
-    final LbHelperImpl helper;
     final InternalLogId subchannelLogId;
     final ChannelLoggerImpl subchannelLogger;
     final ChannelTracer subchannelTracer;
@@ -1943,7 +1942,7 @@ final class ManagedChannelImpl extends ManagedChannel implements
     boolean shutdown;
     ScheduledHandle delayedShutdownTask;
 
-    SubchannelImpl(CreateSubchannelArgs args, LbHelperImpl helper) {
+    SubchannelImpl(CreateSubchannelArgs args) {
       checkNotNull(args, "args");
       addressGroups = args.getAddresses();
       if (authorityOverride != null) {
@@ -1952,7 +1951,6 @@ final class ManagedChannelImpl extends ManagedChannel implements
         args = args.toBuilder().setAddresses(eagsWithoutOverrideAttr).build();
       }
       this.args = args;
-      this.helper = checkNotNull(helper, "helper");
       subchannelLogId = InternalLogId.allocate("Subchannel", /*details=*/ authority());
       subchannelTracer = new ChannelTracer(
           subchannelLogId, maxTraceEvents, timeProvider.currentTimeNanos(),
