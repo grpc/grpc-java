c03858079b204b05adb71f98d62339de0b174685
Vindhya Ningegowda <vindhyan@google.com>
2023-02-15 14:53:01 -0800

    Use pre-defined aggregation from OpenCensus for bytes histogram

--- census/src/main/java/io/grpc/census/internal/ObservabilityCensusConstants.java
+++ census/src/main/java/io/grpc/census/internal/ObservabilityCensusConstants.java
@@ -25,38 +25,17 @@ import static io.opencensus.contrib.grpc.metrics.RpcMeasureConstants.GRPC_SERVER
 import static io.opencensus.contrib.grpc.metrics.RpcMeasureConstants.GRPC_SERVER_SENT_BYTES_PER_RPC;
 import static io.opencensus.contrib.grpc.metrics.RpcMeasureConstants.GRPC_SERVER_STATUS;
 
+import io.opencensus.contrib.grpc.metrics.RpcViewConstants;
 import io.opencensus.stats.Aggregation;
-import io.opencensus.stats.Aggregation.Distribution;
-import io.opencensus.stats.BucketBoundaries;
 import io.opencensus.stats.View;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /** Temporary holder class for the observability specific OpenCensus constants.
  *  The class will be removed once the new views are added in OpenCensus library. */
 public final class ObservabilityCensusConstants {
 
-  static final List<Double> RPC_BYTES_BUCKET_BOUNDARIES =
-      Collections.unmodifiableList(
-          Arrays.asList(
-              0.0,
-              1024.0,
-              2048.0,
-              4096.0,
-              16384.0,
-              65536.0,
-              262144.0,
-              1048576.0,
-              4194304.0,
-              16777216.0,
-              67108864.0,
-              268435456.0,
-              1073741824.0,
-              4294967296.0));
-
   static final Aggregation AGGREGATION_WITH_BYTES_HISTOGRAM =
-      Distribution.create(BucketBoundaries.create(RPC_BYTES_BUCKET_BOUNDARIES));
+      RpcViewConstants.GRPC_CLIENT_SENT_BYTES_PER_RPC_VIEW.getAggregation();
 
   public static final View GRPC_CLIENT_SENT_COMPRESSED_MESSAGE_BYTES_PER_RPC =
       View.create(
