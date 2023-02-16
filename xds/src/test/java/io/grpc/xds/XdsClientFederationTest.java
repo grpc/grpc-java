081251f61ae12675eceb7b4307ba7ecbc9932998
Terry Wilson <terrymwilson@gmail.com>
2023-02-16 14:13:54 -0800

    more test order, verify() shenanigans

--- xds/src/test/java/io/grpc/xds/XdsClientFederationTest.java
+++ xds/src/test/java/io/grpc/xds/XdsClientFederationTest.java
@@ -22,6 +22,7 @@ import static org.mockito.Mockito.verify;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import io.grpc.xds.Filter.NamedFilterConfig;
 import io.grpc.xds.XdsClient.ResourceWatcher;
 import io.grpc.xds.XdsListenerResource.LdsUpdate;
 import java.util.Collections;
@@ -82,15 +83,24 @@ public class XdsClientFederationTest {
    */
   @Test
   public void isolatedResourceDeletions() throws InterruptedException {
-    xdsClient.watchXdsResource(XdsListenerResource.getInstance(), "test-server", mockWatcher);
-    xdsClient.watchXdsResource(XdsListenerResource.getInstance(),
-        "xdstp://server-one/envoy.config.listener.v3.Listener/test-server", mockDirectPathWatcher);
-
     trafficdirector.setLdsConfig(ControlPlaneRule.buildServerListener(),
         ControlPlaneRule.buildClientListener("test-server"));
     directpathPa.setLdsConfig(ControlPlaneRule.buildServerListener(),
         ControlPlaneRule.buildClientListener(
             "xdstp://server-one/envoy.config.listener.v3.Listener/test-server"));
+
+    xdsClient.watchXdsResource(XdsListenerResource.getInstance(), "test-server", mockWatcher);
+    xdsClient.watchXdsResource(XdsListenerResource.getInstance(),
+        "xdstp://server-one/envoy.config.listener.v3.Listener/test-server", mockDirectPathWatcher);
+
+    verify(mockWatcher, timeout(2000)).onChanged(
+        LdsUpdate.forApiListener(
+            HttpConnectionManager.forRdsName(0, "route-config.googleapis.com", ImmutableList.of(
+                new NamedFilterConfig("terminal-filter", RouterFilter.ROUTER_CONFIG)))));
+    verify(mockDirectPathWatcher, timeout(2000)).onChanged(
+        LdsUpdate.forApiListener(
+            HttpConnectionManager.forRdsName(0, "route-config.googleapis.com", ImmutableList.of(
+                new NamedFilterConfig("terminal-filter", RouterFilter.ROUTER_CONFIG)))));
 
     // By setting the LDS config with a new server name we effectively make the old server to go
     // away as it is not in the configuration anymore. This change in one control plane (here the
