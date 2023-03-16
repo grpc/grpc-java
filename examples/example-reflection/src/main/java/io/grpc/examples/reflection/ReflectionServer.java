41f0f0789594e57c0405eaa21d7e8e2bc19df8d8
yifeizhuang <zivy@google.com>
2023-03-16 10:36:46 -0700

    move folder

--- examples/example-reflection/src/main/java/io/grpc/examples/reflection/ReflectionServer.java
+++ examples/example-reflection/src/main/java/io/grpc/examples/reflection/ReflectionServer.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 The gRPC Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package io.grpc.examples.reflection;
 
@@ -41,7 +26,7 @@ public class ReflectionServer {
     int port = 50051;
     server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
         .addService(new GreeterImpl())
-        .addService(ProtoReflectionService.newInstance())
+        .addService(ProtoReflectionService.newInstance()) // add reflection service
         .build()
         .start();
     logger.info("Server started, listening on " + port);
