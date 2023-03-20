9ff90ce32fcf5c8bacee3bda6df7b5a7f97e0e3d
Daniel Liu <danielztliu@google.com>
2023-03-20 21:59:49 +0000

    remove unnecessary code styling

--- xds/src/test/java/io/grpc/xds/orca/OrcaMetricReportingServerInterceptorTest.java
+++ xds/src/test/java/io/grpc/xds/orca/OrcaMetricReportingServerInterceptorTest.java
@@ -130,16 +130,16 @@ public class OrcaMetricReportingServerInterceptorTest {
     final CallMetricRecorder callMetricRecorder = new CallMetricRecorder();
     ServerStreamTracer.Factory callMetricRecorderSharingStreamTracerFactory =
         new ServerStreamTracer.Factory() {
+      @Override
+      public ServerStreamTracer newServerStreamTracer(String fullMethodName, Metadata headers) {
+        return new ServerStreamTracer() {
           @Override
-          public ServerStreamTracer newServerStreamTracer(String fullMethodName, Metadata headers) {
-            return new ServerStreamTracer() {
-              @Override
-              public Context filterContext(Context context) {
-                return context.withValue(InternalCallMetricRecorder.CONTEXT_KEY, callMetricRecorder);
-              }
-            };
+          public Context filterContext(Context context) {
+            return context.withValue(InternalCallMetricRecorder.CONTEXT_KEY, callMetricRecorder);
           }
         };
+      }
+    };
 
     final AtomicReference<CallMetricRecorder> callMetricRecorderCapture = new AtomicReference<>();
     SimpleServiceGrpc.SimpleServiceImplBase simpleServiceImpl =
