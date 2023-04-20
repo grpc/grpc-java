27edab007d94409af8b34e5dae49b850a664fa8d
Eric Anderson <ejona@google.com>
2023-04-20 14:38:19 -0700

    Start 1.56.0 development cycle

--- core/src/main/java/io/grpc/internal/GrpcUtil.java
+++ core/src/main/java/io/grpc/internal/GrpcUtil.java
@@ -217,7 +217,7 @@ public final class GrpcUtil {
 
   public static final Splitter ACCEPT_ENCODING_SPLITTER = Splitter.on(',').trimResults();
 
-  private static final String IMPLEMENTATION_VERSION = "1.55.0-SNAPSHOT"; // CURRENT_GRPC_VERSION
+  private static final String IMPLEMENTATION_VERSION = "1.56.0-SNAPSHOT"; // CURRENT_GRPC_VERSION
 
   /**
    * The default timeout in nanos for a keepalive ping request.
