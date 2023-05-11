017fb7f3b46ced65d2a4a5d12faa0532fbe391bc
wwtbuaa01 <wwtbuaa@google.com>
2023-05-11 14:41:49 +0800

    Add UserHandle and BinderChannelCredentials to BinderChannelBuilder to support cross-user ondevice server.

--- binder/src/main/java/io/grpc/binder/BinderChannelCredentials.java
+++ binder/src/main/java/io/grpc/binder/BinderChannelCredentials.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2022 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package io.grpc.binder;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 import android.content.ComponentName;
 import android.content.Context;
@@ -19,7 +36,7 @@ public final class BinderChannelCredentials extends ChannelCredentials {
    * @return a BinderChannelCredentials
    */
   public static BinderChannelCredentials forDefault(Context sourceContext) {
-    return new BinderChannelCredentials(sourceContext, null);
+    return new BinderChannelCredentials(checkNotNull(sourceContext, "sourceContext"), null);
   }
 
   /**
@@ -32,7 +49,8 @@ public final class BinderChannelCredentials extends ChannelCredentials {
    */
   public static BinderChannelCredentials forDevicePolicyAdmin(
       Context sourceContext, ComponentName devicePolicyAdminComponentName) {
-    return new BinderChannelCredentials(sourceContext, devicePolicyAdminComponentName);
+    return new BinderChannelCredentials(
+      checkNotNull(sourceContext, "sourceContext"), devicePolicyAdminComponentName);
   }
 
   // The sourceContext field is intentionally not guarded, since (aside from the constructor),
