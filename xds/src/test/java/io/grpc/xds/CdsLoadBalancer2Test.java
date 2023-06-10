f6e89dd405660eadd96444c93c45fccf5c533332
larry-safran <lsafran@google.com>
2023-06-09 18:10:33 -0700

    Cleanup.

--- xds/src/test/java/io/grpc/xds/CdsLoadBalancer2Test.java
+++ xds/src/test/java/io/grpc/xds/CdsLoadBalancer2Test.java
@@ -24,7 +24,6 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableList;
