7a5f2cb07fce63c02ed9aff79b5572a4b6d0e7ba
Masakuni Oishi <yamasa@bsdhouse.org>
2023-07-08 11:19:43 +0900

    reformat

--- api/src/main/java/io/grpc/ClientInterceptors.java
+++ api/src/main/java/io/grpc/ClientInterceptors.java
@@ -236,9 +236,11 @@ public class ClientInterceptors {
         // to a NO-OP one to prevent the IllegalStateException. The user will finally get notified
         // about the error through the listener.
         delegate = (ClientCall<ReqT, RespT>) NOOP_CALL;
-        Metadata trailersFromThrowable = Status.trailersFromThrowable(e);
-        Metadata trailers = trailersFromThrowable != null ? trailersFromThrowable : new Metadata();
-        responseListener.onClose(Status.fromThrowable(e), trailers);
+        Metadata trailers = Status.trailersFromThrowable(e);
+        responseListener.onClose(
+          Status.fromThrowable(e),
+          trailers != null ? trailers : new Metadata()
+        );
       }
     }
   }
