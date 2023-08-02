f1dd2f83a5aa5cb08c4280f508d8f9581a511efe
Joy Bestourous <joy.bestourous@datadoghq.com>
2023-08-02 16:31:36 -0400

    subset implementation

--- util/src/main/java/io/grpc/util/DeterministicSubsettingLoadBalancer.java
+++ util/src/main/java/io/grpc/util/DeterministicSubsettingLoadBalancer.java
@@ -11,6 +11,11 @@ import io.grpc.Status;
 import io.grpc.internal.ServiceConfigUtil.PolicySelection;
 import java.net.SocketAddress;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 /**
  * Wraps a child {@code LoadBalancer}, separating the total set of backends
@@ -29,21 +34,70 @@ public final class DeterministicSubsettingLoadBalancer extends LoadBalancer {
     DeterministicSubsettingLoadBalancerConfig config
       = (DeterministicSubsettingLoadBalancerConfig) resolvedAddresses.getLoadBalancingPolicyConfig();
 
-
-    ArrayList<SocketAddress> newAddresses = new ArrayList<>();
+    // The map should only retain entries for addresses in this latest update.
+    ArrayList<SocketAddress> allAddresses = new ArrayList<>();
     for (EquivalentAddressGroup addressGroup : resolvedAddresses.getAddresses()){
-      newAddresses.addAll(addressGroup.getAddresses());
+      allAddresses.addAll(addressGroup.getAddresses());
     }
 
     switchLb.switchTo(config.childPolicy.getProvider());
 
-    // TODO: SUBSET
-    ResolvedAddresses subsetAddresses;
+    ResolvedAddresses subsetAddresses = buildSubsets(resolvedAddresses, config);
 
     switchLb.handleResolvedAddresses(
       subsetAddresses.toBuilder().setLoadBalancingPolicyConfig(config.childPolicy.getConfig())
         .build());
     return true;
+  }
+
+  // implements the subsetting algorithm, as described in A68: https://github.com/grpc/proposal/pull/383
+  private ResolvedAddresses buildSubsets(ResolvedAddresses allAddresses, DeterministicSubsettingLoadBalancerConfig config){
+    // The map should only retain entries for addresses in this latest update.
+    ArrayList<SocketAddress> addresses = new ArrayList<>();
+    for (EquivalentAddressGroup addressGroup : allAddresses.getAddresses()){
+      addresses.addAll(addressGroup.getAddresses());
+    }
+
+    if (addresses.size() <= config.subsetSize) return allAddresses;
+    if (config.sortAddresses) {
+      // if we sort, we do so via destination hashcode. this is deterministic but differs from the method used in golang.
+      addresses.sort(new AddressComparator());
+    }
+
+    Integer backendCount = addresses.size();
+    Integer subsetCount = backendCount / config.subsetSize;
+
+    Integer round = config.clientIndex / subsetCount;
+
+    Integer excludedCount = backendCount % config.subsetSize;
+    Integer excludedStart = (round * excludedCount) % backendCount;
+    Integer excludedEnd = (excludedStart + excludedCount) % backendCount;
+    if (excludedStart <= excludedEnd) {
+      List subList = addresses.subList(0, excludedStart);
+      subList.addAll(addresses.subList(excludedEnd, backendCount-1));
+      addresses = new ArrayList(subList);
+    } else {
+      addresses = new ArrayList(addresses.subList(excludedEnd, excludedStart));
+    }
+
+    Random r = new Random(round);
+    Collections.shuffle(addresses, r);
+
+    Integer subsetId = config.clientIndex % subsetCount;
+
+    Integer start = subsetId * config.subsetSize;
+    Integer end = start + config.subsetSize;
+
+    List<SocketAddress> subset = addresses.subList(start, end);
+
+    // TODO: there is most certainly a cleaner way of doing this that retains the address groups
+    // one idea is that we return the full resolved list, but only connect to the relevant ones and disconnect
+    // to the others.
+    ArrayList<EquivalentAddressGroup>  list = new ArrayList<>();
+    list.add(new EquivalentAddressGroup(subset));
+
+    ResolvedAddresses.Builder builder = allAddresses.toBuilder();
+    return builder.setAddresses(list).build();
   }
 
   @Override
@@ -78,6 +132,14 @@ public final class DeterministicSubsettingLoadBalancer extends LoadBalancer {
       DeterministicSubsettingSubchannel subchannel = new DeterministicSubsettingSubchannel(delegate.createSubchannel(args));
       return subchannel;
     }
+  }
+
+  class AddressComparator implements Comparator<SocketAddress> {
+    @Override
+    public int compare(SocketAddress o1, SocketAddress o2){
+      return o1.hashCode() - o2.hashCode();
+    }
+
   }
 
   class DeterministicSubsettingSubchannel extends ForwardingSubchannel {
