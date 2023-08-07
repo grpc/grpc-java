fad093b878a296db3c063e04198533bb29b39694
Dongqing Hu <sorra@outlook.com>
2023-08-07 12:13:01 +0800

    update copyright year

--- api/src/main/java/io/grpc/ServerTimeoutManager.java
+++ api/src/main/java/io/grpc/ServerTimeoutManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The gRPC Authors
+ * Copyright 2023 The gRPC Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ public class ServerTimeoutManager {
   }
 
   /**
-   * Schedules a timeout and calls the RPC method invocation.
+   * Calls the RPC method invocation with a timeout scheduled.
    * Invalidates the timeout if the invocation completes in time.
    *
    * @param invocation The RPC method invocation that processes a request.
