f832b20be0dbeecfd965f469af42b8c7d5b2df9a
John Cormie <johncormie@gmail.com>
2023-11-16 16:26:58 -0800

    "Clarify" that NameResolver can only be started once.

--- api/src/main/java/io/grpc/NameResolver.java
+++ api/src/main/java/io/grpc/NameResolver.java
@@ -76,6 +76,8 @@ public abstract class NameResolver {
    * Starts the resolution. The method is not supposed to throw any exceptions. That might cause the
    * Channel that the name resolver is serving to crash. Errors should be propagated
    * through {@link Listener#onError}.
+   * 
+   * <p>An instance may not be started more than once, by any overload of this method. 
    *
    * @param listener used to receive updates on the target
    * @since 1.0.0
@@ -102,6 +104,8 @@ public abstract class NameResolver {
    * Starts the resolution. The method is not supposed to throw any exceptions. That might cause the
    * Channel that the name resolver is serving to crash. Errors should be propagated
    * through {@link Listener2#onError}.
+   * 
+   * <p>An instance may not be started more than once, by any overload of this method.
    *
    * @param listener used to receive updates on the target
    * @since 1.21.0
