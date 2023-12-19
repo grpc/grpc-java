ad9678ec6dc33b4b96cbc5e71b7627832c9ea65c
Larry Safran <lsafran@google.com>
2023-12-19 10:11:11 -0800

    Add increment as identified in #10768 (#10769)

--- util/src/main/java/io/grpc/util/MultiChildLoadBalancer.java
+++ util/src/main/java/io/grpc/util/MultiChildLoadBalancer.java
@@ -556,7 +556,7 @@ public abstract class MultiChildLoadBalancer extends LoadBalancer {
       addrs = new String[eag.getAddresses().size()];
       int i = 0;
       for (SocketAddress address : eag.getAddresses()) {
-        addrs[i] = address.toString();
+        addrs[i++] = address.toString();
       }
       Arrays.sort(addrs);
 
