a237a9100a4a56eb72e610f26576ec00f66506b7
Sergii Tkachenko <sergiitk@google.com>
2024-01-12 10:30:33 -0800

    spongify_logs complete

--- buildscripts/kokoro/kokoro.sh
+++ buildscripts/kokoro/kokoro.sh
@@ -2,4 +2,6 @@
 
 spongify_logs() {
   find . -name 'TEST-*.xml' | xargs -I{} -P30 sh -c $'f=\'{}\'; mkdir "${f%.xml}" && cp "$f" "${f%.xml}/sponge_log.xml"'
+  # Just to output the time.
+  echo "spongify_logs complete"
 }
