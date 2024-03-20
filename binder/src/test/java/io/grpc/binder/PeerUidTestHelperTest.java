b0bd2a07f6663c365eda8ea6e13ec25091556758
Aaron Tom <aaronbtom@google.com>
2024-03-20 13:43:36 -0400

    Update style to match GoogleCode style

--- binder/src/test/java/io/grpc/binder/PeerUidTestHelperTest.java
+++ binder/src/test/java/io/grpc/binder/PeerUidTestHelperTest.java
@@ -31,90 +31,93 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class PeerUidTestHelperTest {
-    @Rule public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
-    private static final int FAKE_UID = 12345;
+  @Rule
+  public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
-    private final AtomicReference<PeerUid> clientUidCapture = new AtomicReference<>();
+  private static final int FAKE_UID = 12345;
 
-    @Test
-    public void keyPopulatedWithInterceptorAndHeader() throws Exception {
-        makeServiceCall(/* includeInterceptor= */ true, /* includeUidInHeader= */ true, FAKE_UID);
-        assertThat(clientUidCapture.get()).isEqualTo(new PeerUid(FAKE_UID));
+  private final AtomicReference<PeerUid> clientUidCapture = new AtomicReference<>();
+
+  @Test
+  public void keyPopulatedWithInterceptorAndHeader() throws Exception {
+    makeServiceCall(/* includeInterceptor= */ true, /* includeUidInHeader= */ true, FAKE_UID);
+    assertThat(clientUidCapture.get()).isEqualTo(new PeerUid(FAKE_UID));
+  }
+
+  @Test
+  public void keyNotPopulatedWithInterceptorAndNoHeader() throws Exception {
+    makeServiceCall(/* includeInterceptor= */ true, /* includeUidInHeader= */ false, /* uid= */ -1);
+    assertThat(clientUidCapture.get()).isNull();
+  }
+
+  @Test
+  public void keyNotPopulatedWithoutInterceptorAndWithHeader() throws Exception {
+    makeServiceCall(
+        /* includeInterceptor= */ false, /* includeUidInHeader= */ true, /* uid= */ FAKE_UID);
+    assertThat(clientUidCapture.get()).isNull();
+  }
+
+  private final MethodDescriptor<String, String> method =
+      MethodDescriptor.newBuilder(StringMarshaller.INSTANCE, StringMarshaller.INSTANCE)
+          .setFullMethodName("test/method")
+          .setType(MethodDescriptor.MethodType.UNARY)
+          .build();
+
+  private void makeServiceCall(boolean includeInterceptor, boolean includeUidInHeader, int uid)
+      throws Exception {
+    ServerCallHandler<String, String> callHandler =
+        ServerCalls.asyncUnaryCall(
+            (req, respObserver) -> {
+              clientUidCapture.set(PeerUids.REMOTE_PEER.get());
+              respObserver.onNext(req);
+              respObserver.onCompleted();
+            });
+    ImmutableList<ServerInterceptor> interceptors;
+    if (includeInterceptor) {
+      interceptors = ImmutableList.of(PeerUidTestHelper.newTestPeerIdentifyingServerInterceptor());
+    } else {
+      interceptors = ImmutableList.of();
+    }
+    ServerServiceDefinition serviceDef =
+        ServerInterceptors.intercept(
+            ServerServiceDefinition.builder("test").addMethod(method, callHandler).build(),
+            interceptors);
+
+    InProcessServerBuilder server =
+        InProcessServerBuilder.forName("test").directExecutor().addService(serviceDef);
+
+    grpcCleanup.register(server.build().start());
+
+    Channel channel = InProcessChannelBuilder.forName("test").directExecutor().build();
+    grpcCleanup.register((ManagedChannel) channel);
+
+    if (includeUidInHeader) {
+      Metadata header = new Metadata();
+      header.put(PeerUidTestHelper.UID_KEY, uid);
+      channel =
+          ClientInterceptors.intercept(channel, MetadataUtils.newAttachHeadersInterceptor(header));
     }
 
-    @Test
-    public void keyNotPopulatedWithInterceptorAndNoHeader() throws Exception {
-        makeServiceCall(/* includeInterceptor= */ true, /* includeUidInHeader= */ false, /* uid= */ -1);
-        assertThat(clientUidCapture.get()).isNull();
+    ClientCalls.blockingUnaryCall(channel, method, CallOptions.DEFAULT, "hello");
+  }
+
+  private static class StringMarshaller implements MethodDescriptor.Marshaller<String> {
+
+    public static final StringMarshaller INSTANCE = new StringMarshaller();
+
+    @Override
+    public InputStream stream(String value) {
+      return new ByteArrayInputStream(value.getBytes(UTF_8));
     }
 
-    @Test
-    public void keyNotPopulatedWithoutInterceptorAndWithHeader() throws Exception {
-        makeServiceCall(
-                /* includeInterceptor= */ false, /* includeUidInHeader= */ true, /* uid= */ FAKE_UID);
-        assertThat(clientUidCapture.get()).isNull();
+    @Override
+    public String parse(InputStream stream) {
+      try {
+        return new String(stream.readAllBytes(), UTF_8);
+      } catch (IOException ex) {
+        throw new RuntimeException(ex);
+      }
     }
-
-    private final MethodDescriptor<String, String> method =
-            MethodDescriptor.newBuilder(StringMarshaller.INSTANCE, StringMarshaller.INSTANCE)
-                    .setFullMethodName("test/method")
-                    .setType(MethodDescriptor.MethodType.UNARY)
-                    .build();
-
-    private void makeServiceCall(boolean includeInterceptor, boolean includeUidInHeader, int uid)
-            throws Exception {
-        ServerCallHandler<String, String> callHandler =
-                ServerCalls.asyncUnaryCall(
-                        (req, respObserver) -> {
-                            clientUidCapture.set(PeerUids.REMOTE_PEER.get());
-                            respObserver.onNext(req);
-                            respObserver.onCompleted();
-                        });
-        ImmutableList<ServerInterceptor> interceptors;
-        if (includeInterceptor) {
-            interceptors = ImmutableList.of(PeerUidTestHelper.newTestPeerIdentifyingServerInterceptor());
-        } else {
-            interceptors = ImmutableList.of();
-        }
-        ServerServiceDefinition serviceDef =
-                ServerInterceptors.intercept(
-                        ServerServiceDefinition.builder("test").addMethod(method, callHandler).build(),
-                        interceptors);
-
-        InProcessServerBuilder server =
-                InProcessServerBuilder.forName("test").directExecutor().addService(serviceDef);
-
-        grpcCleanup.register(server.build().start());
-
-        Channel channel = InProcessChannelBuilder.forName("test").directExecutor().build();
-        grpcCleanup.register((ManagedChannel) channel);
-
-        if (includeUidInHeader) {
-            Metadata header = new Metadata();
-            header.put(PeerUidTestHelper.UID_KEY, uid);
-            channel =
-                    ClientInterceptors.intercept(channel, MetadataUtils.newAttachHeadersInterceptor(header));
-        }
-
-        ClientCalls.blockingUnaryCall(channel, method, CallOptions.DEFAULT, "hello");
-    }
-
-    private static class StringMarshaller implements MethodDescriptor.Marshaller<String> {
-        public static final StringMarshaller INSTANCE = new StringMarshaller();
-
-        @Override
-        public InputStream stream(String value) {
-            return new ByteArrayInputStream(value.getBytes(UTF_8));
-        }
-
-        @Override
-        public String parse(InputStream stream) {
-            try {
-                return new String(stream.readAllBytes(), UTF_8);
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-    }
+  }
 }
--- binder/src/testFixtures/java/io/grpc/binder/PeerUidTestHelper.java
+++ binder/src/testFixtures/java/io/grpc/binder/PeerUidTestHelper.java
@@ -7,50 +7,57 @@ import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 
-/** Class which helps set up {@link PeerUids} to be used in tests. */
+/**
+ * Class which helps set up {@link PeerUids} to be used in tests.
+ */
 public class PeerUidTestHelper {
 
-    /** The UID of the calling package is set with the value of this key. */
-    public static final Metadata.Key<Integer> UID_KEY =
-            Metadata.Key.of("binder-remote-uid-for-unit-testing", PeerUidTestMarshaller.INSTANCE);
+  /**
+   * The UID of the calling package is set with the value of this key.
+   */
+  public static final Metadata.Key<Integer> UID_KEY =
+      Metadata.Key.of("binder-remote-uid-for-unit-testing", PeerUidTestMarshaller.INSTANCE);
 
-    /**
-     * Creates an interceptor that associates the {@link PeerUids#REMOTE_PEER} key in the request
-     * {@link Context} with a UID provided by the client in the {@link #UID_KEY} request header, if
-     * present.
-     *
-     * <p>The returned interceptor works with any gRPC transport but is meant for in-process unit
-     * testing of gRPC/binder services that depend on {@link PeerUids}.
-     */
-    public static ServerInterceptor newTestPeerIdentifyingServerInterceptor() {
-        return new ServerInterceptor() {
-            @Override
-            public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
-                    ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
-                Context context = Context.current();
-                if (headers.containsKey(UID_KEY)) {
-                    context = context.withValue(PeerUids.REMOTE_PEER, new PeerUid(headers.get(UID_KEY)));
-                }
-
-                return Contexts.interceptCall(context, call, headers, next);
-            }
-        };
-    }
-
-    private PeerUidTestHelper() {}
-
-    private static class PeerUidTestMarshaller implements Metadata.AsciiMarshaller<Integer> {
-        public static final PeerUidTestMarshaller INSTANCE = new PeerUidTestMarshaller();
-
-        @Override
-        public String toAsciiString(Integer value) {
-            return value.toString();
+  /**
+   * Creates an interceptor that associates the {@link PeerUids#REMOTE_PEER} key in the request
+   * {@link Context} with a UID provided by the client in the {@link #UID_KEY} request header, if
+   * present.
+   *
+   * <p>The returned interceptor works with any gRPC transport but is meant for in-process unit
+   * testing of gRPC/binder services that depend on {@link PeerUids}.
+   */
+  public static ServerInterceptor newTestPeerIdentifyingServerInterceptor() {
+    return new ServerInterceptor() {
+      @Override
+      public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+          ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        Context context = Context.current();
+        if (headers.containsKey(UID_KEY)) {
+          context = context.withValue(PeerUids.REMOTE_PEER, new PeerUid(headers.get(UID_KEY)));
         }
 
-        @Override
-        public Integer parseAsciiString(String serialized) {
-            return Integer.parseInt(serialized);
-        }
+        return Contexts.interceptCall(context, call, headers, next);
+      }
+    };
+  }
+
+  private PeerUidTestHelper() {
+  }
+
+  private static class PeerUidTestMarshaller implements Metadata.AsciiMarshaller<Integer> {
+
+    public static final PeerUidTestMarshaller INSTANCE = new PeerUidTestMarshaller();
+
+    @Override
+    public String toAsciiString(Integer value) {
+      return value.toString();
     }
-    ;
+
+    @Override
+    public Integer parseAsciiString(String serialized) {
+      return Integer.parseInt(serialized);
+    }
+  }
+
+  ;
 }
