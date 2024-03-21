7c2f8b5b97e3df2294bad2c481518cd8909bc667
Larry Safran <lsafran@google.com>
2024-03-21 14:47:58 -0700

    Fix test flakiness where IPv6 was not considered loopback

--- interop-testing/src/test/java/io/grpc/testing/integration/Http2Test.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/Http2Test.java
@@ -18,6 +18,7 @@ package io.grpc.testing.integration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import io.grpc.ChannelCredentials;
 import io.grpc.ManagedChannelBuilder;
@@ -139,7 +140,7 @@ public class Http2Test extends AbstractInteropTest {
   @Test
   public void remoteAddr() {
     InetSocketAddress isa = (InetSocketAddress) obtainRemoteClientAddr();
-    assertEquals(InetAddress.getLoopbackAddress(), isa.getAddress());
+    assertTrue(isa.getAddress().isLoopbackAddress());
     // It should not be the same as the server
     assertNotEquals(((InetSocketAddress) getListenAddress()).getPort(), isa.getPort());
   }
@@ -147,7 +148,7 @@ public class Http2Test extends AbstractInteropTest {
   @Test
   public void localAddr() throws Exception {
     InetSocketAddress isa = (InetSocketAddress) obtainLocalServerAddr();
-    assertEquals(InetAddress.getLoopbackAddress(), isa.getAddress());
+    assertTrue(isa.getAddress().isLoopbackAddress());
     assertEquals(((InetSocketAddress) getListenAddress()).getPort(), isa.getPort());
   }
 
