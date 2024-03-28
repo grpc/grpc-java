19f970f8789c0c706b4185eaa5d253d3e1d3a039
hypnoce <hypnoce@donarproject.org>
2024-03-28 11:17:13 +0100

    Fixes #11053: make sure to remove finished stream in okhttp client transport even if a pending stream was started.

--- okhttp/src/test/java/io/grpc/okhttp/OkHttpClientTransportTest.java
+++ okhttp/src/test/java/io/grpc/okhttp/OkHttpClientTransportTest.java
@@ -2080,6 +2080,26 @@ public class OkHttpClientTransportTest {
     assertEquals(MISCARRIED, listener3.rpcProgress);
   }
 
+  @Test
+  public void finishedStreamRemovedFromInUseState() throws Exception {
+    initTransport();
+    setMaxConcurrentStreams(1);
+    final MockStreamListener listener = new MockStreamListener();
+    OkHttpClientStream stream =
+        clientTransport.newStream(method, new Metadata(), CallOptions.DEFAULT, tracers);
+    stream.start(listener);
+    OkHttpClientStream pendingStream =
+        clientTransport.newStream(method, new Metadata(), CallOptions.DEFAULT, tracers);
+    pendingStream.start(listener);
+    waitForStreamPending(1);
+    clientTransport.finishStream(stream.transportState().id(), Status.OK, PROCESSED,
+        false, null, null);
+    verify(transportListener).transportInUse(true);
+    clientTransport.finishStream(pendingStream.transportState().id(), Status.OK, PROCESSED,
+        false, null, null);
+    verify(transportListener).transportInUse(false);
+  }
+
   private int activeStreamCount() {
     return clientTransport.getActiveStreams().length;
   }
