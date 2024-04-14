dfdff9d8045a8b41468f7927ed56b4d0d07110fd
tangjiafu <jiafu.tang@qq.com>
2024-04-14 12:26:38 +0800

    revert RouteGuideServer

--- examples/src/main/java/io/grpc/examples/routeguide/RouteGuideServer.java
+++ examples/src/main/java/io/grpc/examples/routeguide/RouteGuideServer.java
@@ -72,16 +72,19 @@ public class RouteGuideServer {
   public void start() throws IOException {
     server.start();
     logger.info("Server started, listening on " + port);
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      // Use stderr here since the logger may have been reset by its JVM shutdown hook.
-      System.err.println("*** shutting down gRPC server since JVM is shutting down");
-      try {
-        RouteGuideServer.this.stop();
-      } catch (InterruptedException e) {
-        e.printStackTrace(System.err);
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+        System.err.println("*** shutting down gRPC server since JVM is shutting down");
+        try {
+          RouteGuideServer.this.stop();
+        } catch (InterruptedException e) {
+          e.printStackTrace(System.err);
+        }
+        System.err.println("*** server shut down");
       }
-      System.err.println("*** server shut down");
-    }));
+    });
   }
 
   /** Stop serving requests and shutdown resources. */
