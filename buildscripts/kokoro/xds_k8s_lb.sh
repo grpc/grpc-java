d93f35c5e6981bf79c07ded562b0b3f64d52c556
Sergii Tkachenko <sergiitk@google.com>
2024-04-23 12:07:39 -0700

    Port security suite and move common build to psm-interop-build.sh

--- buildscripts/kokoro/xds_k8s_lb.sh
+++ buildscripts/kokoro/xds_k8s_lb.sh
@@ -6,66 +6,6 @@ readonly GRPC_LANGUAGE="java"
 readonly GITHUB_REPOSITORY_NAME="grpc-java"
 readonly TEST_DRIVER_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/${TEST_DRIVER_REPO_OWNER:-grpc}/psm-interop/${TEST_DRIVER_BRANCH:-main}/.kokoro/psm_interop_kokoro_lib.sh"
 
-# Java-specific
-readonly BUILD_APP_PATH="interop-testing/build/install/grpc-interop-testing"
-
-#######################################
-# Builds the test app using gradle and smoke-checks its binaries
-# Globals:
-#   SRC_DIR
-#   BUILD_APP_PATH
-# Arguments:
-#   None
-# Outputs:
-#   Writes the output of xds-test-client and xds-test-server --help to stderr
-#######################################
-build_java_test_app() {
-  echo "Building Java test app"
-  cd "${SRC_DIR}"
-  GRADLE_OPTS="-Dorg.gradle.jvmargs='-Xmx1g'" \
-  ./gradlew --no-daemon grpc-interop-testing:installDist -x test \
-    -PskipCodegen=true -PskipAndroid=true --console=plain
-
-  # Test-run binaries
-  run_ignore_exit_code "${SRC_DIR}/${BUILD_APP_PATH}/bin/xds-test-client" --help
-  run_ignore_exit_code "${SRC_DIR}/${BUILD_APP_PATH}/bin/xds-test-server" --help
-}
-
-#######################################
-# Builds test app Docker images and pushes them to GCR
-# Globals:
-#   BUILD_APP_PATH
-#   SERVER_IMAGE_NAME: Test server Docker image name
-#   CLIENT_IMAGE_NAME: Test client Docker image name
-#   GIT_COMMIT: SHA-1 of git commit being built
-#   TESTING_VERSION: version branch under test, f.e. v1.42.x, master
-# Arguments:
-#   None
-# Outputs:
-#   Writes the output of `gcloud builds submit` to stdout, stderr
-#######################################
-build_test_app_docker_images() {
-  build_java_test_app
-
-  echo "Building Java xDS interop test app Docker images"
-  local docker_dir="${SRC_DIR}/buildscripts/xds-k8s"
-  local build_dir
-  build_dir="$(mktemp -d)"
-  # Copy Docker files, log properties, and the test app to the build dir
-  cp -v "${docker_dir}/"*.Dockerfile "${build_dir}"
-  cp -v "${docker_dir}/"*.properties "${build_dir}"
-  cp -rv "${SRC_DIR}/${BUILD_APP_PATH}" "${build_dir}"
-  # Pick a branch name for the built image
-  local branch_name='experimental'
-  if is_version_branch "${TESTING_VERSION}"; then
-    branch_name="${TESTING_VERSION}"
-  fi
-  # Run Google Cloud Build
-  gcloud builds submit "${build_dir}" \
-    --config "${docker_dir}/cloudbuild.yaml" \
-    --substitutions "_SERVER_IMAGE_NAME=${SERVER_IMAGE_NAME},_CLIENT_IMAGE_NAME=${CLIENT_IMAGE_NAME},COMMIT_SHA=${GIT_COMMIT},BRANCH_NAME=${branch_name}"
-}
-
 #######################################
 # Executes the test case
 # Globals:
