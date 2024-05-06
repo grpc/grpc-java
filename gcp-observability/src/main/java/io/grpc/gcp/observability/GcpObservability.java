fb1cdfe345705ce4849b98bba8fbdaeae2be043b
Eric Anderson <ejona@google.com>
2024-05-06 07:27:41 -0700

    Migrate GlobalInterceptors to ConfiguratorRegistry
    This should preserve all the existing behavior of GlobalInterceptors as
    used by grpc-gcp-observability, including it disabling the implicit
    OpenCensus integration.
    
    Both the old and new API are internal. I hid Configurator and
    ConfiguratorRegistry behind Internal-prefixed classes, like had been
    done with GlobalInterceptors to further discourage use until the API is
    ready.
    
    GlobalInterceptorsTest was modified to become ConfiguratorRegistryTest.

--- gcp-observability/src/main/java/io/grpc/gcp/observability/GcpObservability.java
+++ gcp-observability/src/main/java/io/grpc/gcp/observability/GcpObservability.java
@@ -19,10 +19,14 @@ package io.grpc.gcp.observability;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import io.grpc.ClientInterceptor;
-import io.grpc.InternalGlobalInterceptors;
+import io.grpc.InternalConfigurator;
+import io.grpc.InternalConfiguratorRegistry;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.ManagedChannelProvider.ProviderNotFoundException;
+import io.grpc.ServerBuilder;
 import io.grpc.ServerInterceptor;
 import io.grpc.ServerStreamTracer;
 import io.grpc.census.InternalCensusStatsAccessor;
@@ -55,7 +59,9 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
@@ -161,8 +167,42 @@ public final class GcpObservability implements AutoCloseable {
       tracerFactories.add(InternalCensusTracingAccessor.getServerStreamTracerFactory());
     }
 
-    InternalGlobalInterceptors.setInterceptorsTracers(
-        clientInterceptors, serverInterceptors, tracerFactories);
+    InternalConfiguratorRegistry.setConfigurators(Arrays.asList(
+        new ObservabilityConfigurator(clientInterceptors, serverInterceptors, tracerFactories)));
+  }
+
+  @VisibleForTesting
+  static final class ObservabilityConfigurator implements InternalConfigurator {
+    final List<ClientInterceptor> clientInterceptors;
+    final List<ServerInterceptor> serverInterceptors;
+    final List<ServerStreamTracer.Factory> tracerFactories;
+
+    ObservabilityConfigurator(
+        List<ClientInterceptor> clientInterceptors,
+        List<ServerInterceptor> serverInterceptors,
+        List<ServerStreamTracer.Factory> tracerFactories) {
+      this.clientInterceptors = ImmutableList.copyOf(
+          checkNotNull(clientInterceptors, "clientInterceptors"));
+      this.serverInterceptors = ImmutableList.copyOf(
+          checkNotNull(serverInterceptors, "serverInterceptors"));
+      this.tracerFactories = ImmutableList.copyOf(
+          checkNotNull(tracerFactories, "tracerFactories"));
+    }
+
+    @Override
+    public void configureChannelBuilder(ManagedChannelBuilder<?> builder) {
+      builder.intercept(clientInterceptors);
+    }
+
+    @Override
+    public void configureServerBuilder(ServerBuilder<?> builder) {
+      for (ServerInterceptor interceptor : serverInterceptors) {
+        builder.intercept(interceptor);
+      }
+      for (ServerStreamTracer.Factory factory : tracerFactories) {
+        builder.addStreamTracerFactory(factory);
+      }
+    }
   }
 
   static ConditionalClientInterceptor getConditionalInterceptor(ClientInterceptor interceptor) {
