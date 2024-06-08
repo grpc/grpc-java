bf0e43710906b944b92547d6174c7c9a2142b62b
Andrey Ermolov <aermolov@google.com>
2024-06-08 01:10:54 +0000

    Delete unused imports

--- util/src/test/java/io/grpc/util/AdvancedTlsX509TrustManagerTest.java
+++ util/src/test/java/io/grpc/util/AdvancedTlsX509TrustManagerTest.java
@@ -20,11 +20,6 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 import io.grpc.internal.FakeClock;
 import io.grpc.internal.testing.TestUtils;
 import io.grpc.testing.TlsTesting;
