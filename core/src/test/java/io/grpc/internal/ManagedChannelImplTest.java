904b89362c87991f3c52d4d764988c10538dbbe7
vhabib <vinodhabib@gmail.com>
2024-07-24 19:38:15 +0530

    defect-11040 - Fixed Review points.

--- core/src/test/java/io/grpc/internal/ManagedChannelImplTest.java
+++ core/src/test/java/io/grpc/internal/ManagedChannelImplTest.java
@@ -4479,6 +4479,8 @@ public class ManagedChannelImplTest {
   @Test
   public void testApplyDefaultServiceConfigIfInitialNameResolutionFails() throws Exception {
     LoadBalancerRegistry.getDefaultRegistry().register(mockLoadBalancerProvider);
+    channelBuilder.maxTraceEvents(10);
+    timer.forwardNanos(1234);
     try {
       FakeNameResolverFactory nameResolverFactory = new FakeNameResolverFactory
               .Builder(expectedUri).setServers(Collections.singletonList(new
@@ -4495,6 +4497,14 @@ public class ManagedChannelImplTest {
               .withDescription("Initial Name Resolution error, using default service config");
       resolver.listener.onError(resolutionError);
       verify(mockLoadBalancer).handleNameResolutionError(resolutionError);
+      int prevSize = getStats(channel).channelTrace.events.size();
+      assertThat(getStats(channel).channelTrace.events).hasSize(prevSize);
+      assertThat(getStats(channel).channelTrace.events.get(prevSize - 1))
+              .isEqualTo(new ChannelTrace.Event.Builder()
+              .setDescription("Initial Name Resolution error, using default service config")
+              .setSeverity(ChannelTrace.Event.Severity.CT_ERROR)
+              .setTimestampNanos(timer.getTicker().read())
+              .build());
     } finally {
       LoadBalancerRegistry.getDefaultRegistry().deregister(mockLoadBalancerProvider);
     }
