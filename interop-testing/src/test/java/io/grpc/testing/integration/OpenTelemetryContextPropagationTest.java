096441ed31459d7f6958ba7aa52ace5fc22a1d5c
yifeizhuang <zivy@google.com>
2024-09-18 13:48:46 -0700

    refactor test to parameterized

--- interop-testing/src/test/java/io/grpc/testing/integration/OpenTelemetryContextPropagationTest.java
+++ interop-testing/src/test/java/io/grpc/testing/integration/OpenTelemetryContextPropagationTest.java
@@ -26,9 +26,12 @@ import io.grpc.ServerBuilder;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
+import io.grpc.netty.InternalNettyChannelBuilder;
 import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.netty.NettyServerBuilder;
 import io.grpc.opentelemetry.GrpcOpenTelemetry;
+import io.grpc.opentelemetry.GrpcTraceBinContextPropagator;
+import io.grpc.opentelemetry.InternalGrpcOpenTelemetry;
 import io.grpc.testing.integration.Messages.SimpleRequest;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
@@ -36,23 +39,49 @@ import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
 import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.runners.Parameterized;
 
-@RunWith(JUnit4.class)
-public class OpenTelemetryContextFilterTest extends AbstractInteropTest {
-  private final OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
-      .setTracerProvider(SdkTracerProvider.builder().build())
-      .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-      .build();
-  private final Tracer tracer = openTelemetrySdk.getTracer("grpc-java");
-  private final GrpcOpenTelemetry grpcOpenTelemetry = GrpcOpenTelemetry.newBuilder()
-      .sdk(openTelemetrySdk).build();
+@RunWith(Parameterized.class)
+public class OpenTelemetryContextPropagationTest extends AbstractInteropTest {
+  private final OpenTelemetrySdk openTelemetrySdk;
+  private final Tracer tracer;
+  private final GrpcOpenTelemetry grpcOpenTelemetry;
   private final AtomicReference<Span> applicationSpan = new AtomicReference<>();
+  private final boolean censusClient;
+
+  @Parameterized.Parameters(name = "ContextPropagator={0}, CensusClient={1}")
+  public static Iterable<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+        {W3CTraceContextPropagator.getInstance(), false},
+        {GrpcTraceBinContextPropagator.defaultInstance(), false},
+        {GrpcTraceBinContextPropagator.defaultInstance(), true}
+    });
+  }
+
+  public OpenTelemetryContextPropagationTest(TextMapPropagator textMapPropagator,
+                                             boolean isCensusClient) {
+    this.openTelemetrySdk = OpenTelemetrySdk.builder()
+        .setTracerProvider(SdkTracerProvider.builder().build())
+        .setPropagators(ContextPropagators.create(TextMapPropagator.composite(
+            textMapPropagator
+        )))
+        .build();
+    this.tracer = openTelemetrySdk
+        .getTracer("grpc-java-interop-test");
+    GrpcOpenTelemetry.Builder grpcOpentelemetryBuilder = GrpcOpenTelemetry.newBuilder()
+        .sdk(openTelemetrySdk);
+    InternalGrpcOpenTelemetry.enableTracing(grpcOpentelemetryBuilder, true);
+    grpcOpenTelemetry = grpcOpentelemetryBuilder.build();
+    this.censusClient = isCensusClient;
+  }
 
   @Override
   protected ServerBuilder<?> getServerBuilder() {
@@ -77,32 +106,37 @@ public class OpenTelemetryContextFilterTest extends AbstractInteropTest {
 
           @Override
           public void onHalfClose() {
-            if (applicationSpan.get() != null) {
-              applicationSpan.get().end();
-            }
+            maybeCloseSpan(applicationSpan);
             delegate().onHalfClose();
           }
 
           @Override
           public void onCancel() {
-            if (applicationSpan.get() != null) {
-              applicationSpan.get().end();
-            }
+            maybeCloseSpan(applicationSpan);
             delegate().onCancel();
           }
 
           @Override
           public void onComplete() {
-            if (applicationSpan.get() != null) {
-              applicationSpan.get().end();
-            }
+            maybeCloseSpan(applicationSpan);
             delegate().onComplete();
           }
         };
       }
     });
+    // To ensure proper propagation of remote spans from gRPC to your application, this interceptor
+    // must be after any application interceptors that interact with spans. This allows the tracing
+    // information to be correctly passed along. However, it's fine for application-level onMessage
+    // handlers to access the span.
     grpcOpenTelemetry.configureServerBuilder(builder);
     return builder;
+  }
+
+  private void maybeCloseSpan(AtomicReference<Span> applicationSpan) {
+    Span tmp = applicationSpan.get();
+    if (tmp != null) {
+      tmp.end();
+    }
   }
 
   @Override
@@ -115,17 +149,43 @@ public class OpenTelemetryContextFilterTest extends AbstractInteropTest {
     NettyChannelBuilder builder = NettyChannelBuilder.forAddress(getListenAddress())
         .maxInboundMessageSize(AbstractInteropTest.MAX_MESSAGE_SIZE)
         .usePlaintext();
-    grpcOpenTelemetry.configureChannelBuilder(builder);
+    if (!censusClient) {
+      // Disabling census-tracing is necessary to avoid trace ID mismatches.
+      // This is because census-tracing overrides the grpc-trace-bin header with
+      // OpenTelemetry's GrpcTraceBinPropagator.
+       InternalNettyChannelBuilder.setTracingEnabled(builder, false);
+       grpcOpenTelemetry.configureChannelBuilder(builder);
+    }
     return builder;
   }
 
   @Test
   public void otelSpanContextPropagation() {
+    Assume.assumeFalse(censusClient);
     Span parentSpan = tracer.spanBuilder("Test.interopTest").startSpan();
     try (Scope scope = Context.current().with(parentSpan).makeCurrent()) {
       blockingStub.unaryCall(SimpleRequest.getDefaultInstance());
     }
     assertEquals(parentSpan.getSpanContext().getTraceId(),
         applicationSpan.get().getSpanContext().getTraceId());
+  }
+
+  @Test
+  @SuppressWarnings("deprecation")
+  public void censusToOtelGrpcTraceBinPropagator() {
+    Assume.assumeTrue(censusClient);
+    io.opencensus.trace.Tracer censusTracer = io.opencensus.trace.Tracing.getTracer();
+    io.opencensus.trace.Span parentSpan = censusTracer.spanBuilder("Test.interopTest")
+        .startSpan();
+    io.grpc.Context context =  io.opencensus.trace.unsafe.ContextUtils.withValue(
+        io.grpc.Context.current(), parentSpan);
+    io.grpc.Context previous = context.attach();
+    try {
+      blockingStub.unaryCall(SimpleRequest.getDefaultInstance());
+      assertEquals(parentSpan.getContext().getTraceId().toLowerBase16(),
+          applicationSpan.get().getSpanContext().getTraceId());
+    } finally {
+      context.detach(previous);
+    }
   }
 }
