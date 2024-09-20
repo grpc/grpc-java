5aa9794d48c60b3a87f69f530f99c1f3b84f328a
Riya Mehta <riyamehta@google.com>
2024-09-20 08:40:47 -0700

    update non-executor version.

--- netty/src/main/java/io/grpc/netty/InternalProtocolNegotiators.java
+++ netty/src/main/java/io/grpc/netty/InternalProtocolNegotiators.java
@@ -70,26 +70,7 @@ public final class InternalProtocolNegotiators {
    * may happen immediately, even before the TLS Handshake is complete.
    */
   public static InternalProtocolNegotiator.ProtocolNegotiator tls(SslContext sslContext) {
-    final io.grpc.netty.ProtocolNegotiator negotiator = ProtocolNegotiators.tls(sslContext);
-    final class TlsNegotiator implements InternalProtocolNegotiator.ProtocolNegotiator {
-
-      @Override
-      public AsciiString scheme() {
-        return negotiator.scheme();
-      }
-
-      @Override
-      public ChannelHandler newHandler(GrpcHttp2ConnectionHandler grpcHandler) {
-        return negotiator.newHandler(grpcHandler);
-      }
-
-      @Override
-      public void close() {
-        negotiator.close();
-      }
-    }
-    
-    return new TlsNegotiator();
+    return tls(sslContext, null);
   }
 
   /**
