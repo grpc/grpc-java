d5f3968944601706782fb068ea4c50882a9c0ed7
Kannan J <kannanjgithub@google.com>
2024-12-04 10:17:52 +0000

    nit

--- examples/example-tls/src/main/java/io/grpc/examples/helloworldtls/HelloWorldClientTls.java
+++ examples/example-tls/src/main/java/io/grpc/examples/helloworldtls/HelloWorldClientTls.java
@@ -68,8 +68,8 @@ public class HelloWorldClientTls {
 
         if (args.length < 2 || args.length == 4 || args.length > 5) {
             System.out.println("USAGE: HelloWorldClientTls host port [trustCertCollectionFilePath " +
-                   "[clientCertChainFilePath clientPrivateKeyFilePath]]\n  Note: clientCertChainFilePath and " +
-                   "clientPrivateKeyFilePath are only needed if mutual auth is desired.");
+                    "[clientCertChainFilePath clientPrivateKeyFilePath]]\n  Note: clientCertChainFilePath and " +
+                    "clientPrivateKeyFilePath are only needed if mutual auth is desired.");
             System.exit(0);
         }
 
