33e35de7c1631cec51d818c4c181c59fc5027d22
Larry Safran <lsafran@google.com>
2024-12-10 17:47:37 -0800

    Add class leval Javadoc and copyright

--- xds/src/main/java/io/grpc/xds/XdsDependencyManager.java
+++ xds/src/main/java/io/grpc/xds/XdsDependencyManager.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.grpc.xds;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -10,13 +26,16 @@ import io.grpc.xds.XdsRouteConfigureResource.RdsUpdate;
 import io.grpc.xds.client.XdsClient;
 import io.grpc.xds.client.XdsLogger;
 import io.grpc.xds.client.XdsResourceType;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * This class acts as a layer of indirection between the XdsClient and the NameResolver. It
+ * maintains the watchers for the xds resources and when an update is received, it either requests
+ * referenced resources or updates the XdsConfig and notifies the XdsConfigWatcher.
+ */
 final class XdsDependencyManager implements XdsClusterSubscriptionRegistry {
   private final XdsClient xdsClient;
   private final XdsConfigWatcher xdsConfigWatcher;
