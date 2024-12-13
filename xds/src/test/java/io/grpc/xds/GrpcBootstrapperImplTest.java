1ac63293b0a58c0afdc978e234d989fda3f7bccc
Eric Anderson <ejona@google.com>
2024-12-13 15:54:19 -0800

    xds: Unexpected types in server_features should be ignored
    It was clearly defined in gRFC A30. The relevant text was copied as a
    comment in the code.
    
    As discovered due to grpc/grpc-go#7932

--- xds/src/test/java/io/grpc/xds/GrpcBootstrapperImplTest.java
+++ xds/src/test/java/io/grpc/xds/GrpcBootstrapperImplTest.java
@@ -651,6 +651,26 @@ public class GrpcBootstrapperImplTest {
   }
 
   @Test
+  public void serverFeatures_ignoresUnknownValues() throws XdsInitializationException {
+    String rawData = "{\n"
+        + "  \"xds_servers\": [\n"
+        + "    {\n"
+        + "      \"server_uri\": \"" + SERVER_URI + "\",\n"
+        + "      \"channel_creds\": [\n"
+        + "        {\"type\": \"insecure\"}\n"
+        + "      ],\n"
+        + "      \"server_features\": [null, {}, 3, true, \"unexpected\", \"trusted_xds_server\"]\n"
+        + "    }\n"
+        + "  ]\n"
+        + "}";
+
+    bootstrapper.setFileReader(createFileReader(BOOTSTRAP_FILE_PATH, rawData));
+    BootstrapInfo info = bootstrapper.bootstrap();
+    ServerInfo serverInfo = Iterables.getOnlyElement(info.servers());
+    assertThat(serverInfo.isTrustedXdsServer()).isTrue();
+  }
+
+  @Test
   public void notFound() {
     bootstrapper.bootstrapPathFromEnvVar = null;
     bootstrapper.bootstrapPathFromSysProp = null;
