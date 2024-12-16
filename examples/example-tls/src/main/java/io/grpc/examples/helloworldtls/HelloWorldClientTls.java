746717e9981c08e258c395b02d42735ca296e4d3
deadEternally <kannan.jayaprakasam@gmail.com>
2024-12-16 22:02:47 +0530

    Revert unintended changes.

--- examples/example-tls/src/main/java/io/grpc/examples/helloworldtls/HelloWorldClientTls.java
+++ examples/example-tls/src/main/java/io/grpc/examples/helloworldtls/HelloWorldClientTls.java
@@ -53,9 +53,7 @@ public class HelloWorldClientTls {
         HelloRequest request = HelloRequest.newBuilder().setName(name).build();
         HelloReply response;
         try {
-            response = io.grpc.stub.ClientCalls.blockingUnaryCall(
-                blockingStub.getChannel(), GreeterGrpc.getSayHelloMethod(),
-                blockingStub.getCallOptions().withAuthority("foo.test.google.in"), request);
+            response = blockingStub.sayHello(request);
         } catch (StatusRuntimeException e) {
             logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
             return;
@@ -90,7 +88,7 @@ public class HelloWorldClientTls {
         }
         String host = args[0];
         int port = Integer.parseInt(args[1]);
-        ManagedChannel channel = OkHttpChannelBuilder.forAddress(host, port, tlsBuilder.build())
+        ManagedChannel channel = Grpc.newChannelBuilderForAddress(host, port, tlsBuilder.build())
                 /* Only for using provided test certs. */
                 .overrideAuthority("foo.test.google.fr")
                 .build();
