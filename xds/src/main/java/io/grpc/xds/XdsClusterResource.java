f8f613984fe1c57171977e948315449e3d013ebc
vinodhabib <47808007+vinodhabib@users.noreply.github.com>
2024-12-17 05:37:22 +0000

    xds: fixed unsupported unsigned 32 bits issue for circuit breaker (#11735)
    Added change for circuit breaking by converting signed 32-bit Int to Unsigned 64-bit Long For MaxRequest negative value ( -1)
    
    Fixes #11695

--- xds/src/main/java/io/grpc/xds/XdsClusterResource.java
+++ xds/src/main/java/io/grpc/xds/XdsClusterResource.java
@@ -215,7 +215,7 @@ class XdsClusterResource extends XdsResourceType<CdsUpdate> {
           continue;
         }
         if (threshold.hasMaxRequests()) {
-          maxConcurrentRequests = (long) threshold.getMaxRequests().getValue();
+          maxConcurrentRequests = Integer.toUnsignedLong(threshold.getMaxRequests().getValue());
         }
       }
     }
