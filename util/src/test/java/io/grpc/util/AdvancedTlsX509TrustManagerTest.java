b762db6507b013c9b1f0a4c00388fac6b763980b
Albumen Kevin <jhq0812@gmail.com>
2025-01-05 18:42:21 +0800

    Fix jdk8

--- util/src/test/java/io/grpc/util/AdvancedTlsX509TrustManagerTest.java
+++ util/src/test/java/io/grpc/util/AdvancedTlsX509TrustManagerTest.java
@@ -31,6 +31,8 @@ import io.grpc.util.AdvancedTlsX509TrustManager.Verification;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -184,8 +186,8 @@ public class AdvancedTlsX509TrustManagerTest {
 
   private void replaceFile(File source, File target) throws IOException {
     try (InputStream in = new BufferedInputStream(
-        Files.newInputStream(source.toPath())); OutputStream os = new BufferedOutputStream(
-        Files.newOutputStream(target.toPath()))) {
+        new FileInputStream(source)); OutputStream os = new BufferedOutputStream(
+        new FileOutputStream(target))) {
       int b;
       while ((b = in.read()) != -1) {
         os.write(b);
