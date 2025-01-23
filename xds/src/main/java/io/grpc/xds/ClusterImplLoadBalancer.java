5225d091513b9bbfeeefe04ddcdbaa752e45aaf1
Kannan J <kannanjgithub@google.com>
2025-01-23 13:18:52 +0000

    Use Locale.US

--- xds/src/main/java/io/grpc/xds/ClusterImplLoadBalancer.java
+++ xds/src/main/java/io/grpc/xds/ClusterImplLoadBalancer.java
@@ -59,6 +59,7 @@ import io.grpc.xds.orca.OrcaPerRequestUtil.OrcaPerRequestReportListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
@@ -406,7 +407,7 @@ final class ClusterImplLoadBalancer extends LoadBalancer {
                 dropStats.recordDroppedRequest();
               }
               return PickResult.withDrop(Status.UNAVAILABLE.withDescription(
-                  String.format("Cluster max concurrent requests limit of %d exceeded",
+                  String.format(Locale.US, "Cluster max concurrent requests limit of %d exceeded",
                       maxConcurrentRequests)));
             }
           }
