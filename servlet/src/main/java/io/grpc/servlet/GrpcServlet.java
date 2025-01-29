a216eb97af38eb1bad492210777e4e58701d8acc
long76 <18124433+long76@users.noreply.github.com>
2025-01-29 17:32:58 +0300

    fix(GrpcServlet): import ServletException

--- servlet/src/main/java/io/grpc/servlet/GrpcServlet.java
+++ servlet/src/main/java/io/grpc/servlet/GrpcServlet.java
@@ -20,6 +20,7 @@ import io.grpc.BindableService;
 import io.grpc.ExperimentalApi;
 import java.io.IOException;
 import java.util.List;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
