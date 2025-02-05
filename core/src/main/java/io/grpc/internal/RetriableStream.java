44e92e2c2c72cd9fc03b1fda1d5335ddf8a1b618
Abhishek Agrawal <81427947+AgraVator@users.noreply.github.com>
2025-02-06 03:48:20 +0530

    core: updates the backoff range as per the A6 redefinition (#11858)
    * core: updates the backoff range being used from [0, 1] to [0.8, 1.2] as per the A6 redefinition
    
    * adds a flag for experimental jitter
    
    * xds: Allow FaultFilter's interceptor to be reused
    
    This is the only usage of PickSubchannelArgs when creating a filter's
    ClientInterceptor, and a follow-up commit will remove the argument and
    actually reuse the interceptors. Other filter's interceptors can
    already be reused.
    
    There doesn't seem to be any significant loss of legibility by making
    FaultFilter a more ordinary interceptor, but the change does cause the
    ForwardingClientCall to be present when faultDelay is configured,
    independent of whether the fault delay ends up being triggered.
    
    Reusing interceptors will move more state management out of the RPC path
    which will be more relevant with RLQS.
    
    * netty: Removed 4096 min buffer size (#11856)
    
    * netty: Removed 4096 min buffer size
    
    * turns the flag in a var for better efficiency
    
    ---------
    
    Co-authored-by: Eric Anderson <ejona@google.com>

--- core/src/main/java/io/grpc/internal/RetriableStream.java
+++ core/src/main/java/io/grpc/internal/RetriableStream.java
@@ -846,6 +846,15 @@ abstract class RetriableStream<ReqT> implements ClientStream {
     }
   }
 
+  private static final boolean isExperimentalRetryJitterEnabled = GrpcUtil
+          .getFlag("GRPC_EXPERIMENTAL_XDS_RLS_LB", true);
+
+  public static long intervalWithJitter(long intervalNanos) {
+    double inverseJitterFactor = isExperimentalRetryJitterEnabled
+            ? 0.8 * random.nextDouble() + 0.4 : random.nextDouble();
+    return (long) (intervalNanos * inverseJitterFactor);
+  }
+
   private static final class SavedCloseMasterListenerReason {
     private final Status status;
     private final RpcProgress progress;
@@ -1066,7 +1075,7 @@ abstract class RetriableStream<ReqT> implements ClientStream {
         if (pushbackMillis == null) {
           if (isRetryableStatusCode) {
             shouldRetry = true;
-            backoffNanos = (long) (nextBackoffIntervalNanos * random.nextDouble());
+            backoffNanos = intervalWithJitter(nextBackoffIntervalNanos);
             nextBackoffIntervalNanos = Math.min(
                 (long) (nextBackoffIntervalNanos * retryPolicy.backoffMultiplier),
                 retryPolicy.maxBackoffNanos);
