e7e308db4042a831bc3a9aee525cfdbc1b65862c
Ashley Zhang <ashleywzhang@google.com>
2025-02-21 21:04:58 +0000

    Fix style

--- xds/src/main/java/io/grpc/xds/XdsTransportCallCredentialsProvider.java
+++ xds/src/main/java/io/grpc/xds/XdsTransportCallCredentialsProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 The gRPC Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.grpc.xds;
 
 import io.grpc.CallCredentials;
@@ -7,7 +23,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Stores custom call credentials to use on the xDS transport for specific targets, and supplies
- * them to {@link GrpcXdsTransport}.
+ * them to the xDS client.
  */
 @Internal
 public final class XdsTransportCallCredentialsProvider {
