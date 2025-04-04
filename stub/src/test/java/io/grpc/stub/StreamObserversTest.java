aae52de3b8fee34dfc548d36d802cbb07af0c2b6
Eric Anderson <ejona@google.com>
2025-04-04 10:58:52 -0700

    stub: Add RunWith(JUnit4) to support varied environments
    Some JUnit environments require the RunWith annotation. Notably
    Blaze/Bazel needs it.

--- stub/src/test/java/io/grpc/stub/StreamObserversTest.java
+++ stub/src/test/java/io/grpc/stub/StreamObserversTest.java
@@ -17,9 +17,12 @@
 package io.grpc.stub;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
+@RunWith(JUnit4.class)
 public class StreamObserversTest {
 
   @Test
