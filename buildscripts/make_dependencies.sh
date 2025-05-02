grpc/grpc-java

commit 8d8e03c291f97965fec53c18400e503f0b32ed7a

On multiarch arch64, it requires cmake 3.13 to allow using abseil module instead of as a package, and this version is not available to install. Hence building cmake from source. Building the latest source of cmake needs openssl location to be specified (or have openssl turned off) so using the older version 3.13.4

buildscripts/make_dependencies.sh
@@ -5,6 +5,7 @@ set -evux -o pipefail
 
 PROTOBUF_VERSION=22.5
 ABSL_VERSION=20230125.4
+CMAKE_VERSION=3.13.4
 
 # ARCH is x86_64 bit unless otherwise specified.
 ARCH="${ARCH:-x86_64}"
@@ -29,6 +30,16 @@ if [ -f ${INSTALL_DIR}/bin/protoc ]; then
   echo "Not building protobuf. Already built"
 # TODO(ejona): swap to `brew install --devel protobuf` once it is up-to-date
 else
+  if [[ ! -d "cmake-${CMAKE_VERSION}" ]]; then
+    curl -Ls "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz" | tar xz
+  fi
+  # the same source dir is used for 32 and 64 bit builds, so we need to clean stale data first 
+  rm -rf "$DOWNLOAD_DIR/cmake-${CMAKE_VERSION}/bin"
+  cd "$DOWNLOAD_DIR/cmake-${CMAKE_VERSION}"
+  ./bootstrap
+  make
+  make install
+  ln -s /usr/local/bin/cmake /usr/bin/cmake
   if [[ ! -d "protobuf-${PROTOBUF_VERSION}" ]]; then
     curl -Ls "https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.gz" | tar xz
     curl -Ls "https://github.com/abseil/abseil-cpp/archive/refs/tags/${ABSL_VERSION}.tar.gz" | tar xz
@@ -60,7 +71,8 @@ else
     cmake .. \
       -DCMAKE_CXX_STANDARD=14 -Dprotobuf_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -Dcrosscompile_ARCH="$GCC_ARCH" \
-      -DCMAKE_TOOLCHAIN_FILE=$BUILDSCRIPTS_DIR/toolchain.cmake
+      -DCMAKE_TOOLCHAIN_FILE=$BUILDSCRIPTS_DIR/toolchain.cmake \
+      -B.
   fi
   export CMAKE_BUILD_PARALLEL_LEVEL="$NUM_CPU"
   cmake --build .

buildscripts/grpc-java-artifacts/Dockerfile
@@ -3,7 +3,6 @@ FROM almalinux:8
 RUN yum install -y \
             autoconf \
             automake \
-            cmake \
             diffutils \
             gcc-c++ \
             glibc-devel \

buildscripts/grpc-java-artifacts/Dockerfile.multiarch.base
@@ -4,7 +4,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get update && \
     apt-get upgrade -y && \
     apt-get install -y --no-install-recommends \
-        cmake \
         build-essential \
         ca-certificates \
         curl \

buildscripts/grpc-java-artifacts/Dockerfile.ubuntu2004.base
@@ -4,7 +4,6 @@ RUN export DEBIAN_FRONTEND=noninteractive && \
     apt-get update && \
     apt-get upgrade -y && \
     apt-get install -y --no-install-recommends \
-        cmake \
         build-essential \
         ca-certificates \
         curl \

commit 767c0723a6a5c76ee9b707a5ffbebc601a82022c

Re-do lines deleted by mistake (introduced in commit 8d8e03c291f97965fec53c18400e503f0b32ed7a) to build cmake from source on 64 bit archs.

buildscripts/make_dependencies.sh
@@ -30,6 +30,16 @@ if [ -f ${INSTALL_DIR}/bin/protoc ]; then
   echo "Not building protobuf. Already built"
 # TODO(ejona): swap to `brew install --devel protobuf` once it is up-to-date
 else
+  if [[ ! -d "cmake-${CMAKE_VERSION}" ]]; then
+    curl -Ls "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz" | tar xz
+  fi
+  # the same source dir is used for 32 and 64 bit builds, so we need to clean stale data first 
+  rm -rf "$DOWNLOAD_DIR/cmake-${CMAKE_VERSION}/bin"
+  cd "$DOWNLOAD_DIR/cmake-${CMAKE_VERSION}"
+  ./bootstrap
+  make
+  make install
+  ln -s /usr/local/bin/cmake /usr/bin/cmake
   cd "$DOWNLOAD_DIR"
   if [[ ! -d "protobuf-${PROTOBUF_VERSION}" ]]; then
     curl -Ls "https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.gz" | tar xz