4177f3eab1ce8bbe1a83234244ac4aff807476cf
Kannan J <kannanjgithub@google.com>
2025-05-02 09:50:20 +0000

    use VC 2022 install path directly in windows32.bat since vswhere doesn't show it even when it is available

--- buildscripts/kokoro/windows32.bat
+++ buildscripts/kokoro/windows32.bat
@@ -18,7 +18,7 @@ set JAVA_HOME=
 
 mkdir grpc-java-helper32
 cd grpc-java-helper32
-call "%VS140COMNTOOLS%\vsvars32.bat" || exit /b 1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat" || exit /b 1
 call "%WORKSPACE%\buildscripts\make_dependencies.bat" || exit /b 1
 
 cd "%WORKSPACE%"
