9f3f4de7b9dd5ec5ce2ba2e815d5bcd555b66498
Kannan J <kannanjgithub@google.com>
2025-05-12 12:14:14 +0000

    KERNEL32.dll position has moved in deps dump output, and avoid patching the CoreFoundation change for linux.

--- buildscripts/make_dependencies.sh
+++ buildscripts/make_dependencies.sh
@@ -34,7 +34,10 @@ else
     curl -Ls "https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.gz" | tar xz
     curl -Ls "https://github.com/abseil/abseil-cpp/archive/refs/tags/${ABSL_VERSION}.tar.gz" | tar xz
     rmdir "protobuf-$PROTOBUF_VERSION/third_party/abseil-cpp"
-    patch abseil-cpp-$ABSL_VERSION/absl/time/CMakeLists.txt $BUILDSCRIPTS_DIR/absl_time_CMakeLists.patch
+    os_name=$(uname -s)
+    if [[ "$os_name" == "Darwin" ]]; then
+      patch abseil-cpp-$ABSL_VERSION/absl/time/CMakeLists.txt $BUILDSCRIPTS_DIR/absl_time_CMakeLists.patch
+    fi
     mv "abseil-cpp-$ABSL_VERSION" "protobuf-$PROTOBUF_VERSION/third_party/abseil-cpp"
   fi
   # the same source dir is used for 32 and 64 bit builds, so we need to clean stale data first
