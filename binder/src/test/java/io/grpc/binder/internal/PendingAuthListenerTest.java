33f8227022b6d9d08c2ac14c3f6e026a9198d274
Mateus Azis <azis@google.com>
2025-07-15 18:01:02 -0700

    format and check status

--- binder/src/test/java/io/grpc/binder/internal/PendingAuthListenerTest.java
+++ binder/src/test/java/io/grpc/binder/internal/PendingAuthListenerTest.java
@@ -2,16 +2,8 @@ package io.grpc.binder.internal;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
-import androidx.annotation.Nullable;
-
-import com.google.common.io.ByteStreams;
 
 import io.grpc.CallOptions;
 import io.grpc.Channel;
@@ -27,7 +19,6 @@ import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
 import io.grpc.ServerInterceptor;
 import io.grpc.ServerServiceDefinition;
-import io.grpc.ServiceDescriptor;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.inprocess.InProcessChannelBuilder;
@@ -36,41 +27,28 @@ import io.grpc.stub.ClientCalls;
 import io.grpc.stub.ServerCalls;
 import io.grpc.testing.GrpcCleanupRule;
 import io.grpc.testing.TestMethodDescriptors;
-
-import org.junit.Assert;
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.time.Duration;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @RunWith(JUnit4.class)
 public final class PendingAuthListenerTest {
 
-  @Rule
-  public final MockitoRule mocks = MockitoJUnit.rule();
-  @Rule
-  public final GrpcCleanupRule grpcCleanupRule = new GrpcCleanupRule();
+  @Rule public final MockitoRule mocks = MockitoJUnit.rule();
+  @Rule public final GrpcCleanupRule grpcCleanupRule = new GrpcCleanupRule();
 
-  @Mock
-  ServerCallHandler<Object, Object> next;
-  @Mock
-  ServerCall<Object, Object> call;
-  @Mock
-  ServerCall.Listener<Object> delegate;
+  @Mock ServerCallHandler<Object, Object> next;
+  @Mock ServerCall<Object, Object> call;
+  @Mock ServerCall.Listener<Object> delegate;
 
   private final Metadata headers = new Metadata();
   private final PendingAuthListener<Object, Object> listener = new PendingAuthListener<>();
@@ -131,55 +109,76 @@ public final class PendingAuthListenerTest {
     AtomicBoolean closed = new AtomicBoolean(false);
     MethodDescriptor<Void, Void> method = TestMethodDescriptors.voidMethod();
     ServerCallHandler<Void, Void> callHandler =
-        ServerCalls.asyncUnaryCall((req, respObserver) -> {
-          throw new IllegalStateException("ooops");
-        });
+        ServerCalls.asyncUnaryCall(
+            (req, respObserver) -> {
+              throw new IllegalStateException("ooops");
+            });
     ServerServiceDefinition serviceDef =
         ServerServiceDefinition.builder(name).addMethod(method, callHandler).build();
     Server server =
-        InProcessServerBuilder.forName(name).addService(serviceDef).intercept(
-            new ServerInterceptor() {
-              @SuppressWarnings("unchecked")
-              @Override
-              public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
-                  ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        InProcessServerBuilder.forName(name)
+            .addService(serviceDef)
+            .intercept(
+                new ServerInterceptor() {
+                  @SuppressWarnings("unchecked")
+                  @Override
+                  public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
+                      ServerCall<ReqT, RespT> call,
+                      Metadata headers,
+                      ServerCallHandler<ReqT, RespT> next) {
 
-                listener.startCall((ServerCall<Object, Object>) call, headers, (ServerCallHandler<Object,
-                    Object>) next);
-                return (ServerCall.Listener<ReqT>) listener;
-              }
-          }).build().start();
+                    listener.startCall(
+                        (ServerCall<Object, Object>) call,
+                        headers,
+                        (ServerCallHandler<Object, Object>) next);
+                    return (ServerCall.Listener<ReqT>) listener;
+                  }
+                })
+            .build()
+            .start();
     ManagedChannel channel =
-        InProcessChannelBuilder.forName(name).intercept(new ClientInterceptor() {
-      @Override
-      public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
-          MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
-        ClientCall<ReqT, RespT> delegate = next.newCall(method, callOptions);
-        return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(delegate){
-          @Override
-          public void start(Listener<RespT> responseListener, Metadata headers) {
-            ClientCall.Listener<RespT> wrappedListener =
-                new ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(
-                    responseListener){
+        InProcessChannelBuilder.forName(name)
+            .intercept(
+                new ClientInterceptor() {
+                  @Override
+                  public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
+                      MethodDescriptor<ReqT, RespT> method, CallOptions callOptions, Channel next) {
+                    ClientCall<ReqT, RespT> delegate = next.newCall(method, callOptions);
+                    return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(
+                        delegate) {
                       @Override
-                      public void onClose(Status status, Metadata trailers) {
-                        super.onClose(status, trailers);
-                        closed.set(true);
+                      public void start(Listener<RespT> responseListener, Metadata headers) {
+                        ClientCall.Listener<RespT> wrappedListener =
+                            new ForwardingClientCallListener.SimpleForwardingClientCallListener<
+                                RespT>(responseListener) {
+                              @Override
+                              public void onClose(Status status, Metadata trailers) {
+                                super.onClose(status, trailers);
+                                closed.set(true);
+                              }
+                            };
+                        super.start(wrappedListener, headers);
                       }
                     };
-            super.start(wrappedListener, headers);
-          }
-        };
-      }
-    }).build();
+                  }
+                })
+            .build();
     grpcCleanupRule.register(server);
     grpcCleanupRule.register(channel);
 
     // Act
-    assertThrows(StatusRuntimeException.class, () -> ClientCalls.blockingUnaryCall(channel,
-        method, CallOptions.DEFAULT.withDeadlineAfter(Duration.ofSeconds(5)), /* request= */ (Void)null));
+    StatusRuntimeException ex =
+        assertThrows(
+            StatusRuntimeException.class,
+            () ->
+                ClientCalls.blockingUnaryCall(
+                    channel,
+                    method,
+                    CallOptions.DEFAULT.withDeadlineAfter(Duration.ofSeconds(5)),
+                    /* request= */ null));
 
     // Assert
+    assertThat(ex.getStatus().getCode()).isEqualTo(Status.Code.INTERNAL);
     assertThat(closed.get()).isTrue();
   }
 }
