d40799c6bd4da8a9112629bffd80576cc97aa781
John Cormie <jdcormie@google.com>
2025-07-29 01:26:19 -0700

    Rephrase javadoc, strike nullable

--- binder/src/main/java/io/grpc/binder/internal/SystemApis.java
+++ binder/src/main/java/io/grpc/binder/internal/SystemApis.java
@@ -19,25 +19,25 @@ import android.content.Context;
 import android.os.UserHandle;
 import com.google.common.base.VerifyException;
 import java.lang.reflect.Method;
-import javax.annotation.Nullable;
 
 /**
  * A collection of static methods that wrap hidden Android "System APIs."
  *
- * <p>grpc-java can't call Android's @SystemApi methods directly. Being a library built outside the
- * Android source tree, the compiler just can't see these "non-SDK" methods. Instead we resort to
- * reflection but use static wrappers for javadoc and to keep call sites tidy and type safe.
+ * <p>grpc-java can't call Android methods marked @SystemApi directly, even though many of our users
+ * are "system apps" entitled to do so. Being a library built outside the Android source tree, these
+ * "non-SDK" elements simply don't exist from our compiler's perspective. Instead we resort to
+ * reflection but use the static wrappers found here to keep call sites readable and type safe.
  *
- * <p>>Modern Android's JRE also restricts the visibility of these methods at runtime, whether
- * reflection is used or not. Callers are responsible for ensuring our host is actually a "system
- * app". See https://developer.android.com/guide/app-compatibility/restrictions-non-sdk-interfaces
- * for more.
+ * <p>>Modern Android's JRE also limits the visibility of these methods at *runtime*. Only certain
+ * privileged apps installed on the system image app can call them, even using reflection, and this
+ * wrapper doesn't change that. Callers are responsible for ensuring that the host app actually has
+ * the ability to call @SystemApis. See
+ * https://developer.android.com/guide/app-compatibility/restrictions-non-sdk-interfaces for more.
  */
 public class SystemApis {
   private static volatile Method createContextAsUserMethod;
 
   /** Returns a new Context object whose methods act as if they were running in the given user. */
-  @Nullable
   public static Context createContextAsUser(Context context, UserHandle userHandle, int flags) {
     try {
       if (createContextAsUserMethod == null) {
