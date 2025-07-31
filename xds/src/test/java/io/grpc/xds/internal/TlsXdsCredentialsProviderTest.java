3a2382525c2c787d2e8035208aa203ae68332aab
Damian Zgoda <dzgoda@google.com>
2025-07-31 11:13:07 +0000

    Bootstrap file validation for TLS channel creds

--- xds/src/test/java/io/grpc/xds/internal/TlsXdsCredentialsProviderTest.java
+++ xds/src/test/java/io/grpc/xds/internal/TlsXdsCredentialsProviderTest.java
@@ -16,14 +16,21 @@
 
 package io.grpc.xds.internal;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import com.google.common.collect.ImmutableMap;
+import io.grpc.ChannelCredentials;
 import io.grpc.InternalServiceProviders;
 import io.grpc.TlsChannelCredentials;
 import io.grpc.xds.XdsCredentialsProvider;
+import java.io.File;
+import java.util.Map;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -31,6 +38,9 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class TlsXdsCredentialsProviderTest {
   private TlsXdsCredentialsProvider provider = new TlsXdsCredentialsProvider();
+
+  @Rule
+  public TemporaryFolder tempFolder = new TemporaryFolder();
 
   @Test
   public void provided() {
@@ -50,8 +60,50 @@ public class TlsXdsCredentialsProviderTest {
   }
 
   @Test
-  public void channelCredentials() {
+  public void channelCredentialsWhenNullConfig() {
     assertSame(TlsChannelCredentials.class,
         provider.newChannelCredentials(null).getClass());
+  }
+
+  @Test
+  public void channelCredentialsWhenNotExistingTrustFileConfig() {
+    Map<String, ?> jsonConfig = ImmutableMap.of(
+        "ca_certificate_file", "/tmp/not-exisiting-file.txt");
+    assertNull(provider.newChannelCredentials(jsonConfig));
+  }
+
+  @Test
+  public void channelCredentialsWhenNotExistingCertificateFileConfig() {
+    Map<String, ?> jsonConfig = ImmutableMap.of(
+        "certificate_file", "/tmp/not-exisiting-file.txt",
+        "private_key_file", "/tmp/not-exisiting-file-2.txt");
+    assertNull(provider.newChannelCredentials(jsonConfig));
+  }
+
+  @Test
+  public void channelCredentialsWhenInvalidConfig() throws Exception {
+    File certFile = tempFolder.newFile(new String("identity.cert"));
+    Map<String, ?> jsonConfig = ImmutableMap.of("certificate_file", certFile.toString());
+    assertNull(provider.newChannelCredentials(jsonConfig));
+  }
+
+  @Test
+  public void channelCredentialsWhenValidConfig() throws Exception {
+    File trustFile = tempFolder.newFile(new String("root.cert"));
+    File certFile = tempFolder.newFile(new String("identity.cert"));
+    File keyFile = tempFolder.newFile(new String("private.key"));
+
+    Map<String, ?> jsonConfig = ImmutableMap.of(
+        "ca_certificate_file", trustFile.toString(),
+        "certificate_file", certFile.toString(),
+        "private_key_file", keyFile.toString());
+
+    ChannelCredentials creds = provider.newChannelCredentials(jsonConfig);
+    assertSame(TlsChannelCredentials.class, creds.getClass());
+    assertSame(((TlsChannelCredentials) creds).getCustomCertificatesConfig(), jsonConfig);
+
+    trustFile.delete();
+    certFile.delete();
+    keyFile.delete();
   }
 }
