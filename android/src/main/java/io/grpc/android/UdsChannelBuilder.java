3f7ec922db5f905cf0bdc0a664c9814755319a05
vimanikag <viswakarma18rc@gmail.com>
2025-08-29 11:28:54 +0000

    11922 :: UdsChannelBuilder does not work with WiFi Proxy on Android.

--- android/src/main/java/io/grpc/android/UdsChannelBuilder.java
+++ android/src/main/java/io/grpc/android/UdsChannelBuilder.java
@@ -82,9 +82,6 @@ public final class UdsChannelBuilder {
       OKHTTP_CHANNEL_BUILDER_CLASS
           .getMethod("socketFactory", SocketFactory.class)
           .invoke(builder, new UdsSocketFactory(path, namespace));
-      if (builder == null) {
-        throw new NullPointerException("builder must not be null");
-      }
       return builder.proxyDetector(GrpcUtil.NOOP_PROXY_DETECTOR);
     } catch (IllegalAccessException e) {
       throw new RuntimeException("Failed to create OkHttpChannelBuilder", e);
