593f4fbf9c07a2c268b321a33fc1f41b1637a920
MV Shiva <speakupshiva@gmail.com>
2025-09-30 19:27:50 +0530

    Update XdsTrustManagerFactoryTest.java

--- xds/src/test/java/io/grpc/xds/internal/security/trust/XdsTrustManagerFactoryTest.java
+++ xds/src/test/java/io/grpc/xds/internal/security/trust/XdsTrustManagerFactoryTest.java
@@ -189,7 +189,7 @@ public class XdsTrustManagerFactoryTest {
         new XdsTrustManagerFactory(new X509Certificate[] {x509Cert}, certValidationContext, false);
     XdsX509TrustManager xdsX509TrustManager = (XdsX509TrustManager) factory.getTrustManagers()[0];
     assertThat(xdsX509TrustManager.getAcceptedIssuers()).isNotNull();
-    assertThat(xdsX509TrustManager.getAcceptedIssuers()).hasLength(2);
+    assertThat(xdsX509TrustManager.getAcceptedIssuers()).hasLength(1);
     assertThat(xdsX509TrustManager.getAcceptedIssuers()[0].getIssuerX500Principal().getName())
         .isEqualTo("CN=testca,O=Internet Widgits Pty Ltd,ST=Some-State,C=AU");
     assertThat(xdsX509TrustManager.getAcceptedIssuers()[1].getIssuerX500Principal().getName())
