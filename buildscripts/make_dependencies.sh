9888b797d942bf005c586248131a2f30628caf33
Kannan J <kannanjgithub@google.com>
2025-11-24 14:17:27 +0000

    Try force fetch protobuf deps because of the error "Could NOT find absl (missing: absl_DIR)" (https://btx.cloud.google.com/invocations/a22ed88e-4a46-4b41-a306-c6fcd4df73af/targets/grpc%2Fjava%2Fmaster%2Fpresubmit%2Flinux_artifacts;config=default/log)

--- buildscripts/make_dependencies.sh
+++ buildscripts/make_dependencies.sh
@@ -45,11 +45,13 @@ else
       -DCMAKE_CXX_STANDARD=17 -Dprotobuf_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DABSL_INTERNAL_AT_LEAST_CXX17=0 \
       -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+      -Dprotobuf_FORCE_FETCH_DEPENDENCIES=ON \
       -B. || exit 1
   elif [[ "$ARCH" == x86* ]]; then
     CFLAGS=-m${ARCH#*_} CXXFLAGS=-m${ARCH#*_} cmake .. \
       -DCMAKE_CXX_STANDARD=17 -Dprotobuf_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DABSL_INTERNAL_AT_LEAST_CXX17=0 \
+      -Dprotobuf_FORCE_FETCH_DEPENDENCIES=ON \
       -B. || exit 1
   else
     if [[ "$ARCH" == aarch_64 ]]; then
