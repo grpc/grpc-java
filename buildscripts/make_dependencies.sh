df7cc5ac389ed705acd96309ee44857de55af476
Kannan J <kannanjgithub@google.com>
2025-11-25 09:53:12 +0000

    Build and install abseil separately before protobuf, in an attempt to avoid a different version of aseil getting downloaded from it

--- buildscripts/make_dependencies.sh
+++ buildscripts/make_dependencies.sh
@@ -12,6 +12,50 @@ ARCH="${ARCH:-x86_64}"
 DOWNLOAD_DIR=/tmp/source
 INSTALL_DIR="/tmp/protobuf-cache/$PROTOBUF_VERSION/$(uname -s)-$ARCH"
 BUILDSCRIPTS_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+function build_and_install() {
+  if [[ "$1" == "abseil" ]]; then
+    TESTS_OFF_ARG=ABSL_BUILD_TEST_HELPERS
+  else
+    TESTS_OFF_ARG=protobuf_BUILD_TESTS  
+  fi
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    cmake .. \
+      -DCMAKE_CXX_STANDARD=17 -D${TESTS_OFF_ARG}=OFF -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+      -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+      -B. || exit 1
+  elif [[ "$ARCH" == x86* ]]; then
+    CFLAGS=-m${ARCH#*_} CXXFLAGS=-m${ARCH#*_} cmake .. \
+      -DCMAKE_CXX_STANDARD=17 -D${TESTS_OFF_ARG}=OFF -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+      -B. || exit 1
+  else
+    if [[ "$ARCH" == aarch_64 ]]; then
+      GCC_ARCH=aarch64-linux-gnu
+    elif [[ "$ARCH" == ppcle_64 ]]; then
+      GCC_ARCH=powerpc64le-linux-gnu
+    elif [[ "$ARCH" == s390_64 ]]; then
+      GCC_ARCH=s390x-linux-gnu
+    elif [[ "$ARCH" == loongarch_64 ]]; then
+      GCC_ARCH=loongarch64-unknown-linux-gnu
+    else
+      echo "Unknown architecture: $ARCH"
+      exit 1
+    fi
+    cmake .. \
+      -DCMAKE_CXX_STANDARD=17 -D${TESTS_OFF_ARG}=OFF -DBUILD_SHARED_LIBS=OFF \
+      -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+      -Dcrosscompile_ARCH="$GCC_ARCH" \
+      -DCMAKE_TOOLCHAIN_FILE=$BUILDSCRIPTS_DIR/toolchain.cmake \
+      -B. || exit 1
+  fi
+  export CMAKE_BUILD_PARALLEL_LEVEL="$NUM_CPU"
+  cmake --build . || exit 1
+  # install here so we don't need sudo
+  cmake --install . || exit 1
+}
+
 mkdir -p $DOWNLOAD_DIR
 cd "$DOWNLOAD_DIR"
 
@@ -33,49 +77,21 @@ else
   if [[ ! -d "protobuf-${PROTOBUF_VERSION}" ]]; then
     curl -Ls "https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.gz" | tar xz
     curl -Ls "https://github.com/abseil/abseil-cpp/archive/refs/tags/${ABSL_VERSION}.tar.gz" | tar xz
-    mv "abseil-cpp-$ABSL_VERSION" "protobuf-$PROTOBUF_VERSION/third_party/abseil-cpp"
   fi
   # the same source dir is used for 32 and 64 bit builds, so we need to clean stale data first
+  rm -rf "$DOWNLOAD_DIR/abseil-cpp-${ABSL_VERSION}/build"
+  mkdir "$DOWNLOAD_DIR/abseil-cpp-${ABSL_VERSION}/build"
+  pushd "$DOWNLOAD_DIR/abseil-cpp-${ABSL_VERSION}/build"
+  build_and_install "abseil"
+  popd 
+  
   rm -rf "$DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION}/build"
   mkdir "$DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION}/build"
   pushd "$DOWNLOAD_DIR/protobuf-${PROTOBUF_VERSION}/build"
-  # install here so we don't need sudo
-  if [[ "$(uname -s)" == "Darwin" ]]; then
-    cmake .. \
-      -DCMAKE_CXX_STANDARD=17 -Dprotobuf_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
-      -B. || exit 1
-  elif [[ "$ARCH" == x86* ]]; then
-    CFLAGS=-m${ARCH#*_} CXXFLAGS=-m${ARCH#*_} cmake .. \
-      -DCMAKE_CXX_STANDARD=17 -Dprotobuf_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -B. || exit 1
-  else
-    if [[ "$ARCH" == aarch_64 ]]; then
-      GCC_ARCH=aarch64-linux-gnu
-    elif [[ "$ARCH" == ppcle_64 ]]; then
-      GCC_ARCH=powerpc64le-linux-gnu
-    elif [[ "$ARCH" == s390_64 ]]; then
-      GCC_ARCH=s390x-linux-gnu
-    elif [[ "$ARCH" == loongarch_64 ]]; then
-      GCC_ARCH=loongarch64-unknown-linux-gnu
-    else
-      echo "Unknown architecture: $ARCH"
-      exit 1
-    fi
-    cmake .. \
-      -DCMAKE_CXX_STANDARD=17 -Dprotobuf_BUILD_TESTS=OFF -DBUILD_SHARED_LIBS=OFF \
-      -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -Dcrosscompile_ARCH="$GCC_ARCH" \
-      -DCMAKE_TOOLCHAIN_FILE=$BUILDSCRIPTS_DIR/toolchain.cmake \
-      -B. || exit 1
-  fi
-  export CMAKE_BUILD_PARALLEL_LEVEL="$NUM_CPU"
-  cmake --build . || exit 1
-  cmake --install . || exit 1
-  [ -d "$INSTALL_DIR/lib64" ] && mv "$INSTALL_DIR/lib64" "$INSTALL_DIR/lib"
+  build_and_install "protobuf"
   popd
+  
+  [ -d "$INSTALL_DIR/lib64" ] && mv "$INSTALL_DIR/lib64" "$INSTALL_DIR/lib"  
 fi
 
 # If /tmp/protobuf exists then we just assume it's a symlink created by us.
@@ -93,3 +109,4 @@ export CXXFLAGS="$(PKG_CONFIG_PATH=/tmp/protobuf/lib/pkgconfig pkg-config --cfla
 export LIBRARY_PATH=/tmp/protobuf/lib
 export LD_LIBRARY_PATH=/tmp/protobuf/lib
 EOF
+
