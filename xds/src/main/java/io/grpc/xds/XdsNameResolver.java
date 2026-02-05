88e63d800846ead9231ec340b6e541a330de65be
petit_baguette <petit_baguette@yahoo.co.jp>
2026-02-05 21:53:29 +0900

    chore(xds): remove unused import

--- xds/src/main/java/io/grpc/xds/XdsNameResolver.java
+++ xds/src/main/java/io/grpc/xds/XdsNameResolver.java
@@ -73,7 +73,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
