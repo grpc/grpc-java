449ba369f603b7849e7230b1cbb6527a791d38d3
Kannan J <kannanjgithub@google.com>
2026-02-05 15:29:51 +0530

    Update EquivalentAddressGroup.java
    Wrap lines to be lesser than 100 characters.

--- api/src/main/java/io/grpc/EquivalentAddressGroup.java
+++ api/src/main/java/io/grpc/EquivalentAddressGroup.java
@@ -113,9 +113,9 @@ public final class EquivalentAddressGroup {
 
   @Override
   public String toString() {
-    // EquivalentAddressGroup is intended to contain a small number of addresses for the same endpoint
-    // (e.g., IPv4/IPv6). Aggregating many groups into a single EquivalentAddressGroup is no
-    // longer done, so this no longer needs summarization.
+    // EquivalentAddressGroup is intended to contain a small number of addresses for the same
+    // endpoint(e.g., IPv4/IPv6). Aggregating many groups into a single EquivalentAddressGroup
+    // is no longer done, so this no longer needs summarization.
     return "[" + addrs + "/" + attrs + "]";
   }
 
