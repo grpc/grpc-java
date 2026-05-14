20063f0e4dad34019faa270be08b36160604f34f
John Cormie <jdcormie@google.com>
2026-05-13 18:24:01 -0700

    examples: Add an grpc-android dep to helloworld to demonstrate

--- examples/android/helloworld/app/src/main/java/io/grpc/helloworldexample/HelloworldActivity.java
+++ examples/android/helloworld/app/src/main/java/io/grpc/helloworldexample/HelloworldActivity.java
@@ -29,7 +29,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.android.AndroidChannelBuilder;
 import io.grpc.examples.helloworld.GreeterGrpc;
 import io.grpc.examples.helloworld.HelloReply;
 import io.grpc.examples.helloworld.HelloRequest;
@@ -91,7 +91,12 @@ public class HelloworldActivity extends AppCompatActivity {
       String portStr = params[2];
       int port = TextUtils.isEmpty(portStr) ? 0 : Integer.valueOf(portStr);
       try {
-        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        Activity activity = activityReference.get();
+        AndroidChannelBuilder channelBuilder = AndroidChannelBuilder.forAddress(host, port);
+        if (activity != null) {
+          channelBuilder.context(activity.getApplicationContext());
+        }
+        channel = channelBuilder.usePlaintext().build();
         GreeterGrpc.GreeterBlockingStub stub = GreeterGrpc.newBlockingStub(channel);
         HelloRequest request = HelloRequest.newBuilder().setName(message).build();
         HelloReply reply = stub.sayHello(request);
