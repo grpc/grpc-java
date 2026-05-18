e231c879b81e5957068681cf994f70c10f182649
Kainsin <github@kainsin.com>
2026-05-18 08:47:13 -0400

    Remove JSR-305 ThreadSafe annotation and replace with JavaDoc (#12762)
    For non-final public classes and interfaces only, this PR removes JSR-305 ThreadSafe annotations but instead of replacing with ErrorProne's ThreadSafe, sticks to adding a JavaDoc comment. This should basically keep things inline with what JSR-305 ThreadSafe affords.
    
    Adding ErrorProne's ThreadSafe can be considered in the future, as it
    expects more things than JSR-305.
    
    Removing the JSR-305 dependency here allows Java applications that have
    moved away from javax to compile and avoids a bug in Immutables and
    Lombok (and possibly other annotation processors) from failing when
    JSR-305 is not present.

--- api/src/main/java/io/grpc/Server.java
+++ api/src/main/java/io/grpc/Server.java
@@ -21,13 +21,13 @@ import java.net.SocketAddress;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Server for listening for and dispatching incoming calls. It is not expected to be implemented by
  * application code or interceptors.
+ *
+ * <p>This class is thread-safe.
  */
-@ThreadSafe
 public abstract class Server {
 
   /**
