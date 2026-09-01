ad6d6f80ce667da2778d0f4eddfb75c96e7f83f8
Kannan J <kannanjgithub@google.com>
2026-09-01 06:57:55 +0000

    Revert "Implement gRFC A97: xDS JWT Call Credentials (#12951)" (#13017)
    This reverts commit 1053f4be9505ae50c9a9678fd23595991cbdd359. We need to
    address the concerns raised in #13006.

--- xds/src/test/java/io/grpc/xds/RbacFilterTest.java
+++ xds/src/test/java/io/grpc/xds/RbacFilterTest.java
@@ -483,7 +483,7 @@ public class RbacFilterTest {
             .node(Node.newBuilder().build())
             .build())
         .serverInfo(ServerInfo.create(
-            "test_target", Collections.emptyMap(), false, true, false, false, null))
+            "test_target", Collections.emptyMap(), false, true, false, false))
         .build();
   }
 }
