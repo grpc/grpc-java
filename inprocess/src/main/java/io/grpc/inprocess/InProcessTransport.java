117b185efbe2ddbd43d1404ab18ecd71ab824457
jlaportebot <jlaportebot@gmail.com>
2026-09-14 15:05:29 -0400

    fix: remove @Override from InProcessServerStream.setMessageCompression(boolean, String) - ServerStream doesn't declare this method

--- inprocess/src/main/java/io/grpc/inprocess/InProcessTransport.java
+++ inprocess/src/main/java/io/grpc/inprocess/InProcessTransport.java
@@ -668,8 +668,6 @@ final class InProcessTransport implements ServerTransport, ConnectionClientTrans
         // noop
       }
 
-      @Override
-      @SuppressWarnings({"UnusedMethod", "UnusedVariable"})
       public void setMessageCompression(boolean enabled, String compressorName) {
         // noop
       }
